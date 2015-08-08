25e0068c4e45ba6e8e729eec04762e874f531a23
Jeremy Caney <Jeremy.Caney@Ignia.com>
2015-08-07 18:00:58 -0700

    Delete Prop.bat

--- Prop.bat
+++
@@ -1,9 +0,0 @@
-copy Ignia.Topics.dll d:\Web.Sites\Test.Sites\GoldSim.com\Bin
-copy Ignia.Topics.pdb d:\Web.Sites\Test.Sites\GoldSim.com\Bin
-copy Ignia.Topics.dll d:\Web.Sites\Test.Sites\Ignia.com\Bin
-copy Ignia.Topics.pdb d:\Web.Sites\Test.Sites\Ignia.com\Bin
-copy Ignia.Topics.dll d:\Web.Sites\Test.Sites\Vertafore.Projects.Ignia.com\Bin
-copy Ignia.Topics.pdb d:\Web.Sites\Test.Sites\Vertafore.Projects.Ignia.com\Bin
-REM copy Ignia.Topics.dll d:\Web.Sites\Live.Sites\Ignia.com\Bin
-REM copy Ignia.Topics.pdb d:\Web.Sites\Live.Sites\Ignia.com\Bin
-pause
