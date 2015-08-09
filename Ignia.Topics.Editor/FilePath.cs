742890e2b89f0242806fedc79c18fb01dc2b6ea8
Katherine Trunkey <ktrunkey@gmail.com>
2015-08-09 16:02:11 -0700

    Ignia.Topics.Editor Comment Conversion: Initial Pass
    Converted flowerbox comment-block comments to XML documentation format
    (as summary elements); added param and returns elements as appropriate.

--- Ignia.Topics.Editor/FilePath.cs
+++ Ignia.Topics.Editor/FilePath.cs
@@ -2,35 +2,40 @@
 | Author        Katherine Trunkey, Ignia LLC
 | Client        Ignia, LLC
 | Project       Topics Library
-|
-| Purpose       Provides a strongly-typed class associated with the FilePath.ascx Attribute Type control and locic associated
-|               with building a configured file path from values passed to the constructor.
-|
 >=============================================================================================================================*/
 using System;
 
 namespace Ignia.Topics.Editor {
 
-  /*=============================================================================================================================
+  /*============================================================================================================================
   | CLASS
-  \----------------------------------------------------------------------------------------------------------------------------*/
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Provides a strongly-typed class associated with the FilePath.ascx Attribute Type control and logic associated with
+  ///   building a configured file path from values passed to the constructor.
+  /// </summary>
   public class FilePath {
 
-  /*===========================================================================================================================
-  | DECLARE PRIVATE VARIABLES
-  \--------------------------------------------------------------------------------------------------------------------------*/
-
-  /*===========================================================================================================================
-  | CONSTRUCTOR
-  \--------------------------------------------------------------------------------------------------------------------------*/
+    /*==========================================================================================================================
+    | CONSTRUCTOR
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Initializes a new instance of the <see cref="FilePath"/> class.
+    /// </summary>
     public FilePath() { }
 
-  /*===========================================================================================================================
-  | GET PATH
-  >============================================================================================================================
-  | Static helper method that returns a constructed file path based on evaluation and processing of the parameter
-  | values/settings passed to the method.
-  \--------------------------------------------------------------------------------------------------------------------------*/
+    /*==========================================================================================================================
+    | METHOD: GET PATH
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Static helper method that returns a constructed file path based on evaluation and processing of the parameter
+    ///   values/settings passed to the method.
+    /// </summary>
+    /// <param name="topic">The topic object.</param>
+    /// <param name="attributeKey">The attribute key.</param>
+    /// <param name="includeLeafTopic">Boolean indicator as to whether to include the endpoint/leaf topic in the path.</param>
+    /// <param name="truncatePathAtTopic">The assembled topic keys at which to end the path string.</param>
+    /// <returns></returns>
     public static string GetPath(
       Topic     topic,
       string    attributeKey,
@@ -38,22 +43,22 @@ namespace Ignia.Topics.Editor {
       string[]  truncatePathAtTopic     = null
       ) {
 
-    /*-------------------------------------------------------------------------------------------------------------------------
-    | ONLY PROCESS THE PATH IF BOTH TOPIC AND ATTRIBUTEKEY ARE PROVIDED
-    \------------------------------------------------------------------------------------------------------------------------*/
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Only process the path if both topic and attribtueKey are provided
+      \-----------------------------------------------------------------------------------------------------------------------*/
       if (topic == null || String.IsNullOrEmpty(attributeKey)) return "";
 
-    /*-------------------------------------------------------------------------------------------------------------------------
-    | BUILD CONFIGURED FILE PATH STRING BASED ON VALUES AND SETTINGS PARAMETERS PASSED TO THE METHOD
-    \------------------------------------------------------------------------------------------------------------------------*/
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Build configured file path string base on values and settings paramters passed to the method
+      \-----------------------------------------------------------------------------------------------------------------------*/
       string    filePath                = "";
       string    relativePath            = null;
       Topic     startTopic              = topic;
       Topic     endTopic                = includeLeafTopic? topic : topic.Parent;
 
-    /*-------------------------------------------------------------------------------------------------------------------------
-    | CRAWL UP THE TOPICS TREE TO FIND FILE PATH VALUES SET AT A HIGHER LEVEL
-    \------------------------------------------------------------------------------------------------------------------------*/
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Crawl up the topics tree to find file path values set at a higher level
+      \-----------------------------------------------------------------------------------------------------------------------*/
       while (String.IsNullOrEmpty(filePath) && startTopic != null) {
         startTopic                      = startTopic.Parent;
         if (startTopic != null && !String.IsNullOrEmpty(attributeKey)) {
@@ -61,14 +66,14 @@ namespace Ignia.Topics.Editor {
         }
       }
 
-    /*-------------------------------------------------------------------------------------------------------------------------
-    | ADD TOPIC KEYS (DIRECTORY NAMES) BETWEEN THE START TOPIC AND END TOPIC BASED ON THE TOPICS' WEBPATH PROPERTIES
-    \------------------------------------------------------------------------------------------------------------------------*/
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Add topic keys (directory names) between the start topic and the end topic based on the topic's webpath property
+      \-----------------------------------------------------------------------------------------------------------------------*/
       relativePath                      = endTopic.WebPath.Substring(startTopic.WebPath.Length);
 
-    /*-------------------------------------------------------------------------------------------------------------------------
-    | PERFORM PATH TRUNCATION BASED ON TOPICS INCLUDED IN TRUNCATEPATHATTOPIC
-    \------------------------------------------------------------------------------------------------------------------------*/
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Perform path truncation based on topics included in TruncatePathAtTopic
+      \-----------------------------------------------------------------------------------------------------------------------*/
       if (truncatePathAtTopic != null) {
         foreach (string truncationTopic in truncatePathAtTopic) {
           int truncateTopicLocation     = relativePath.IndexOf(truncationTopic, StringComparison.InvariantCultureIgnoreCase);
@@ -78,21 +83,21 @@ namespace Ignia.Topics.Editor {
         }
       }
 
-    /*-------------------------------------------------------------------------------------------------------------------------
-    | ADD RESULTING RELATIVE PATH TO THE ORIGINAL FILE PATH (BASED ON STARTING TOPIC)
-    \------------------------------------------------------------------------------------------------------------------------*/
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Add resulting relative path to the original file path (based on starting topic)
+      \-----------------------------------------------------------------------------------------------------------------------*/
       filePath                         += relativePath;
 
-    /*-------------------------------------------------------------------------------------------------------------------------
-    | REPLACE PATH SLASHES WITH BACKSLASHES IF THE RESULTING FILE PATH VALUE USES A UNC OR BASIC FILE PATH FORMAT
-    \------------------------------------------------------------------------------------------------------------------------*/
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Replace path slashes with backslahes if the resulting file path value uses a UNC or basic file path format
+      \-----------------------------------------------------------------------------------------------------------------------*/
       if (filePath.IndexOf("\\") >= 0) {
         filePath                        = filePath.Replace("/", "\\");
       }
 
-    /*-------------------------------------------------------------------------------------------------------------------------
-    | RETURN RESULTING FILE PATH
-    \------------------------------------------------------------------------------------------------------------------------*/
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Return resulting file path
+      \-----------------------------------------------------------------------------------------------------------------------*/
       return filePath;
 
     }
