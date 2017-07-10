c910787c2a69d78fa4772bd49a2a089b8b248a61
Jeremy Caney <Jeremy.Caney@Ignia.com>
2017-07-09 18:01:09 -0700

    TopicController Placeholder
    Established the project and template for the MVC project itself,
    including the TopicController class file. Included references as
    appropriate to the test project and overall solution. The controller
    file is, as of now, empty; the actual code will come later.

---
+++ Ignia.Topics.Web.Mvc/TopicController.cs
@@ -0,0 +1,28 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Topics Library
+\=============================================================================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+/*============================================================================================================================
+| CLASS: TOPIC TEST
+\---------------------------------------------------------------------------------------------------------------------------*/
+/// <summary>
+///   Provides a default ASP.NET MVC Controller for any paths associated with the Ignia Topic Library. Responsible for 
+///   identifying the topic associated with the given path, determining its content type, and returning a view associated with
+///   that content type (with potential overrides for multiple views). 
+/// </summary>
+namespace Ignia.Topics.Web.Mvc {
+  class TopicController : Controller {
+
+  } //Class
+
+} //Namespace
+
+
