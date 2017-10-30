3e7efed9a4f983037c577bb76b3f1bf87fa1facb
Jeremy Caney <Jeremy.Caney@Ignia.com>
2017-10-30 12:14:24 -0700

    Extended documentation
    This is a somewhat complex and unintuitive part of recent refactoring. As a result, I've updated the documentation with more details and concrete examples to ensure its functionality is clear.

--- Ignia.Topics/AttributeSetterAttribute.cs
+++ Ignia.Topics/AttributeSetterAttribute.cs
@@ -13,12 +13,37 @@ using Ignia.Topics.Collections;
 namespace Ignia.Topics {
 
   /*============================================================================================================================
-  | CLASS: ATTRIBUTE
+  | CLASS: ATTRIBUTE SETTER [ATTRIBUTE]
   \---------------------------------------------------------------------------------------------------------------------------*/
   /// <summary>
   ///   Flags that a property should be used when setting an attribute via
   ///   <see cref="AttributeValueCollection.SetValue(String, String, Boolean?)"/>.
   /// </summary>
+  /// <remarks>
+  ///   <para>
+  ///     When a call is made to <see cref="AttributeValueCollection.SetValue(String, String, Boolean?)"/>, the code will check
+  ///     to see if a property with the same name as the attribute key exists, and whether that property is decorated with the
+  ///     <see cref="AttributeSetterAttribute"/> (i.e., <code>[AttributeSetter]</code>). If it is, then the update will be
+  ///     routed through that property. This ensures that business logic is enforced by local properties, instead of allowing
+  ///     business logic to be potentially bypassedby writing directly to the <see cref="Topic.Attributes"/> collection.
+  ///   </para>
+  ///   <para>
+  ///     As an example, the <see cref="Topic.Key"/> property is adorned with the <see cref="AttributeSetterAttribute"/>. As a
+  ///     result, if a client calls <code>topic.Attributes.SetValue("Key", "NewKey")</code> then that update will be routed
+  ///     through <see cref="Topic.Key"/>, thus enforcing key validation, and calling
+  ///     <see cref="TopicCollection{T}.ChangeKey(T, string)"/>. Similarly, if <code>topic.Attributes.SetValue("Key", ":/? ")
+  ///     </code> were called, a contract exception will be thrown since <code>:/? </code> violates
+  ///     <see cref="Topic.ValidateKey(string, bool)"/>.
+  ///   </para>
+  ///   <para>
+  ///     To ensure this logic, it is critical that implementers of <see cref="AttributeSetterAttribute"/> ensure that the
+  ///     property setters call <see cref="AttributeValueCollection.SetValue(String, String, Boolean?, Boolean)"/> overload with
+  ///     the final parameter set to false to disable the enforcement of business logic. Otherwise, an infinite loop will occur.
+  ///     Calling that overload tells <see cref="AttributeValueCollection"/> that the business logic has already been enforced
+  ///     by the caller. As this is an internal overload, implementers should use the local proxy at
+  ///     <see cref="Topic.SetAttributeValue(String, String, Boolean?)"/>, which ensures that final parameter is set to false.
+  ///   </para>
+  /// </remarks>
   [System.AttributeUsage(System.AttributeTargets.Property)]
   sealed class AttributeSetterAttribute : System.Attribute {
 
