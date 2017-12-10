6b439ac85dc3ab0fb538b505d38f824304c8639c
Jeremy Caney <Jeremy.Caney@Ignia.com>
2017-12-10 12:41:01 -0800

    Removed legacy Attributes objects
    Previously, Attributes (and AttributeTypes) were stored in their own table, and accessed using their own sprocs. Today, those object types are stored as topic attributes via Oroborous configuration. As such, those objects no longer provide value.

--- Ignia.Topics.Data.Sql.Database/dbo/Tables/topics_AttributeTypes.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[topics_AttributeTypes] (
-    [AttributeTypeID]      INT            NOT NULL,
-    [AttributeTypeName]    NVARCHAR (124) NOT NULL,
-    [DefaultConfiguration] VARCHAR (124)  CONSTRAINT [DF_topics_AttributeTypes_DefaultConfiguration] DEFAULT ('') NULL,
-    CONSTRAINT [PK_topics_AttributeTypes] PRIMARY KEY CLUSTERED ([AttributeTypeID] ASC)
-);
-
