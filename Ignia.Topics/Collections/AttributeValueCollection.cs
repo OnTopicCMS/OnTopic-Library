0892eee69a11ab5264316933b966b43924a30005
Jeremy Caney <Jeremy.Caney@Ignia.com>
2018-03-17 16:28:20 -0700

    Fixed XmlDoc reference

--- Ignia.Topics/Collections/AttributeValueCollection.cs
+++ Ignia.Topics/Collections/AttributeValueCollection.cs
@@ -223,7 +223,7 @@ namespace Ignia.Topics.Collections {
     ///   Specified whether the value should be marked as <see cref="AttributeValue.IsDirty"/>. By default, it will be marked as
     ///   dirty if the value is new or has changed from a previous value. By setting this parameter, that behavior is
     ///   overwritten to accept whatever value is submitted. This can be used, for instance, to prevent an update from being
-    ///   persisted to the data store on <see cref="Topic.Save(Boolean, Boolean)"/>.
+    ///   persisted to the data store on <see cref="Repositories.ITopicRepository.Save(Topic, bool, bool)"/>.
     /// </param>
     /// <param name="enforceBusinessLogic">
     ///   Instructs the underlying code to call corresponding properties, if available, to ensure business logic is enforced.
