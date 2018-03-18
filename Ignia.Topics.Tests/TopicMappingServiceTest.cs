10c395c517c7f667831db091a499ec6b478e369a
Jeremy Caney <Jeremy.Caney@Ignia.com>
2018-03-18 12:33:54 -0700

    Added test for dynamic lookup of content type
    This test uses the default `Map(Topic topic)` overload, which will lookup a topic's `ContentType` and find any classes called `{ContentType}TopicViewModel`.

--- Ignia.Topics.Tests/TopicMappingServiceTest.cs
+++ Ignia.Topics.Tests/TopicMappingServiceTest.cs
@@ -57,6 +57,42 @@ namespace Ignia.Topics.Tests {
 
     }
 
+    /*==========================================================================================================================
+    | TEST: PROPERTY INFO COLLECTION CONSTRUCTOR
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Establishes a <see cref="PropertyInfoCollection"/> based on a type, and confirms that the property collection is
+    ///   returning expected types.
+    /// </summary>
+    [TestMethod]
+    public void TopicMappingService_MapDynamic() {
+
+      var mappingService = new TopicMappingService();
+      var grandParent = TopicFactory.Create("Grandparent", "Page");
+      var parent = TopicFactory.Create("Parent", "Page", grandParent);
+      var topic = TopicFactory.Create("Test", "Page", parent);
+
+      topic.Attributes.SetValue("MetaTitle", "ValueA");
+      topic.Attributes.SetValue("Title", "Value1");
+      topic.Attributes.SetValue("IsHidden", "1");
+
+      parent.Attributes.SetValue("Title", "Value2");
+      parent.Attributes.SetValue("IsHidden", "1");
+
+      grandParent.Attributes.SetValue("Title", "Value3");
+      grandParent.Attributes.SetValue("IsHidden", "1");
+
+      var target = (PageTopicViewModel)mappingService.Map(topic);
+
+      Assert.AreEqual<string>("ValueA", target.MetaTitle);
+      Assert.AreEqual<string>("Value1", target.Title);
+      Assert.AreEqual<bool>(true, target.IsHidden);
+      Assert.AreEqual<string>("Value2", target.Parent.Title);
+      Assert.AreEqual<string>("Value3", target.Parent.Parent.Title);
+
+    }
+
+
   } //Class
 
 } //Namespace
