3c7bd3c572bc1f80d6eb7464f14b349fc7b536df
Jeremy Caney <Jeremy.Caney@Ignia.com>
2018-03-19 15:54:28 -0700

    Ensure collections are sorted
    By default, all collections (e.g., Children) are sorted when loaded from the repository. But if their sort order is changed in the editor, that change won't be fully reflected until the data is reloaded. This is because it's involved to change the sort order of an existing collection. To ensure this doesn't impact views, calling `Sorted` on `Children` references ensures that they are correctly sorted.

--- Ignia.Topics/TopicMappingService.cs
+++ Ignia.Topics/TopicMappingService.cs
@@ -245,7 +245,7 @@ namespace Ignia.Topics {
 
           //Handle children
           if (property.Name.Equals("Children") && includeRelationships) {
-            listSource = topic.Children;
+            listSource = (IList)topic.Children.Sorted;
           }
 
           //Handle (outgoing) relationships
@@ -259,7 +259,7 @@ namespace Ignia.Topics {
           //Handle nested topics
           if (listSource.Count == 0) {
             if (topic.Children.Contains(property.Name) && topic.Children[property.Name].ContentType.Equals("TopicList")) {
-              listSource = topic.Children[property.Name].Children;
+              listSource = (IList)topic.Children[property.Name].Children.Sorted;
             }
           }
 
