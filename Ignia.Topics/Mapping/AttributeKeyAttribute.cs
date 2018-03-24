36f766c0734ccff41c6aa0a842198fc71270e15d
Jeremy Caney <Jeremy.Caney@Ignia.com>
2018-03-24 14:21:41 -0700

    Introduced `[AttributeKey]` attribute
    Have not yet added behavior to any `ITopicMappingService` implementations.

---
+++ Ignia.Topics/Mapping/AttributeKeyAttribute.cs
@@ -0,0 +1,63 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Topics Library
+\=============================================================================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ignia.Topics.Collections;
+
+namespace Ignia.Topics.Mapping {
+
+  /*============================================================================================================================
+  | ATTRIBUTE: ATTRIBUTE KEY
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Flags that a property should be mapped to a specific <c>attributeKey</c> in when calling <see
+  ///   cref="AttributeValueCollection.GetValue(String, Boolean)"/>.
+  /// </summary>
+  /// <remarks>
+  ///   By default, <see cref="ITopicMappingService"/> implementations will attempt to map the property of the target data
+  ///   transfer object to a corresponding attribute of the same name on the source topic. This attribute instructs the
+  ///   <see cref="ITopicMappingService"/> to instead look for a specified key. This allows the target property name to be
+  ///   decoupled from the source attribute key.
+  /// </remarks>
+  [System.AttributeUsage(System.AttributeTargets.Property)]
+  sealed class AttributeKeyAttribute : System.Attribute {
+
+    /*==========================================================================================================================
+    | PRIVATE VARIABLES
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    private                     string                          _attributeKey                   = null;
+
+    /*==========================================================================================================================
+    | CONSTRUCTOR
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Annotates a property with the <see cref="AttributeKeyAttribute"/> class by providing a (required) attribute key.
+    /// </summary>
+    /// <param name="attributeKey">The key value of the attribute associated with the current property.</param>
+    public AttributeKeyAttribute(string attributeKey) {
+      TopicFactory.ValidateKey(attributeKey, false);
+      _attributeKey = attributeKey;
+    }
+
+    /*==========================================================================================================================
+    | PROPERTY: VALUE
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Gets the value of the attribute key.
+    /// </summary>
+    public string Value {
+      get {
+        return _attributeKey;
+      }
+    }
+
+  } //Class
+
+} //Namespace
