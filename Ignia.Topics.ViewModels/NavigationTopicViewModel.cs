89ab030de49a834fd507d7134e61b73368ea5c92
Jeremy Caney <Jeremy.Caney@Ignia.com>
2018-04-18 23:18:52 -0700

    Introduced `NavigationTopicViewModel`
    The `NavigationTopicViewModel` is meant to provide a concrete implementation of the `INavigationTopicViewModelCore`, as a wrapper around the `IPageTopicViewModelCore` (i.e., in context of this project, the `PageTopicViewModel`).

---
+++ Ignia.Topics.ViewModels/NavigationTopicViewModel.cs
@@ -0,0 +1,28 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia
+| Project       Website
+\=============================================================================================================================*/
+
+using System.Collections.ObjectModel;
+
+namespace Ignia.Topics.ViewModels {
+
+  /*============================================================================================================================
+  | VIEW MODEL: NAVIGATION TOPIC
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Provides a strongly-typed data transfer object for feeding views with information about the navigation.
+  /// </summary>
+  /// <remarks>
+  ///   No topics are expected to have a <c>Navigation</c> content type. Instead, this view model is expected to be manually
+  ///   constructed by e.g. a <c>LayoutController</c>.
+  /// </remarks>
+  public class NavigationTopicViewModel : PageTopicViewModel, INavigationTopicViewModelCore {
+
+    public Collection<INavigationTopicViewModelCore> Children { get; set; }
+    public bool IsSelected(string uniqueKey) => uniqueKey?.StartsWith(this.UniqueKey) ?? false;
+
+  } // Class
+
+} // Namespace
