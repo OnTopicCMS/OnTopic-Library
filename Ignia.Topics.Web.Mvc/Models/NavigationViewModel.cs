cb69bad9f5e1b9f3f680791725d10c1ce3cb5527
Jeremy Caney <Jeremy.Caney@Ignia.com>
2018-04-18 18:18:13 -0700

    Introduced `NavigationViewModel`
    The `NavigationViewModel` provides a strongly-typed view model of `IPageTopicViewModelCore` implementations associated with a root node of a navigation tier. There may be multiple `NavigationViewModel`s for a site, for different tiers or levels of navigation.
    
    The view model does not assume a concrete type for the underlying `NavigationRoot`; that is provided by a generic, so long as the implementation implements `IPageTopicViewModelCore`.

---
+++ Ignia.Topics.Web.Mvc/Models/NavigationViewModel.cs
@@ -0,0 +1,35 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Topics Library
+\=============================================================================================================================*/
+using Ignia.Topics;
+
+namespace Ignia.Topics.Web.Mvc.Models {
+
+  /*============================================================================================================================
+  | VIEW MODEL: NAVIGATION TOPIC
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Provides a strongly-typed data transfer object for feeding views with information about a tier of navigation.
+  /// </summary>
+  /// <remarks>
+  ///   <para>
+  ///     No topics are expected to have a <c>Navigation</c> content type. Instead, this view model is expected to be manually
+  ///     constructed by the <see cref="LayoutController"/>.
+  ///   </para>
+  ///   <para>
+  ///     The <see cref="NavigationRoot"/> can be any view model that implements <see cref="INavigationTopicViewModelCore"/>,
+  ///     which provides a base level of support for properties associated with the typical <c>Page</c> content type as well as
+  ///     a method for determining if a given <see cref="INavigationTopicViewModelCore"/> instance is the currently-selected
+  ///     topic. Implementations may support additional properties, as appropriate.
+  ///   </para>
+  /// </remarks>
+  public class NavigationViewModel<T> where T: IPageTopicViewModelCore {
+
+    public T NavigationRoot { get; set; }
+    public string CurrentKey { get; set; }
+
+  } // Class
+
+} // Namespace
