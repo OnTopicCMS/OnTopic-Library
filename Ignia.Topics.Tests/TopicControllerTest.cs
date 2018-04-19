0290feab1dc79ed4fca6cca06fcc2081a341c226
Jeremy Caney <Jeremy.Caney@Ignia.com>
2018-04-18 22:12:36 -0700

    Introduced reusable `ErrorController
    This provides baseline support for throwing errors in an OnTopic environment. An `IPageTopicViewModelCore` is returned to the view with basic data about the error.

---
+++ Ignia.Topics.Tests/TopicControllerTest.cs
@@ -0,0 +1,107 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Topics Library
+\=============================================================================================================================*/
+using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ignia.Topics.Querying;
+using Ignia.Topics.Web.Mvc;
+using Ignia.Topics.Repositories;
+using Ignia.Topics.Data.Caching;
+using Ignia.Topics.Tests.TestDoubles;
+using Ignia.Topics.Web.Mvc.Controllers;
+using Ignia.Topics.ViewModels;
+using System.Web.Mvc;
+
+namespace Ignia.Topics.Tests {
+
+  /*============================================================================================================================
+  | CLASS: TOPIC CONTROLLER TEST
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Provides unit tests for the <see cref="TopicController"/>, and other <see cref="Controller"/> classes that are part of
+  ///   the <see cref="Ignia.Topics.Web.Mvc"/> namespace.
+  /// </summary>
+  [TestClass]
+  public class TopicControllerTest {
+
+    /*==========================================================================================================================
+    | PRIVATE VARIABLES
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    ITopicRepository            _topicRepository                = null;
+
+    /*==========================================================================================================================
+    | CONSTRUCTOR
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Initializes a new instance of the <see cref="TopicControllerTest"/> with shared resources.
+    /// </summary>
+    /// <remarks>
+    ///   This uses the <see cref="FakeTopicRepository"/> to provide data, and then <see cref="CachedTopicRepository"/> to
+    ///   manage the in-memory representation of the data. While this introduces some overhead to the tests, the latter is a
+    ///   relatively lightweight façade to any <see cref="ITopicRepository"/>, and prevents the need to duplicate logic for
+    ///   crawling the object graph.
+    /// </remarks>
+    public TopicControllerTest() {
+      _topicRepository = new CachedTopicRepository(new FakeTopicRepository());
+    }
+
+    /*==========================================================================================================================
+    | TEST: ERROR
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Triggers the error action of the <see cref="ErrorController{T}.Error(string)" /> action.
+    /// </summary>
+    [TestMethod]
+    public void ErrorController_Error() {
+
+      var controller            = new ErrorController<PageTopicViewModel>();
+      var result                = controller.Error("ErrorPage") as ViewResult;
+      var model                 = result.Model as PageTopicViewModel;
+
+      Assert.IsNotNull(model);
+      Assert.AreEqual<string>("ErrorPage", model.Title);
+
+    }
+
+    /*==========================================================================================================================
+    | TEST: NOT FOUND ERROR
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Triggers the error action of the <see cref="ErrorController{T}.NotFound(string)" /> action.
+    /// </summary>
+    [TestMethod]
+    public void ErrorController_NotFound() {
+
+      var controller            = new ErrorController<PageTopicViewModel>();
+      var result                = controller.Error("NotFoundPage") as ViewResult;
+      var model                 = result.Model as PageTopicViewModel;
+
+      Assert.IsNotNull(model);
+      Assert.AreEqual<string>("NotFoundPage", model.Title);
+
+    }
+
+    /*==========================================================================================================================
+    | TEST: INTERNAL SERVER ERROR
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Triggers the error action of the <see cref="ErrorController{T}.InternalServer(string)" /> action.
+    /// </summary>
+    [TestMethod]
+    public void ErrorController_InternalServer() {
+
+      var controller            = new ErrorController<PageTopicViewModel>();
+      var result                = controller.Error("InternalServer") as ViewResult;
+      var model                 = result.Model as PageTopicViewModel;
+
+      Assert.IsNotNull(model);
+      Assert.AreEqual<string>("InternalServer", model.Title);
+
+    }
+
+  } //Class
+
+} //Namespace
---
+++ Ignia.Topics.Web.Mvc/Controllers/ErrorController.cs
@@ -0,0 +1,115 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Topics Library
+\=============================================================================================================================*/
+using System.Web.Mvc;
+
+namespace Ignia.Topics.Web.Mvc.Controllers {
+
+  /*============================================================================================================================
+  | CLASS: ERROR CONTROLLER
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Provides access to the views associated with 400 and 500 error results.
+  /// </summary>
+  /// <remarks>
+  ///   Implementers may wish to provide derived classes which return more specific error messages. This class provides a
+  ///   generic implementation that should suit most requirements.
+  /// </remarks>
+  public class ErrorController<T> : Controller where T : IPageTopicViewModelCore, new() {
+
+    /*==========================================================================================================================
+    | GET: /Error/Error
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Provides the default custom error page for the site.
+    /// </summary>
+    /// <returns>The site's default error view.</returns>
+    public ActionResult Error(string title = "General Error") {
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Instantiate view model
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      var viewModel             = new T();
+      viewModel.Key             = "Error";
+      viewModel.WebPath         = "/Error/Error";
+      viewModel.ContentType     = "Page";
+      viewModel.Title           = title;
+      viewModel.MetaKeywords    = "";
+      viewModel.MetaDescription = "";
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Return the view
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      return View("Error", viewModel);
+
+    }
+
+    /*==========================================================================================================================
+    | GET: /Error/NotFound
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Provides the custom 404 error page for the site.
+    /// </summary>
+    /// <returns>The site's 404 (not found) error view.</returns>
+    public ActionResult NotFound(string title = "Page Not Found") {
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Return the proper status code
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      Response.StatusCode       = 404;
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Instantiate view model
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      var viewModel             = new T();
+      viewModel.Key             = "NotFound";
+      viewModel.WebPath         = "/Error/NotFound";
+      viewModel.ContentType     = "Page";
+      viewModel.Title           = title;
+      viewModel.MetaKeywords    = "";
+      viewModel.MetaDescription = "";
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Return the view
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      return View("NotFound", viewModel);
+
+    }
+
+    /*==========================================================================================================================
+    | GET: /Error/InternalServer
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Provides the custom 500 error page for the site.
+    /// </summary>
+    /// <returns>The site's 500 (internal server) error view.</returns>
+    public ActionResult InternalServer(string title = "Internal Server Error") {
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Return the proper status code
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      Response.StatusCode       = 500;
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Instantiate model
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      var viewModel             = new T();
+      viewModel.Key             = "InternalServer";
+      viewModel.WebPath         = "/Error/InternalServer";
+      viewModel.ContentType     = "Page";
+      viewModel.Title           = title;
+      viewModel.MetaKeywords    = "";
+      viewModel.MetaDescription = "";
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Return the view
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      return View("InternalServer", viewModel);
+
+    }
+
+  } // Class
+
+} // Namespace
