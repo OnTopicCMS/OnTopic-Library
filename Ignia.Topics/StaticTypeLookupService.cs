fcbd2e8af7b01af49f05e8c370cec0b51a5b73ea
Jeremy Caney <Jeremy.Caney@Ignia.com>
2018-05-06 16:35:42 -0700

    Removed unused `using` statements

--- Ignia.Topics/StaticTypeLookupService.cs
+++ Ignia.Topics/StaticTypeLookupService.cs
@@ -7,7 +7,6 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics.Contracts;
-using System.Linq;
 using System.Reflection;
 
 namespace Ignia.Topics {
