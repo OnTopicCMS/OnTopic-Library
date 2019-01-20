61566a5e53b1195a6c7901bd8e592613df81edbe
Jeremy Caney <Jeremy.Caney@Ignia.com>
2019-01-20 02:52:29 -0800

    Introduced attributes for syntactical consistency
    Code Contracts support a number of attributes to associate classes with one another or mark methods as pure. These serve no purpose in absence of code rewrite and analysis modules. Nevertheless, they are provided for syntactical consistency.

---
+++ Ignia.Topics/Diagnostics/ContractClassForAttribute.cs
@@ -0,0 +1,30 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Topics Library
+\=============================================================================================================================*/
+using System;
+
+namespace Ignia.Topics.Diagnostics {
+
+  /*============================================================================================================================
+  | CLASS: CONTRACT CLASS FOR ATTRIBUTE
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Designates which class the code contracts in the decorated class are intended to apply to. Intended only for syntactical
+  ///   consistency with Code Contracts.
+  /// </summary>
+  [Obsolete("Not implemented. The attribute is maintained for syntactical consistency only. References should be removed.")]
+  public class ContractClassForAttribute : Attribute {
+
+    /*==========================================================================================================================
+    | CONSTRUCTOR
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Initializes a new instance of the <see cref="ContractClassAttribute"/> with an associated <see cref="Type"/>.
+    /// </summary>
+    /// <param name="type">The class that contains the code contracts.</param>
+    public ContractClassForAttribute(Type type) { }
+
+  }
+}
---
+++ Ignia.Topics/Diagnostics/PureAttribute.cs
@@ -0,0 +1,20 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Topics Library
+\=============================================================================================================================*/
+using System;
+
+namespace Ignia.Topics.Diagnostics {
+
+  /*============================================================================================================================
+  | CLASS: PURE ATTRIBUTE
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Marks that a method is flagged as pure. Intended only for syntactical consistency with Code Contracts.
+  /// </summary>
+  [Obsolete("Not implemented. The attribute is maintained for syntactical consistency only. References should be removed.")]
+  public class PureAttribute : Attribute {
+  }
+
+}
