0283d2f2245b5df63894c9bfa129962f92fd64ab
Jeremy Caney <Jeremy.Caney@Ignia.com>
2019-02-06 20:26:42 -0800

    Established extension method for configuring OnTopic
    The `ServiceCollectionExtensions.AddTopicSupport()` provides a convenient way to register *OnTopic* related features with the *ASP.NET Core* configuration pipeline. In this case, it adds  support for the new `TopicViewLocationExpander` so that it doesn't need to be configured manually by implementers. Additional items may be added to this in the future.

---
+++ Ignia.Topics.AspNetCore.Mvc/ServiceCollectionExtensions.cs
@@ -0,0 +1,47 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Topics Library
+\=============================================================================================================================*/
+using System;
+using Ignia.Topics.Diagnostics;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ignia.Topics.AspNetCore.Mvc {
+
+  /*============================================================================================================================
+  | CLASS: SERVICE COLLECTION EXTENSIONS
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Provides extension methods for configuring the OnTopic library with ASP.NET Core MVC.
+  /// </summary>
+  public static class ServiceCollectionExtensions {
+
+    /*==========================================================================================================================
+    | EXTENSION: ADD TOPIC SUPPORT
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Configures the Razor engine to include OnTopic view locations via the <see cref="TopicViewLocationExpander"/>.
+    /// </summary>
+    public static IMvcBuilder AddTopicSupport(this IMvcBuilder services) {
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Validate parameters
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      Contract.Requires<ArgumentNullException>(services == null, nameof(services));
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Configure services
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      services.AddRazorOptions(o => {
+        o.ViewLocationExpanders.Add(new TopicViewLocationExpander());
+      });
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Return services for fluent API
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      return services;
+    }
+
+  } //Class
+} //Namespace
