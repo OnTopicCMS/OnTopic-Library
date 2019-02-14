OnTopicCMS/OnTopic-Library

commit bf51bd293f2afb5407cc31c954145bdddad7b43c

Replaced `XmlDocument` with `XmlReader`
The `XmlReader`, while less elegant syntactically, is safer and faster, plus a more deliberate workflow in terms of working with XML content from a `SqlDataReader`.

Ignia.Topics.Data.Sql/SqlTopicRepository.cs
@@ -147,8 +147,8 @@ namespace Ignia.Topics.Data.Sql {
       /*------------------------------------------------------------------------------------------------------------------------
       | Load blob into XmlDocument
       \-----------------------------------------------------------------------------------------------------------------------*/
-      var blob = new XmlDocument();
-      blob.LoadXml((string)reader?["Blob"]);
+      var xmlData               = reader.GetSqlXml(1);
+      var xmlReader             = xmlData.CreateReader();
 
       /*------------------------------------------------------------------------------------------------------------------------
       | Identify the current topic
@@ -158,13 +158,13 @@ namespace Ignia.Topics.Data.Sql {
       /*------------------------------------------------------------------------------------------------------------------------
       | Loop through nodes to set attributes
       \-----------------------------------------------------------------------------------------------------------------------*/
-      foreach (XmlNode attribute in blob.DocumentElement.GetElementsByTagName("attribute")) {
+      while (xmlReader.ReadToFollowing("attribute")) {
 
         /*----------------------------------------------------------------------------------------------------------------------
         | Identify attributes
         \---------------------------------------------------------------------------------------------------------------------*/
-        var name                = attribute.Attributes?["key"]?.Value;
-        var value               = WebUtility.HtmlDecode(attribute.InnerXml);
+        var name                = (string)xmlReader.GetAttribute("key");
+        var value               = WebUtility.HtmlDecode(xmlReader.ReadInnerXml());
 
         /*----------------------------------------------------------------------------------------------------------------------
         | Validate assumptions
@@ -181,6 +181,7 @@ namespace Ignia.Topics.Data.Sql {
         current.Attributes.SetValue(name, value, false);
 
       }
+
     }
 
     /*==========================================================================================================================

commit 126718a1c2eb9c0249d5173b09c94698eb8d831f

Resolved bug in reading blob attributes
A recent commit, bf51bd2, introduced an `XmlReader` in preference over the previous `XmlDocument`. While faster, and safer, this also introduced a new bug because both `ReadToFollowing()` _and_ `ReadInnerXml()` advance the position of the reader. As a result, only every other attribute would be read. This is resolved by, instead, using a `do {} while (reader.Name == "attribute")` which prevents the loop itself from advancing the reader. This resolves issues with some blob attributes not showing up, which was causing problems in both the public interface as well as the editor.

Ignia.Topics.Data.Sql/SqlTopicRepository.cs
@@ -156,15 +156,20 @@ namespace Ignia.Topics.Data.Sql {
       var current               = topics[id];
 
       /*------------------------------------------------------------------------------------------------------------------------
+      | Handle scenario where there isn't an <attribute /> element
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      if (!xmlReader.ReadToFollowing("attribute")) return;
+
+      /*------------------------------------------------------------------------------------------------------------------------
       | Loop through nodes to set attributes
       \-----------------------------------------------------------------------------------------------------------------------*/
-      while (xmlReader.ReadToFollowing("attribute")) {
+      do {
 
         /*----------------------------------------------------------------------------------------------------------------------
         | Identify attributes
         \---------------------------------------------------------------------------------------------------------------------*/
-        var name                = (string)xmlReader.GetAttribute("key");
-        var value               = WebUtility.HtmlDecode(xmlReader.ReadInnerXml());
+        var name = (string)xmlReader.GetAttribute("key");
+        var value = WebUtility.HtmlDecode(xmlReader.ReadInnerXml());
 
         /*----------------------------------------------------------------------------------------------------------------------
         | Validate assumptions
@@ -180,7 +185,7 @@ namespace Ignia.Topics.Data.Sql {
         if (String.IsNullOrEmpty(value)) continue;
         current.Attributes.SetValue(name, value, false);
 
-      }
+      } while (xmlReader.Name == "attribute");
 
     }
 