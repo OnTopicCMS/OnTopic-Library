7ef76393dc4853f029dc068013be2b0c510c9d90
Jeremy Caney <Jeremy.Caney@Ignia.com>
2019-02-20 21:07:59 -0800

    Established initial interface definition
    This definition may need to evolve as the design of the concrete implementation is fleshed out, and new requirements emerge. For now, however, this covers the basic aspects that we'd expect a reverse mapping service to cover. The big unknown right now is how to handle relationships, since that gets into the merging question.

---
+++ Ignia.Topics/Mapping/IReverseTopicMappingService.cs
@@ -0,0 +1,93 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Topics Library
+\=============================================================================================================================*/
+using System;
+using System.Threading.Tasks;
+using Ignia.Topics.Repositories;
+using Ignia.Topics.ViewModels;
+
+namespace Ignia.Topics.Mapping {
+
+  /*============================================================================================================================
+  | INTERFACE: REVERSE TOPIC MAPPING SERVICE
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   The <see cref="IReverseTopicMappingService"/> interface provides an abstraction for mapping Data Transfer Objects
+  ///   (DTOs), such as View Models or, more likely, Binding Models, back to <see cref="Topic"/> instances.
+  /// </summary>
+  /// <remarks>
+  ///   <para>
+  ///     Unlike something like e.g. AutoMapper, the reverse mapping scenario isn't simply a matter of "flipping the script" on
+  ///     the <see cref="ITopicMappingService"/>. A binding model should not be relied upon to be an accurate description of the
+  ///     <see cref="ContentTypeDescriptor"/> (even though, ideally, it should be). Therefore, the <see
+  ///     cref="IReverseTopicMappingService"/> will instead rely upon the <see cref="ContentTypeDescriptor"/> to define the model
+  ///     and use that to selectively pull in property values accordingly. At the same time, however, it must honor hints from
+  ///     the binding model regarding what properties are intended for—especially in terms of e.g aliased relationships.
+  ///   </para>
+  ///   <para>
+  ///     The current design of <see cref="IReverseTopicMappingService"/> is not intended to support mapping relationships, as
+  ///     this overlaps with a broader concern of merging topics with an existing <see cref="Topic"/> graph (e.g., as part of an
+  ///     <see cref="ITopicRepository"/>.
+  ///   </para>
+  ///   <para>
+  ///     Despite the differences between the <see cref="ITopicMappingService"/> and <see cref="IReverseTopicMappingService"/>s,
+  ///     many attributes are able to be reused between them. For instance, the <see cref="AttributeKeyAttribute"/> can still
+  ///     map a property on a binding model to an attribute of a different name on a <see cref="Topic"/>, just as the <see
+  ///     cref="RelationshipAttribute"/> can with relationships. Other attributes, however, provde no benefit in the reverse
+  ///     scenario, such as <see cref="FlattenAttribute"/> or <see cref="FilterByAttributeAttribute"/>, which really only make
+  ///     sense in creating a "produced view" that is a subset of the original model. That is valuable when creating a view
+  ///     model, but isn't a useful use case when working with binding models.
+  ///   </para>
+  /// </remarks>
+  public interface IReverseTopicMappingService {
+
+    /*==========================================================================================================================
+    | METHOD: MAP (DYNAMIC)
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Given a binding model, will create a new instance of <see cref="Topic"/> (or the appropriate subclass), and will map
+    ///   the properties of the binding model to attributes on the <see cref="Topic"/>.
+    /// </summary>
+    /// <remarks>
+    ///   <para>
+    ///     Because the class is using reflection to determine the target View Models, the return type is <see cref="Topic"/>.
+    ///     These results may need to be cast to a specific type, depending on the context. That said, strongly-typed views
+    ///     should be able to cast the object to the appropriate View Model type. If the type of the Topic is known
+    ///     upfront, and it is imperative that it be strongly-typed, then prefer <see cref="MapAsync{T}(ITopicViewModel)"/>.
+    ///   </para>
+    /// </remarks>
+    /// <param name="bindingModel">The binding model—any plain old C# object—to derive the data from.</param>
+    /// <returns>An instance of the dynamically determined <see cref="Topic"/> with attributes appropriately mapped.</returns>
+    Task<Topic> MapAsync(ITopicViewModel bindingModel);
+
+    /*==========================================================================================================================
+    | METHOD: MAP (GENERIC)
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Given a binding model, will instantiate a new instance of the generic type and map the properties of the binding model
+    ///   to attributes on the <see cref="Topic"/>.
+    /// </summary>
+    /// <param name="bindingModel">The binding model—any plain old C# object—to derive the data from.</param>
+    /// <returns>
+    ///   An instance of the requested Topc <typeparamref name="T"/> with attributes appropriately mapped.
+    /// </returns>
+    Task<T> MapAsync<T>(ITopicViewModel bindingModel) where T : Topic;
+
+    /*==========================================================================================================================
+    | METHOD: MAP (INSTANCES)
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Given a binding model and an existing <see cref="Topic"/>, will map the properties of the binding model to attributes
+    ///   on the <see cref="Topic"/>.
+    /// </summary>
+    /// <param name="bindingModel">The binding model—any plain old C# object—to derive the data from.</param>
+    /// <param name="topic">The <see cref="Topic"/> entity to map the data to.</param>
+    /// <returns>
+    ///   An instance of provided <see cref="Topic"/> with attributes appropriately mapped.
+    /// </returns>
+    Task<Topic> MapAsync(ITopicViewModel bindingModel, Topic topic);
+
+  } //Interface
+} //Namespace
