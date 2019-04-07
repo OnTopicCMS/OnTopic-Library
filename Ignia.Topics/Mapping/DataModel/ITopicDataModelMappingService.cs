89e9dc3265cf6631f0eedd15a3c01ccb4f0c1366
Jeremy Caney <Jeremy.Caney@Ignia.com>
2019-04-07 02:33:34 -0700

    Introduced `TopicDataModelMappingService`
    The `ITopicDataModelMappingService` will map a `Topic` to a `TopicDataModel`, which provides a light-weight representation of topic data suitable for conversion, serialization, and data transfer.

---
+++ Ignia.Topics/Mapping/DataModel/ITopicDataModelMappingService.cs
@@ -0,0 +1,30 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Topics Library
+\=============================================================================================================================*/
+using System.Threading.Tasks;
+using Ignia.Topics.Models;
+
+namespace Ignia.Topics.Mapping.DataModel {
+
+  /*==============================================================================================================================
+  | INTERFACE: TOPIC DATA MODEL MAPPING SERVICE
+  \=============================================================================================================================*/
+  /// <summary>
+  ///   Provides an interface for a service that maps a <see cref="Topic"/> to a <see cref="TopicDataModel"/>.
+  /// </summary>
+  public interface ITopicDataModelMappingService {
+
+    /*==========================================================================================================================
+    | MAP (ASYNC)
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Given a <paramref name="source"/>, maps a <paramref name="target"/>.
+    /// </summary>
+    /// <param name="source">The <see cref="Topic"/> to pull the values from.</param>
+    /// <param name="target">The <see cref="TopicDataModel"/> to set the values on.</param>
+    Task<TopicDataModel> MapAsync(Topic source, TopicDataModel target = null);
+
+    } //Interface
+  } //Class
---
+++ Ignia.Topics/Mapping/DataModel/TopicDataModelMappingService.cs
@@ -0,0 +1,104 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Topics Library
+\=============================================================================================================================*/
+using Ignia.Topics.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Ignia.Topics.Mapping.DataModel
+{
+
+  /*============================================================================================================================
+  | CLASS: TOPIC DATA MODEL MAPPING SERVICE
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Implements a service that maps a <see cref="Topic"/> to a <see cref="TopicDataModel"/>.
+  /// </summary>
+  /// <remarks>
+  ///   Compared to other concrete mapping service implementations, the <see cref="TopicDataModelMappingService"/> is quite
+  ///   simple, as it doesn't need to deal with reflection. That said, it still helps centralize the logic for an otherwise
+  ///   tedious and repetitive task.
+  /// </remarks>
+  public class TopicDataModelMappingService : ITopicDataModelMappingService {
+
+    /*==========================================================================================================================
+    | CONSTRUCTOR
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Establishes a new instance of a <see cref="TopicDataModelMappingService"/> with required dependencies.
+    /// </summary>
+    public TopicDataModelMappingService() {}
+
+    /*==========================================================================================================================
+    | METHOD: MAP (OBJECTS)
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Given a <see cref="Topic"/> and an (optional) instance of a <see cref="TopicDataModel"/>, will populate the <see
+    ///   cref="TopicDataModel"/> with values from the <see cref="Topic"/>.
+    /// </summary>
+    /// <param name="source">The <see cref="Topic"/> entity to derive the data from.</param>
+    /// <param name="target">The target <see cref="TopicDataModel"/> to map the data to.</param>
+    /// <returns>
+    ///   The target <see cref="TopicDataModel"/> with the properties appropriately mapped.
+    /// </returns>
+    public async Task<TopicDataModel> MapAsync(Topic source, TopicDataModel target = null) {
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Validate input
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      if (source == null) {
+        return target;
+      }
+
+      if (target == null) {
+        target = new TopicDataModel();
+      }
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Queue up mapping tasks
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      var taskQueue = new List<Task<TopicDataModel>>();
+
+      foreach (var childTopic in source.Children) {
+        taskQueue.Add(MapAsync(childTopic));
+      }
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Process mapping tasks
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      while (taskQueue.Count > 0) {
+        var mappingTask = await Task.WhenAny(taskQueue).ConfigureAwait(false);
+        taskQueue.Remove(mappingTask);
+        var childTopic = await mappingTask.ConfigureAwait(false);
+        target.Children.Add(childTopic);
+      }
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Map attributes
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      foreach (var attribute in source.Attributes) {
+        target.Attributes.Add(attribute.Key, attribute.Value);
+      }
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Map relationships
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      foreach (var scope in source.Relationships) {
+        var relationships = new Dictionary<string, string>();
+        target.Relationships.Add(scope.Name, relationships);
+        foreach (var relationship in scope) {
+          relationships.Add(relationship.GetUniqueKey(), relationship.Title);
+        }
+      }
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Return result
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      return target;
+
+    }
+
+  } //Class
+} //Namespace
