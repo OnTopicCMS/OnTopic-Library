5fa39a30a46f839d131aa98eef88856ffbb09227
Jeremy Caney <Jeremy.Caney@Ignia.com>
2019-09-24 17:52:25 -0700

    Suppressed `CA1062` false positives
    The `CA1062` warning pertains to *C# 8*'s new nullable annotation context. Unfortunately, variables that are ensured to not be null via `Contract.Requires()` or `Contract.Assure()` are not detected as such by Roslyn's flow analysis. We'll ultimately need to report this as a bug to the language team. In the meanwhile, they're distracting from actual issues by introducing false positives. I'm not a big fan of using global suppression files—_but_ this provides a clean way of coming back to these at a later date and removing them, once the core issue has been resolved.

---
+++ Ignia.Topics.Data.Sql/GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1062:Validate arguments of public methods", Justification = "Handled by Requires()", Scope = "member", Target = "~M:Ignia.Topics.Data.Sql.SqlTopicRepository.Save(Ignia.Topics.Topic,System.Boolean,System.Boolean)~System.Int32")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1062:Validate arguments of public methods", Justification = "Handled by Requires()", Scope = "member", Target = "~M:Ignia.Topics.Data.Sql.SqlTopicRepository.Delete(Ignia.Topics.Topic,System.Boolean)")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1062:Validate arguments of public methods", Justification = "Handled by Requires()", Scope = "member", Target = "~M:Ignia.Topics.Data.Sql.SqlTopicRepository.Move(Ignia.Topics.Topic,Ignia.Topics.Topic,Ignia.Topics.Topic)")]
---
+++ Ignia.Topics.Web.Mvc/GlobalSuppressions.cs
@@ -0,0 +1,10 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1062:Validate arguments of public methods", Justification = "Handled by Requires()", Scope = "member", Target = "~M:Ignia.Topics.Web.Mvc.TopicViewResult.#ctor(Ignia.Topics.Models.ITopicViewModel)")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1062:Validate arguments of public methods", Justification = "Handled by Requires()", Scope = "member", Target = "~M:Ignia.Topics.Web.Mvc.TopicViewEngine.FindPartialView(System.Web.Mvc.ControllerContext,System.String,System.Boolean)~System.Web.Mvc.ViewEngineResult")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1062:Validate arguments of public methods", Justification = "Handled by Requires()", Scope = "member", Target = "~M:Ignia.Topics.Web.Mvc.TopicViewEngine.FindView(System.Web.Mvc.ControllerContext,System.String,System.String,System.Boolean)~System.Web.Mvc.ViewEngineResult")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1062:Validate arguments of public methods", Justification = "Handled by Requires()", Scope = "member", Target = "~M:Ignia.Topics.Web.Mvc.TopicViewResult.FindView(System.Web.Mvc.ControllerContext)~System.Web.Mvc.ViewEngineResult")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1062:Validate arguments of public methods", Justification = "Handled by Requires()", Scope = "member", Target = "~M:Ignia.Topics.Web.Mvc.Controllers.TopicController.OnActionExecuting(System.Web.Mvc.ActionExecutingContext)")]
