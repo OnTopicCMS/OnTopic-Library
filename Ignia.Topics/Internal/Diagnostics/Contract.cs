5289fcbd1118d46989e175802828fbd18b08cd15
Jeremy Caney <Jeremy.Caney@Ignia.com>
2019-09-26 00:50:59 -0700

    Removed self-assignment hack in `Requires()`
    To work around an apparent issue in *Roslyn*'s flow analysis, the `requiredObject` parameter of the `Requires()` method was set to itself in #edf39c2. This mitigated a number of warnings, but introduced its own `CS1717`, which was suppressed in #64e2a06. That issue has been resolved by introducing the `[ValidatedNotNull]` attribute, as per #b9e0151. Given that, the original hack—as well as the suppression of its subsequent warning—can be removed. Good riddance!

--- Ignia.Topics/Internal/Diagnostics/Contract.cs
+++ Ignia.Topics/Internal/Diagnostics/Contract.cs
@@ -63,15 +63,8 @@ namespace Ignia.Topics.Internal.Diagnostics {
     /// <exception cref="ArgumentNullException">
     ///   Thrown when <paramref name="requiredObject"/> is <see langword="null"/>.
     /// </exception>
-      //###HACK JJC20190908: Roslyn's flow analysis doesn't accept the [NotNull] hint for parameters unless the variable has
-      //been locally assigned, which is an issue for Requires() since it is intended to be used exclusively as a guard clause for
-      //parameters. Assigning the parameter to itself mitigates this issue—though it does prompt its own warning in return.
-      #pragma warning disable CS1717 // Assignment made to same variable
-      requiredObject = requiredObject;
-      #pragma warning restore CS1717 // Assignment made to same variable
     public static void Requires([ValidatedNotNull, NotNull]object? requiredObject, string? errorMessage = null) =>
       Requires<ArgumentNullException>(requiredObject != null, errorMessage);
-    }
 
     /// <summary>
     ///   Will throw the provided generic exception if the supplied expression evaluates to false.
