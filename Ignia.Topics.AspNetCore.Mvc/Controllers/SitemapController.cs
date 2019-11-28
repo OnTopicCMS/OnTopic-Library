d6ee255c54ce92026ba6bb03d94715925b880cc5
JeremyCaney <jeremy.caney@ignia.com>
2019-11-28 13:48:31 -0800

    Moved sitemap view logic inline
    Previously, the majority of the sitemap logic was in the view, which was simply responsible for generating XML. There were several problems with this. First, it pushed the logic down to the implementation, whereas we'd prefer for it to be centralized. Second, that put a lot of logic in the view, which it isn't well suited for. Third, it was generating static text in the format of XML, which introduced potential issues with e.g. encoding. All three of these are resolved by moving this logic into the `SitemapController` itself and then returning the output of an `XDocument`. There are probably still some further optimizations that could be made to this in terms of how the `XDocument` is constructed, but it's a big improvement over the legacy approach.

--- Ignia.Topics.AspNetCore.Mvc/Controllers/SitemapController.cs
+++ Ignia.Topics.AspNetCore.Mvc/Controllers/SitemapController.cs
@@ -7,6 +7,14 @@ using Microsoft.AspNetCore.Mvc;
 using Ignia.Topics.Repositories;
 using Ignia.Topics.AspNetCore.Mvc.Models;
 using Ignia.Topics.Internal.Diagnostics;
+using System.Xml;
+using System.Xml.Linq;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.IO;
 
 namespace Ignia.Topics.AspNetCore.Mvc.Controllers {
 
@@ -23,6 +31,38 @@ namespace Ignia.Topics.AspNetCore.Mvc.Controllers {
     | PRIVATE VARIABLES
     \-------------------------------------------------------------------------------------------------------------------------*/
     private readonly            ITopicRepository                _topicRepository;
+
+    /*==========================================================================================================================
+    | CONSTANTS
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    private static readonly XNamespace _sitemapNamespace = "http://www.sitemaps.org/schemas/sitemap/0.9";
+    private static readonly XNamespace _pagemapNamespace = "http://www.google.com/schemas/sitemap-pagemap/1.0";
+
+    /*==========================================================================================================================
+    | EXCLUDE CONTENT TYPES
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Specifies what content types should not be listed in the sitemap.
+    /// </summary>
+    private static string[] ExcludeContentTypes { get; } = { "List" };
+
+    /*==========================================================================================================================
+    | EXCLUDE ATTRIBUTES
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Specifies what attributes should not be listed in the sitemap.
+    /// </summary>
+    private static string[] ExcludeAttributes { get; } = {
+      "Body",
+      "IsActive",
+      "IsDisabled",
+      "ParentID",
+      "TopicID",
+      "IsHidden",
+      "NoIndex",
+      "URL",
+      "SortOrder"
+    };
 
     /*==========================================================================================================================
     | CONSTRUCTOR
@@ -66,19 +106,116 @@ namespace Ignia.Topics.AspNetCore.Mvc.Controllers {
       );
 
       /*------------------------------------------------------------------------------------------------------------------------
-      | Establish view model
+      | Establish sitemap
       \-----------------------------------------------------------------------------------------------------------------------*/
-      var topicViewModel = new TopicEntityViewModel(_topicRepository, rootTopic);
-
-      /*------------------------------------------------------------------------------------------------------------------------
-      | Define content type
-      \-----------------------------------------------------------------------------------------------------------------------*/
-      Response.ContentType = "text/xml";
+      var sitemap = GenerateSitemap(rootTopic);
+      var sitemapFile = new StringBuilder();
+      using (var writer = new StringWriter(sitemapFile)) {
+        sitemap.Save(writer);
+      }
 
       /*------------------------------------------------------------------------------------------------------------------------
       | Return the homepage view
       \-----------------------------------------------------------------------------------------------------------------------*/
-      return View("Sitemap", topicViewModel);
+      return Content(sitemapFile.ToString(), "text/xml");
+
+    }
+
+    /*==========================================================================================================================
+    | METHOD: GENERATE SITEMAP
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Given a root topic, generates an XML-formatted sitemap.
+    /// </summary>
+    /// <returns>The site's homepage view.</returns>
+    public virtual XDocument GenerateSitemap(Topic rootTopic) =>
+      new XDocument(
+        new XDeclaration("1.0", "utf-8", String.Empty),
+        new XElement(_sitemapNamespace + "urlset",
+          from topic in rootTopic?.Children
+          select AddTopic(topic)
+        )
+      );
+
+    /*==========================================================================================================================
+    | METHOD: ADD TOPIC
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Given a <see cref="Topic"/>, adds it to a given <see cref="XmlNode"/>.
+    /// </summary>
+    public IEnumerable<XElement> AddTopic(Topic topic) {
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Establish return collection
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      var topics = new List<XElement>();
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Validate topic
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      if (topic == null) return topics;
+      if (topic.Attributes.GetValue("NoIndex") == "1") return topics;
+      if (topic.Attributes.GetValue("IsDisabled") == "1") return topics;
+      if (ExcludeContentTypes.Any(c => topic.ContentType.Equals(c, StringComparison.InvariantCultureIgnoreCase))) return topics;
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Establish variables
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      var domain = $"{HttpContext.Request.Scheme}://{HttpContext.Request.Host}";
+      var lastModified = new DateTime(Math.Max(topic.LastModified.Ticks, new DateTime(2000, 1, 1).Ticks));
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Establish root element
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      var topicElement = new XElement(_sitemapNamespace + "url",
+        new XElement(_sitemapNamespace + "loc", domain + topic.GetWebPath()),
+        new XElement(_sitemapNamespace + "changefreq", "monthly"),
+        new XElement(_sitemapNamespace + "lastmod", lastModified.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture)),
+        new XElement(_sitemapNamespace + "priority", 1),
+        new XElement(_pagemapNamespace + "PageMap",
+          new XElement(_pagemapNamespace + "DataObject",
+            new XAttribute("type", topic.ContentType?? "Page"),
+            getAttributes()
+          ),
+          getRelationships()
+        )
+      );
+      topics.Add(topicElement);
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Iterate over children
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      foreach (var childTopic in topic.Children) {
+        topics.AddRange(AddTopic(childTopic));
+      }
+
+      return topics;
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Get attributes
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      IEnumerable<XElement> getAttributes() =>
+        from attribute in topic.Attributes
+        where !ExcludeAttributes.Contains(attribute.Key, StringComparer.InvariantCultureIgnoreCase)
+        where topic.Attributes.GetValue(attribute.Key)?.Length < 256
+        select new XElement(_pagemapNamespace + "Attribute",
+          new XAttribute("name", attribute.Key),
+          new XText(topic.Attributes.GetValue(attribute.Key))
+        );
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Get relationships
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      IEnumerable<XElement> getRelationships() =>
+        from relationship in topic.Relationships
+        select new XElement(_pagemapNamespace + "DataObject",
+          new XAttribute("type", relationship.Name),
+          from relatedTopic in topic.Relationships[relationship.Name]
+          select new XElement(_pagemapNamespace + "Attribute",
+            new XAttribute("name", "TopicKey"),
+            new XText(relatedTopic.GetUniqueKey().Replace("Root:", "", StringComparison.InvariantCultureIgnoreCase))
+          )
+        );
 
     }
 
