f798371cb1340fd2386033d30ad004d7e863bdfe
JeremyCaney <jeremy.caney@ignia.com>
2019-12-12 00:58:48 -0800

    Introduced script to detect potential sources of corruption
    Hopefully, the nested set hierarchy remains in healthy shape—and with diligence when writing queries and maintaining appropriate locks during updates, it should. However, because the nested set can be susceptible to corruption, it's important to have a way to detect and isolate problems, should they occur. This stored procedure is incredibly slow (there's no optimization for speed, and the queries are pretty ugly), but it will detect the three scenarios that indicate data corruption and help isolate them down to affected topics. That said, it makes no attempt to fix the issues; that will always require manual intervention to review the specific cases and determine what should be done.

---
+++ Ignia.Topics.Data.Sql.Database/dbo/Stored Procedures/topics_DetectHierarchyCorruption.sql
@@ -0,0 +1,74 @@
+﻿--------------------------------------------------------------------------------------------------------------------------------
+-- DETECT HIERARCHICAL CORRUPTION
+--------------------------------------------------------------------------------------------------------------------------------
+-- Helps identify potential sources of corruption in the nested set hierarchy
+--------------------------------------------------------------------------------------------------------------------------------
+-- ### NOTE JJC20191211: The nested set model is incredibly useful to fast reads of a hierarchy. But it is also susceptible to
+-- corruption from e.g., errant queries or poorly handled transactions. Ideally, this script shouldn't be necessary. But, as a
+-- safety precaution, it provides a means of identifying potential sources of corruption. It won't resolve the corruption, nor
+-- fully identify where it is. But it'll help identify scenarios that should never occur.
+--------------------------------------------------------------------------------------------------------------------------------
+CREATE PROCEDURE [dbo].[topics_DetectHierarchyCorruption]
+AS
+
+--------------------------------------------------------------------------------------------------------------------------------
+-- DETECT RANGE OVERLAPS
+--------------------------------------------------------------------------------------------------------------------------------
+-- Ranges should always be inclusive. The following query identifies scenarios where a range starts before a set, and ends in
+-- the middle of it. For example, if you have a range 2-5 and another with a range of 3-6, that's a scenario that should never
+-- occur, and which the following query will identify. Note that this query is SLOW; it is not optimized for performance!
+--------------------------------------------------------------------------------------------------------------------------------
+PRINT	'Detect range overlaps'
+
+SELECT	*
+FROM	topics_Topics		OuterTopics
+WHERE (
+  SELECT	COUNT(TopicID)
+  FROM	topics_Topics		InnerTopics
+  WHERE (	RangeLeft		< OuterTopics.RangeLeft
+    AND	RangeRight		< OuterTopics.RangeRight
+    AND	RangeRight		> OuterTopics.RangeLeft
+  )
+  OR (	RangeLeft		> OuterTopics.RangeLeft
+    AND	RangeRight		> OuterTopics.RangeRight
+    AND	RangeLeft		< OuterTopics.RangeRight
+  )
+) > 0
+
+--------------------------------------------------------------------------------------------------------------------------------
+-- DETECT RANGE DUPLICATES
+--------------------------------------------------------------------------------------------------------------------------------
+-- The RangeLeft and RangeRight should be unique within the database. This isn't enforced, however, as it complicates making
+-- updates to the hierarchy. If there is ever a scenario where two topics have the same RangeLeft or RangeRight value as another
+-- range, that indicates a problem.
+--------------------------------------------------------------------------------------------------------------------------------
+PRINT	'Detect range duplicates'
+
+SELECT	*
+FROM	topics_Topics		OuterTopics
+WHERE (
+  SELECT	COUNT(TopicID)
+  FROM	topics_Topics		InnerTopics
+  WHERE	TopicID		!= OuterTopics.TopicID
+    AND (	RangeLeft
+      IN (	  OuterTopics.RangeLeft,
+	  OuterTopics.RangeRight
+        )
+    OR	RangeRight
+      IN (	  OuterTopics.RangeLeft,
+	  OuterTopics.RangeRight
+      )
+    )
+) > 0
+
+--------------------------------------------------------------------------------------------------------------------------------
+-- DETECT RANGE MISMATCHES
+--------------------------------------------------------------------------------------------------------------------------------
+-- The LeftRange should always be lower than the RightRange. If that's not the case, something is wrong.
+--------------------------------------------------------------------------------------------------------------------------------
+PRINT	'Detect range mismatches'
+
+SELECT	*
+FROM	topics_Topics
+WHERE	RangeLeft	>= RangeRight
+
