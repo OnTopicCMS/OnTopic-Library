18c991df582478c4a79c26c53aea5246f6e3683f
JeremyCaney <jeremy.caney@ignia.com>
2019-12-17 13:43:15 -0800

    Migrated to new project format, test libraries
    When creating a new unit test project for **.NET Framework**, **Visual Studio 2019** inexplicably created the project using the legacy project format, and used old versions of **MSTest**. It's unclear why this happened. Worse, the project doesn't work; the tests aren't detected, nor can the dependencies be restored on **Azure Pipelines**. To mitigate this, the template previously used for `Ignia.Topics.Tests` (prior to its recent migration to **.NET Core 3.0**; see #8b98b43) was used instead.

--- Ignia.Topics.Web.Mvc.Tests/Properties/AssemblyInfo.cs
+++ Ignia.Topics.Web.Mvc.Tests/Properties/AssemblyInfo.cs
@@ -1,20 +1,4 @@
-using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Ignia.Topics.Web.Mvc.Tests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Ignia.Topics.Web.Mvc.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 [assembly: ComVisible(false)]
-
 [assembly: Guid("2ac4b63b-16d3-4398-baeb-e9ef3a7636a8")]
-
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
