ebd7fe113858a27030fae05440e29ba3a4fc1e61
JeremyCaney <jeremy.caney@ignia.com>
2019-12-19 15:21:04 -0800

    Introduced basic error response pages

---
+++ Ignia.Topics.Web.Mvc.Host/Controllers/ErrorController.cs
@@ -0,0 +1,20 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       OnTopicSample OnTopic Site
+\=============================================================================================================================*/
+using Ignia.Topics.ViewModels;
+using Ignia.Topics.Web.Mvc.Controllers;
+
+namespace Ignia.Topics.Web.Mvc.Host.Controllers {
+
+  /*============================================================================================================================
+  | CLASS: ERROR CONTROLLER
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Provides access to the views associated with 400 and 500 error results.
+  /// </summary>
+  public class ErrorController : ErrorControllerBase<PageTopicViewModel> {
+
+  } // Class
+} // Namespace
