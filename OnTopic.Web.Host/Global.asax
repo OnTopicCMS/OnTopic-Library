5df14d8e508aa9459ddd39fe3e0d8797a7fb14e2
JeremyCaney <jeremy.caney@ignia.com>
2019-12-25 19:23:39 -0800

    Removed `OnTopic.Web` and `OnTopic.Web.Mvc` projects
    The `OnTopic.Web` and `OnTopic.Web.Mvc` projects have been migrated to their own separate repositories—`OnTopic-WebForms` and `OnTopic-MVC`—which will be maintained independently of the main `Topics-Library` project.

--- OnTopic.Web.Host/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OnTopic.Web.Host.Global" Language="C#" %>
--- OnTopic.Web.Mvc.Tests/Properties/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-using System.Runtime.InteropServices;
-
-[assembly: ComVisible(false)]
-[assembly: Guid("2ac4b63b-16d3-4398-baeb-e9ef3a7636a8")]
