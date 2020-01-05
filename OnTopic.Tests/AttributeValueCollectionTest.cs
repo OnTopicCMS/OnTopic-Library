496f06d3789b471607a3c5a7012c59331318c5c7
JeremyCaney <jeremy.caney@ignia.com>
2020-01-04 23:27:15 -0800

    Broke `AttributeValueCollection` tests into more granular units
    Previously, there was a 1:1 mapping between most methods and their tests. Now, different states (e.g., invalid value, invalid key) are broken out as distinct tests in order to provide more granular units. This dramatically increases the number of tests. This also necessitated adopting a more expressive test naming convention: `METHOD_CONDITION_RESULT` (e.g., `GetValue_IncorrectKey_ReturnsDefault()`).

--- OnTopic.Tests/AttributeValueCollectionTest.cs
+++ OnTopic.Tests/AttributeValueCollectionTest.cs
@@ -21,157 +21,238 @@ namespace OnTopic.Tests {
   public class AttributeValueCollectionTest {
 
     /*==========================================================================================================================
-    | TEST: GET VALUE
+    | TEST: GET VALUE: CORRECT VALUE: IS RETURNED
     \-------------------------------------------------------------------------------------------------------------------------*/
     /// <summary>
     ///   Creates a new topic and ensures that the key can be returned as an attribute.
     /// </summary>
     [TestMethod]
-    public void GetValue() {
+    public void GetValue_CorrectValue_IsReturned() {
       var topic = TopicFactory.Create("Test", "Container");
       Assert.AreEqual<string>("Test", topic.Attributes.GetValue("Key"));
     }
 
     /*==========================================================================================================================
-    | TEST: GET INTEGER
+    | TEST: GET VALUE: MISSING VALUE: RETURNS DEFAULT
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Creates a new topic and requests an invalid attribute; ensures falls back to the default.
+    /// </summary>
+    [TestMethod]
+    public void GetValue_MissingValue_ReturnsDefault() {
+      var topic = TopicFactory.Create("Test", "Container");
+      Assert.AreEqual<string>("Foo", topic.Attributes.GetValue("InvalidAttribute", "Foo"));
+    }
+
+    /*==========================================================================================================================
+    | TEST: GET INTEGER: CORRECT VALUE: IS RETURNED
     \-------------------------------------------------------------------------------------------------------------------------*/
     /// <summary>
     ///   Ensures that integer values can be set and retrieved as expected.
     /// </summary>
     [TestMethod]
-    public void GetInteger() {
+    public void GetInteger_CorrectValue_IsReturned() {
 
       var topic = TopicFactory.Create("Test", "Container");
 
       topic.Attributes.SetInteger("Number1", 1);
-      topic.Attributes.SetInteger("Number2", 2);
-      topic.Attributes.SetValue("Number3", "Invalid");
 
       Assert.AreEqual<int>(1, topic.Attributes.GetInteger("Number1", 5));
-      Assert.AreEqual<int>(2, topic.Attributes.GetInteger("Number2", 5));
+
+    }
+
+    /*==========================================================================================================================
+    | TEST: GET INTEGER: INCORRECT VALUE: RETURNS DEFAULT
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Ensures that invalid values return the default.
+    /// </summary>
+    [TestMethod]
+    public void GetInteger_IncorrectValue_ReturnsDefault() {
+
+      var topic = TopicFactory.Create("Test", "Container");
+
+      topic.Attributes.SetValue("Number3", "Invalid");
+
       Assert.AreEqual<int>(5, topic.Attributes.GetInteger("Number3", 5));
+
+    }
+
+    /*==========================================================================================================================
+    | TEST: GET INTEGER: INCORRECT KEY: RETURNS DEFAULT
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Ensures that incorrect key names return the default.
+    /// </summary>
+    [TestMethod]
+    public void GetInteger_IncorrectKey_ReturnsDefault() {
+
+      var topic = TopicFactory.Create("Test", "Container");
+
       Assert.AreEqual<int>(5, topic.Attributes.GetInteger("InvalidKey", 5));
 
     }
 
     /*==========================================================================================================================
-    | TEST: GET DATETIME
+    | TEST: GET DATETIME: CORRECT VALUE: IS RETURNED
     \-------------------------------------------------------------------------------------------------------------------------*/
     /// <summary>
     ///   Ensures that integer values can be set and retrieved as expected.
     /// </summary>
     [TestMethod]
-    public void GetDateTime() {
+    public void GetDateTime_CorrectValue_IsReturned() {
+
+      var topic                 = TopicFactory.Create("Test", "Container");
+      var dateTime1             = new DateTime(1976, 10, 15);
+
+      topic.Attributes.SetDateTime("DateTime1", dateTime1);
+
+      Assert.AreEqual<DateTime>(dateTime1, topic.Attributes.GetDateTime("DateTime1", DateTime.MinValue));
+
+    }
+
+    /*==========================================================================================================================
+    | TEST: GET DATETIME: INCORRECT VALUE: RETURNS DEFAULT
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Ensures that invalid values return the default.
+    /// </summary>
+    [TestMethod]
+    public void GetDateTime_IncorrectValue_ReturnsDefault() {
 
       var topic                 = TopicFactory.Create("Test", "Container");
       var dateTime1             = new DateTime(1976, 10, 15);
       var dateTime2             = new DateTime(1981, 06, 03);
 
-      topic.Attributes.SetDateTime("DateTime1", dateTime1);
       topic.Attributes.SetDateTime("DateTime2", dateTime2);
-      topic.Attributes.SetValue("DateTime3", "Invalid");
 
-      Assert.AreEqual<DateTime>(dateTime1, topic.Attributes.GetDateTime("DateTime1", DateTime.MinValue));
-      Assert.AreEqual<DateTime>(dateTime2, topic.Attributes.GetDateTime("DateTime2", DateTime.MinValue));
       Assert.AreEqual<DateTime>(dateTime1, topic.Attributes.GetDateTime("DateTime3", dateTime1));
-      Assert.AreEqual<DateTime>(dateTime2, topic.Attributes.GetDateTime("InvalidKey", dateTime2));
 
     }
 
     /*==========================================================================================================================
-    | TEST: GET BOOLEAN
+    | TEST: GET DATETIME: INCORRECT KEY: RETURNS DEFAULT
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Ensures that incorrect key names return the default.
+    /// </summary>
+    [TestMethod]
+    public void GetDateTime_IncorrectKey_ReturnsDefault() {
+
+      var topic                 = TopicFactory.Create("Test", "Container");
+      var dateTime1             = new DateTime(1976, 10, 15);
+      var dateTime2             = new DateTime(1981, 06, 03);
+
+      topic.Attributes.SetDateTime("DateTime2", dateTime2);
+
+      Assert.AreEqual<DateTime>(dateTime1, topic.Attributes.GetDateTime("DateTime3", dateTime1));
+
+    }
+
+    /*==========================================================================================================================
+    | TEST: GET BOOLEAN: CORRECT VALUE: IS RETURNED
     \-------------------------------------------------------------------------------------------------------------------------*/
     /// <summary>
     ///   Ensures that boolean values can be set and retrieved as expected.
     /// </summary>
     [TestMethod]
-    public void GetBoolean() {
+    public void GetBoolean_CorrectValue_IsReturned() {
 
       var topic = TopicFactory.Create("Test", "Container");
 
       topic.Attributes.SetBoolean("IsValue1", true);
       topic.Attributes.SetBoolean("IsValue2", false);
-      topic.Attributes.SetValue("IsValue3", "Invalid");
 
       Assert.IsTrue(topic.Attributes.GetBoolean("IsValue1", false));
       Assert.IsFalse(topic.Attributes.GetBoolean("IsValue2", true));
-      Assert.IsTrue(topic.Attributes.GetBoolean("IsValue3", true));
-      Assert.IsFalse(topic.Attributes.GetBoolean("IsValue3", false));
+
+    }
+
+    /*==========================================================================================================================
+    | TEST: GET BOOLEAN: INCORRECT VALUE: RETURN DEFAULT
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Ensures that invalid values return the default.
+    /// </summary>
+    [TestMethod]
+    public void GetBoolean_IncorrectValue_ReturnDefault() {
+
+      var topic = TopicFactory.Create("Test", "Container");
+
+      topic.Attributes.SetValue("IsValue", "Invalid");
+
+      Assert.IsTrue(topic.Attributes.GetBoolean("IsValue", true));
+      Assert.IsFalse(topic.Attributes.GetBoolean("IsValue", false));
+
+    }
+
+    /*==========================================================================================================================
+    | TEST: GET BOOLEAN: INCORRECT KEY: RETURN DEFAULT
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Ensures that incorrect key names return the default.
+    /// </summary>
+    [TestMethod]
+    public void GetBoolean_IncorrectKey_ReturnDefault() {
+
+      var topic = TopicFactory.Create("Test", "Container");
+
       Assert.IsTrue(topic.Attributes.GetBoolean("InvalidKey", true));
       Assert.IsFalse(topic.Attributes.GetBoolean("InvalidKey", false));
 
     }
 
     /*==========================================================================================================================
-    | TEST: DEFAULT VALUE
-    \-------------------------------------------------------------------------------------------------------------------------*/
-    /// <summary>
-    ///   Creates a new topic and requests an invalid attribute; ensures falls back to the default.
-    /// </summary>
-    [TestMethod]
-    public void DefaultValue() {
-      var topic = TopicFactory.Create("Test", "Container");
-      Assert.AreEqual<string>("Foo", topic.Attributes.GetValue("InvalidAttribute", "Foo"));
-    }
-
-    /*==========================================================================================================================
-    | TEST: SET VALUE
+    | TEST: SET VALUE: CORRECT VALUE: IS RETURNED
     \-------------------------------------------------------------------------------------------------------------------------*/
     /// <summary>
     ///   Sets a custom attribute on a topic and ensures it can be retrieved.
     /// </summary>
     [TestMethod]
-    public void SetValue() {
+    public void SetValue_CorrectValue_IsReturned() {
       var topic = TopicFactory.Create("Test", "Container");
       topic.Attributes.SetValue("Foo", "Bar");
       Assert.AreEqual<string>("Bar", topic.Attributes.GetValue("Foo"));
     }
 
     /*==========================================================================================================================
-    | TEST: SET VALUE: IS DIRTY?
+    | TEST: SET VALUE: VALUE CHANGED: IS DIRTY?
     \-------------------------------------------------------------------------------------------------------------------------*/
     /// <summary>
     ///   Modifies the value of a custom attribute on a topic and ensures it is marked as IsDirty.
     /// </summary>
     [TestMethod]
-    public void SetValue_IsDirtyTest() {
+    public void SetValue_ValueChanged_IsDirty() {
 
       var topic = TopicFactory.Create("Test", "Container");
 
       topic.Attributes.SetValue("Foo", "Bar", false);
-      topic.Attributes.SetValue("Fah", "Bar", false);
       topic.Attributes.SetValue("Foo", "Baz");
-      topic.Attributes.SetValue("Fah", "Bar");
 
-      Assert.AreEqual<string>("Baz", topic.Attributes.GetValue("Foo"));
-      Assert.AreEqual<string>("Bar", topic.Attributes.GetValue("Fah"));
-      Assert.AreEqual<bool>(true, topic.Attributes["Foo"].IsDirty);
-      Assert.AreEqual<bool>(false, topic.Attributes["Fah"].IsDirty);
+      Assert.IsTrue(topic.Attributes["Foo"].IsDirty);
 
     }
 
     /*==========================================================================================================================
-    | TEST: SET VALUE BACKDOOR
+    | TEST: SET VALUE: VALUE UNCHANGED: IS NOT DIRTY?
     \-------------------------------------------------------------------------------------------------------------------------*/
     /// <summary>
-    ///   Sets a custom attribute on a topic by directly adding an <see cref="AttributeValue"/> instance; ensures it can be
-    ///   retrieved.
+    ///   Sets the value of a custom attribute to the existing value and ensures it is <i>not</i> marked as IsDirty.
     /// </summary>
     [TestMethod]
-    public void SetValue_BackdoorTest() {
+    public void SetValue_ValueUnchanged_IsNotDirty() {
 
       var topic = TopicFactory.Create("Test", "Container");
 
-      topic.Attributes.SetValue("Key", "NewKey");
-      topic.Attributes.SetValue("Foo", "Bar");
+      topic.Attributes.SetValue("Fah", "Bar", false);
+      topic.Attributes.SetValue("Fah", "Bar");
 
-      Assert.AreEqual<string>("NewKey", topic.Key);
-      Assert.AreEqual<string>("Bar", topic.Attributes.GetValue("Foo"));
+      Assert.IsFalse(topic.Attributes["Fah"].IsDirty);
 
     }
 
     /*==========================================================================================================================
-    | TEST: ENFORCE BUSINESS LOGIC
+    | TEST: SET VALUE: INVALID VALUE: THROWS EXCEPTION
     \-------------------------------------------------------------------------------------------------------------------------*/
     /// <summary>
     ///   Attempts to violate the business logic by bypassing the property setter; ensures that business logic is enforced.
@@ -181,13 +262,32 @@ namespace OnTopic.Tests {
       typeof(TargetInvocationException),
       "The topic allowed a key to be set via a back door, without routing it through the Key property."
     )]
-    public void EnforceBusinessLogic() {
+    public void SetValue_InvalidValue_ThrowsException() {
       var topic = TopicFactory.Create("Test", "Container");
       topic.Attributes.SetValue("Key", "# ?");
     }
 
     /*==========================================================================================================================
-    | TEST: ENFORCE BUSINESS LOGIC BACKDOOR
+    | TEST: ADD: VALID ATTRIBUTE VALUE: IS RETURNED
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Sets a custom attribute on a topic by directly adding an <see cref="AttributeValue"/> instance; ensures it can be
+    ///   retrieved.
+    /// </summary>
+    [TestMethod]
+    public void Add_ValidAttributeValue_IsReturned() {
+
+      var topic = TopicFactory.Create("Test", "Container");
+
+      topic.Attributes.Remove("Key");
+      topic.Attributes.Add(new AttributeValue("Key", "NewKey", false));
+
+      Assert.AreEqual<string>("NewKey", topic.Key);
+
+    }
+
+    /*==========================================================================================================================
+    | TEST: SET VALUE: INSERT INVALID ATTRIBUTE VALUE: THROWS EXCEPTION
     \-------------------------------------------------------------------------------------------------------------------------*/
     /// <summary>
     ///   Attempts to violate the business logic by bypassing SetValue() entirely; ensures that business logic is enforced.
@@ -197,20 +297,20 @@ namespace OnTopic.Tests {
       typeof(TargetInvocationException),
       "The topic allowed a key to be set via a back door, without routing it through the Key property."
     )]
-    public void EnforceBusinessLogic_Backdoor() {
+    public void Add_InvalidAttributeValue_ThrowsException() {
       var topic = TopicFactory.Create("Test", "Container");
       topic.Attributes.Remove("Key");
       topic.Attributes.Add(new AttributeValue("Key", "# ?"));
     }
 
     /*==========================================================================================================================
-    | TEST: INHERIT FROM PARENT
+    | TEST: GET VALUE: INHERIT FROM PARENT: RETURNS PARENT VALUE
     \-------------------------------------------------------------------------------------------------------------------------*/
     /// <summary>
     ///   Sets an attribute on the parent of a topic and ensures it can be retrieved using inheritance.
     /// </summary>
     [TestMethod]
-    public void InheritFromParent() {
+    public void GetValue_InheritFromParent_ReturnsParentValue() {
 
       var topics = new Topic[8];
 
@@ -229,13 +329,36 @@ namespace OnTopic.Tests {
     }
 
     /*==========================================================================================================================
-    | TEST: MAX HOPS
+    | TEST: GET VALUE: INHERIT FROM DERIVED: RETURNS DERIVED VALUE
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Establishes a long tree of derives topics, and ensures that the derived value is returned.
+    /// </summary>
+    [TestMethod]
+    public void GetValue_InheritFromDerived_ReturnsDerivedValue() {
+
+      var topics = new Topic[5];
+
+      for (var i = 0; i <= 4; i++) {
+        var topic = TopicFactory.Create("Topic" + i, "Container");
+        if (i > 0) topics[i - 1].DerivedTopic = topic;
+        topics[i] = topic;
+      }
+
+      topics[4].Attributes.SetValue("Foo", "Bar");
+
+      Assert.AreEqual<string>("Bar", topics[0].Attributes.GetValue("Foo", null, true, true));
+
+    }
+
+    /*==========================================================================================================================
+    | TEST: GET VALUE: EXCEEDS MAX HOPS: RETURNS DEFAULT
     \-------------------------------------------------------------------------------------------------------------------------*/
     /// <summary>
     ///   Establishes a long tree of derives topics, and ensures that inheritance will pursue no more than five hops.
     /// </summary>
     [TestMethod]
-    public void MaxHops() {
+    public void GetValue_ExceedsMaxHops_ReturnsDefault() {
 
       var topics = new Topic[8];
 
@@ -247,10 +370,7 @@ namespace OnTopic.Tests {
 
       topics[7].Attributes.SetValue("Foo", "Bar");
 
-      Assert.IsNull(topics[0].Attributes.GetValue("Foo", null, true, false));
-      Assert.AreEqual<string>("Bar", topics[2].Attributes.GetValue("Foo", null, true, true));
-      Assert.AreNotEqual<string>("Bar", topics[1].Attributes.GetValue("Foo", null, true, true));
-      Assert.AreNotEqual<string>("Bar", topics[0].Attributes.GetValue("Foo", null, true, true));
+      Assert.IsNull(topics[0].Attributes.GetValue("Foo", null, true, true));
 
     }
 
