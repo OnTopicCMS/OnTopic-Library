cfa8fd34ae610cbc51ee8d1c2c8b0b09bc45a6d9
JeremyCaney <jeremy.caney@ignia.com>
2020-05-02 18:01:14 -0700

    Established new `TopicQueryingTests`
    Previously, unit tests for the `OnTopic.Querying.TopicExtensions` class were placed in the `TopicTest` class. This made good sense as we only had a single unit test and it related to the `Topic` class. As we prepare to expand the `TopicExtensions` class, however, there's value to separating these out so that the extension methods are independently validated from the main `Topic` class.
    
    To kick this off, the existing `FindAllByAttribute_ReturnsCorrectTopics` unit test is being moved from `TopicTest` to `TopicQueryingTest`. In the near future, additional tests will be introduced, as new features are introduced.

---
+++ OnTopic.Tests/TopicQueryingTest.cs
@@ -0,0 +1,48 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Topics Library
+\=============================================================================================================================*/
+using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OnTopic.Querying;
+
+namespace OnTopic.Tests {
+
+  /*============================================================================================================================
+  | CLASS: TOPIC QUERYING TEST
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Provides unit tests for the <see cref="TopicExtensions"/> class.
+  /// </summary>
+  [TestClass]
+  public class TopicQueryingTest {
+
+    /*==========================================================================================================================
+    | TEST: FIND ALL BY ATTRIBUTE: RETURNS CORRECT TOPICS
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Looks for a deeply nested child topic using only the attribute value.
+    /// </summary>
+    [TestMethod]
+    public void FindAllByAttribute_ReturnsCorrectTopics() {
+
+      var parentTopic           = TopicFactory.Create("ParentTopic", "Page", 1);
+      var childTopic            = TopicFactory.Create("ChildTopic", "Page", 5, parentTopic);
+      var grandChildTopic       = TopicFactory.Create("GrandChildTopic", "Page", 20, childTopic);
+      var grandNieceTopic       = TopicFactory.Create("GrandNieceTopic", "Page", 3, childTopic);
+      var greatGrandChildTopic  = TopicFactory.Create("GreatGrandChildTopic", "Page", 7, grandChildTopic);
+
+      grandChildTopic.Attributes.SetValue("Foo", "Baz");
+      greatGrandChildTopic.Attributes.SetValue("Foo", "Bar");
+      grandNieceTopic.Attributes.SetValue("Foo", "Bar");
+
+      Assert.ReferenceEquals(parentTopic.FindAllByAttribute("Foo", "Bar").First(), grandNieceTopic);
+      Assert.AreEqual<int>(2, parentTopic.FindAllByAttribute("Foo", "Bar").Count);
+      Assert.ReferenceEquals(parentTopic.FindAllByAttribute("Foo", "Baz").First(), grandChildTopic);
+
+    }
+
+  } //Class
+} //Namespace
