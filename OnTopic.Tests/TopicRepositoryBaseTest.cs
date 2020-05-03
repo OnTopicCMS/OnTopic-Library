477b628efcbaca7c9d19adffea44d5823fed7c66
JeremyCaney <jeremy.caney@ignia.com>
2020-05-02 19:30:00 -0700

    Established new `TopicRepositoryBaseTest`
    Previously, unit tests for the `OnTopic.Repositories.TopicRepositoryBase` class were placed in the `ITopicRepositoryTest` class. This made reasonable sense as the `TopicRepositoryBase` is an `abstract` class, and almost all of the functionality needs to be tested through a concrete implementation such as `StubTopicRepository` anyway.
    
    As we prepare to expand the `TopicRepositoryBase` class, however, there's value to separating these out so that the methods can be independently validated. These are still dependent on a concrete implementation, but that work can be done through simple pass-through methods.
    
    As some tests will still benefit from having prepackaged data to work with, we'll continue to operate against the `StubTopicRepository`—as opposed to the simpler `DummyTopicRepository`—though for many tests we'll just be relying on minimal pass-through methods. In fact, for most cases, we'll be exposing "proxy" methods to publicly expose the underlying `protected` methods with no intermediary business logic.

---
+++ OnTopic.Tests/TopicRepositoryBaseTest.cs
@@ -0,0 +1,71 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Topics Library
+\=============================================================================================================================*/
+using System;
+using System.Linq;
+using OnTopic.Data.Caching;
+using OnTopic.Metadata;
+using OnTopic.Repositories;
+using OnTopic.TestDoubles;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OnTopic.Metadata.AttributeTypes;
+using OnTopic.Attributes;
+
+namespace OnTopic.Tests {
+
+  /*============================================================================================================================
+  | CLASS: TOPIC REPOSITORY BASE TESTS
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Provides unit tests for the <see cref="TopicRepositoryBase"/> class.
+  /// </summary>
+  /// <remarks>
+  ///   These tests evaluate features that are specific to the <see cref="TopicRepositoryBase"/> class.
+  /// </remarks>
+  [TestClass]
+  public class TopicRepositoryBaseTest {
+
+    /*==========================================================================================================================
+    | PRIVATE VARIABLES
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    readonly                    StubTopicRepository             _topicRepository;
+
+    /*==========================================================================================================================
+    | CONSTRUCTOR
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Initializes a new instance of the <see cref="TopicRepositoryBaseTest"/> with shared resources.
+    /// </summary>
+    /// <remarks>
+    ///   This uses the <see cref="StubTopicRepository"/> to provide data, and then <see cref="CachedTopicRepository"/> to
+    ///   manage the in-memory representation of the data. While this introduces some overhead to the tests, the latter is a
+    ///   relatively lightweight façade to any <see cref="ITopicRepository"/>, and prevents the need to duplicate logic for
+    ///   crawling the object graph.
+    /// </remarks>
+    public TopicRepositoryBaseTest() {
+      _topicRepository = new StubTopicRepository();
+    }
+
+    /*==========================================================================================================================
+    | TEST: GET CONTENT TYPE DESCRIPTORS: RETURNS CONTENT TYPES
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Retrieves a list of <see cref="ContentTypeDescriptor"/>s from the <see cref="ITopicRepository"/> and ensures that
+    ///   the expected number (2) are present.
+    /// </summary>
+    [TestMethod]
+    public void GetContentTypeDescriptors_ReturnsContentTypes() {
+
+      var contentTypes          = _topicRepository.GetContentTypeDescriptors();
+
+      Assert.AreEqual<int>(15, contentTypes.Count);
+      Assert.IsNotNull(contentTypes.GetTopic("ContentTypeDescriptor"));
+      Assert.IsNotNull(contentTypes.GetTopic("Page"));
+      Assert.IsNotNull(contentTypes.GetTopic("LookupListItem"));
+
+    }
+
+  } //Class
+} //Namespace
