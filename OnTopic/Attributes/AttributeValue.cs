aa40acd4a45d0bc56236e2f8ddc0eac2e59cd581
JeremyCaney <jeremy.caney@ignia.com>
2020-05-26 15:58:44 -0700

    Introduce new `IsExtendedAttribute` property to `AttributeValue`
    The object model doesn't care whether or not an `AttributeValue` was loaded from an `IsExtendedAttribute` repository store or not. Nevertheless, keeping track of this is useful for if the attribute is `Save()`d, as it allows us to detect potential mismatches between where the attribute _was_ saved, and where it is _supposed_ to be saved. As such, tracking this will enable us—in a future commit!—to detect cases where an attribute value hasn't changed (i.e., `!IsDirty`) _but_ the attribute should be saved _anyway_ since its _configured storage location_ (from `AttributeDescriptor.IsExtendedAttribute`) has changed since the attribute value was last saved.

--- OnTopic/Attributes/AttributeValue.cs
+++ OnTopic/Attributes/AttributeValue.cs
@@ -98,6 +98,7 @@ namespace OnTopic.Attributes {
     ///   populating the topic graph from a persistent data store as a means of indicating the current version for each
     ///   attribute. This is used when e.g. importing values to determine if the existing value is newer than the source value.
     /// </param>
+    /// <param name="isExtendedAttribute">Determines if the attribute originated from an extended attributes data store.</param>
     /// <requires
     ///   description="The key must be specified for the key/value pair." exception="T:System.ArgumentNullException">
     ///   !String.IsNullOrWhiteSpace(key)
@@ -107,7 +108,8 @@ namespace OnTopic.Attributes {
       string? value,
       bool isDirty,
       bool enforceBusinessLogic,
-      DateTime? lastModified = null
+      DateTime? lastModified = null,
+      bool? isExtendedAttribute = null
     ): this(
       key,
       value,
@@ -115,6 +117,7 @@ namespace OnTopic.Attributes {
     ) {
       EnforceBusinessLogic      = enforceBusinessLogic;
       LastModified              = lastModified?? DateTime.Now;
+      IsExtendedAttribute       = isExtendedAttribute;
     }
 
     /*==========================================================================================================================
@@ -190,6 +193,37 @@ namespace OnTopic.Attributes {
     ///   Read-only reference to the last DateTime the <see cref="AttributeValue"/> instance was updated.
     /// </summary>
     public DateTime LastModified { get; internal set; } = DateTime.Now;
+
+    /*==========================================================================================================================
+    | PROPERTY: IS EXTENDED ATTRIBUTE
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Determines if this attribute originated from a data store as an extended attribute.
+    /// </summary>
+    /// <remarks>
+    ///   <para>
+    ///     How an attribute is stored in the underlying repository doesn't impact how the attribute is treated as part of the
+    ///     object model. By tracking this, however, OnTopic is able to evaluate configuration mismatches during <see
+    ///     cref="ITopicRepository.Save"/>. This allows the <see cref="ITopicRepository"/> to effective handle scenarios where
+    ///     the configuration for an <see cref="AttributeDescriptor"/> has changed prior to the last time a <see cref="Topic"/>
+    ///     was saved, and thus change the location where it is stored.
+    ///   </para>
+    ///   <para>
+    ///     This is important because, otherwise, <see cref="ITopicRepository"/> implementations rely primarily on <see
+    ///     cref="IsDirty"/> to determine if a value should be saved. If an attribute's value hasn't changed, but the location
+    ///     it should be stored has, that could potentially result in the attribute being deleted, as the attribute won't show
+    ///     up for when <see cref="TopicRepositoryBase.GetAttributes"/> is called with <c>isDirty</c> set to <c>true</c> and
+    ///     <c>isExtendedAttribute</c> is set to either <c>true</c> or <c>false</c>. By introducing <see
+    ///     cref="IsExtendedAttribute"/>, the <see cref="TopicRepositoryBase"/> is able to detect conflicts between the
+    ///     configuration and the underlying data store, and ensure data is stored appropriately.
+    ///   </para>
+    ///   <para>
+    ///     The <see cref="IsExtendedAttribute"/> property maps to the <see cref="AttributeDescriptor.IsExtendedAttribute"/>
+    ///     property. The former describes where the data was <i>actually</i> stored, whereas the latter describes where the
+    ///     data <i>should</i> be stored.
+    ///   </para>
+    /// </remarks>
+    public bool? IsExtendedAttribute { get; internal set; }
 
   } //Class
 } //Namespace
