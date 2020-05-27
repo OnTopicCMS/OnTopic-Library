476269468e186483d138b130f910f5f902ab66ba
JeremyCaney <jeremy.caney@ignia.com>
2020-05-27 13:50:49 -0700

    Introduced `IsDirty` flag to `NamedTopicCollection`
    The `IsDirty` flag tracks whether or not the collection has been modified in a meaningful way. By default, this value is set by overriding protected methods for `InsertItem()`, `SetItem()`, `RemoveItem()`, and `ClearItems()`, which covers underlying methods such as `Add()`, `Remove()`, &c. from a variety of enty points.
    
    The `IsDirty` flag can be reset by any external agent. For instance, an `ITopicRepository` can populate the relationships, then reset `IsDirty` to `false` to indicate that this was part of the preloading process.
    
    Attempts are made to eliminate false positives. If a `topic.Key` already exists, for instance, when calling `InsertItem()` or `SetItem()`, the `IsDirty` flag won't be enabled. In these cases, an `ArgumentException` will likely be thrown due to a duplicate key, but even if that's handled by the caller we don't want `IsDirty` to be set. Likewise, if `ClearItems()` is called, it first checks to confirm that any topics exist.
    
    Includes a set of unit tests for evaluating various scenarios—including potential sources of false positives, such as inserting duplicates, clearing empty collections, and removing items that weren't already in the collection.

--- OnTopic/Collections/NamedTopicCollection.cs
+++ OnTopic/Collections/NamedTopicCollection.cs
@@ -6,6 +6,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using OnTopic.Internal.Diagnostics;
 
 namespace OnTopic.Collections {
 
@@ -31,6 +32,66 @@ namespace OnTopic.Collections {
       if (topics != null) {
         CopyTo(topics.ToArray(), 0);
       }
+    }
+
+    /*==========================================================================================================================
+    | IS DIRTY?
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Determines if the collection has been modified. This value is set to <c>true</c> any time a new item is inserted or
+    ///   removed from the collection.
+    /// </summary>
+    public bool IsDirty { get; set; } = false;
+
+    /*==========================================================================================================================
+    | INSERT ITEM
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   When inserting an item, determine if it will change the collection; if it will, mark the collection as <see
+    ///   cref="IsDirty"/>.
+    /// </summary>
+    protected override void InsertItem(int index, Topic topic) {
+      Contract.Requires(index, nameof(index));
+      Contract.Requires(topic, nameof(topic));
+      IsDirty = IsDirty || !Contains(topic.Key);
+      base.InsertItem(index, topic);
+    }
+
+    /*==========================================================================================================================
+    | SET ITEM
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   When updating an existing item, determine if it will change the collection; if it will, mark the collection as <see
+    ///   cref="IsDirty"/>.
+    /// </summary>
+    protected override void SetItem(int index, Topic topic) {
+      Contract.Requires(index, nameof(index));
+      Contract.Requires(topic, nameof(topic));
+      IsDirty = IsDirty || !Contains(topic.Key);
+      base.SetItem(index, topic);
+    }
+
+    /*==========================================================================================================================
+    | REMOVE ITEM
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   When removing an item from the collection, mark the collection as <see cref="IsDirty"/>.
+    /// </summary>
+    protected override void RemoveItem(int index) {
+      Contract.Requires(index, nameof(index));
+      IsDirty = IsDirty || index < Count;
+      base.RemoveItem(index);
+    }
+
+    /*==========================================================================================================================
+    | CLEAR ITEMS
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   When clearing the collection, mark the collection as <see cref="IsDirty"/> if it had items in it.
+    /// </summary>
+    protected override void ClearItems() {
+      IsDirty = IsDirty || Count > 0;
+      base.ClearItems();
     }
 
     /*==========================================================================================================================
