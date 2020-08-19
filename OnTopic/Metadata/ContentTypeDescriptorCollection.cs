3e3930f6ab5942459c87867e3e933e4197d6943e
JeremyCaney <jeremy.caney@ignia.com>
2020-08-19 00:44:05 -0700

    Introduce `ContentTypeDescriptorCollection.Refresh()` method
    The `Refresh()` method makes it easier for the `ContentTypeDescriptionCollection` to be used as a cache for the flatterned version of the `ContentTypeDescriptor`s used for the `ITopicRepository.GetContentTypes()` cache—which is it's primary use case today. Specifically, the `Refresh()` method allows callers to send a `rootContentType` (i.e., `Root:Configuration:ContentTypes`) to the `Refresh()` method in order to update the cached values.

--- OnTopic/Metadata/ContentTypeDescriptorCollection.cs
+++ OnTopic/Metadata/ContentTypeDescriptorCollection.cs
@@ -3,7 +3,12 @@
 | Client        Ignia, LLC
 | Project       Topics Library
 \=============================================================================================================================*/
+using System;
+using System.Diagnostics.Contracts;
+using System.Linq;
 using OnTopic.Collections;
+using OnTopic.Querying;
+using OnTopic.Repositories;
 
 namespace OnTopic.Metadata {
 
@@ -13,6 +18,13 @@ namespace OnTopic.Metadata {
   /// <summary>
   ///   Represents a collection of <see cref="ContentTypeDescriptor"/> objects.
   /// </summary>
+  /// <remarks>
+  ///   While the <see cref="ContentTypeDescriptorCollection"/> can be used to store any collection of <see cref=
+  ///   "ContentTypeDescriptor"/> objects, it has additional tooling in the form of the <see cref="Refresh(
+  ///   ContentTypeDescriptor?)"/> as well as the <see cref="ContentTypeDescriptorCollection(ContentTypeDescriptor?)"/>
+  ///   constructor for handling a flattened list of <see cref="ContentTypeDescriptor"/>s used for the <see cref=
+  ///   "ITopicRepository.GetContentTypeDescriptors()"/> method.
+  /// </remarks>
   public class ContentTypeDescriptorCollection : TopicCollection<ContentTypeDescriptor> {
 
     /*==========================================================================================================================
@@ -22,6 +34,40 @@ namespace OnTopic.Metadata {
     ///   Initializes a new instance of the <see cref="ContentTypeDescriptorCollection"/> class.
     /// </summary>
     public ContentTypeDescriptorCollection() : base(null) {
+    }
+
+    /*==========================================================================================================================
+    | REFRESH
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Updates the current instance of the <see cref="ContentTypeDescriptorCollection"/> based on a new root <see cref=
+    ///   "ContentTypeDescriptor"/>.
+    /// </summary>
+    /// <param name="rootContentType">The <see cref="ContentTypeDescriptor"/> from which to initialize the collection.</param>
+    public void Refresh(ContentTypeDescriptor? rootContentType) {
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Validate parameters
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      if (rootContentType == null || rootContentType.Children.Count == 0) {
+        return;
+      }
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Clear any existing values
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      Clear();
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Add all ContentTypeDescriptors to collection
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      var contentTypeDescriptors = rootContentType
+        .FindAll(t => typeof(ContentTypeDescriptor).IsAssignableFrom(t.GetType()))
+        .Cast<ContentTypeDescriptor>();
+      foreach (var contentType in contentTypeDescriptors) {
+        Add((ContentTypeDescriptor)contentType);
+      }
+
     }
 
   } //Class
