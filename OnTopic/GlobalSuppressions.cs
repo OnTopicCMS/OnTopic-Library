88801a9f131cd777c4ba9f5eef0c50590f836810
JeremyCaney <jeremy.caney@ignia.com>
2020-08-31 17:57:58 -0700

    Suppress `CA2249` globally
    `CA2249` suggests "Consider using 'string.Contains' instead of 'string.IndexOf'". This is good advice. Unfortunately, since we're (currently) targeting .NET Standard 2.0, however, we can't use `Contains()` because it doesn't have an overload supporting `StringComparison`, and is thus not globalization friendly.
    
    (We could implement `Contains()` without `StringComparison`, but that will correctly trigger a different warning.)
    
    We can reevaluate this once we migrate to .NET 5.0 with OnTopic 5.0.0, as then we will no longer be supporting .NET Standard.

--- OnTopic/GlobalSuppressions.cs
+++ OnTopic/GlobalSuppressions.cs
@@ -6,3 +6,4 @@
 using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("Globalization", "CA1307:Specify StringComparison", Justification = "Invalid overload; known bug in code analysis", Scope = "member", Target = "~M:OnTopic.Topic.GetWebPath~System.String")]
+[assembly: SuppressMessage("Usage", "CA2249:Consider using 'string.Contains' instead of 'string.IndexOf'", Justification = "StringComparison overload not supported by .NET Standard 2.0.", Scope = "member", Target = "~M:OnTopic.Querying.TopicExtensions.FindAllByAttribute(OnTopic.Topic,System.String,System.String)~OnTopic.Collections.ReadOnlyTopicCollection{OnTopic.Topic}")]
