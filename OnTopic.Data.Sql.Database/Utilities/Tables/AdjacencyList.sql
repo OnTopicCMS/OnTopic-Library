17b98b4b32653b467134dd092e89915b372a09e8
JeremyCaney <jeremy.caney@ignia.com>
2020-09-09 11:03:31 -0700

    Moved legacy `Hierarchy` to `Utilities` schema, renamed `AdjacencyList`
    While `Hierarchy` is a preexisting table, and thus this is a breaking change, it's not a table that is publicly documented, and it's exclusively used by the `GenerateNestedSet` stored procedure—which is _also_ a preexisting object which was moved to the `Utilities` schema as part of a minor release under the same justification.
    
    As with other similar updates (e.g., e565f0e), this helps keep the `dbo` schema focused on items which are publicly documented and expected to be used directly as part of the library.

--- OnTopic.Data.Sql.Database/Utilities/Tables/AdjacencyList.sql
+++ OnTopic.Data.Sql.Database/Utilities/Tables/AdjacencyList.sql
@@ -1,12 +1,12 @@
 ﻿--------------------------------------------------------------------------------------------------------------------------------
--- HIERARCHY (TABLE)
+-- ADJACENCY LIST (TABLE)
 --------------------------------------------------------------------------------------------------------------------------------
 -- Provides temporary storage for representing the topic hierarchy as an adjacency list. This isn't the preferred format for
 -- representing the topic hierarchy, and is not used in production code. Nevertheless, having this available is useful for
 -- processing migrations from other data formats, or rebuilding the nested set hierarchy should it become corrupted.
 --------------------------------------------------------------------------------------------------------------------------------
 CREATE
-TABLE	[dbo].[Hierarchy] (
+TABLE	[Utilities].[AdjacencyList] (
 	  [TopicID]		INT	NOT NULL,
 	  [Parent_TopicID]	INT	NULL,
 	  [DateAdded]		DATETIME	NOT NULL,
