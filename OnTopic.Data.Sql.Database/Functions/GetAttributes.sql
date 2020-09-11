aa040efb679aff6a9f1940f6c69dc00f26652eef
JeremyCaney <jeremy.caney@ignia.com>
2020-09-11 15:33:07 -0700

    Refactored `GetAttributes` as user-defined, table-valued function
    This doesn't impact backward compatibility, in that the function can still be called as a stored procedure. But it provides more flexibility in how the code is integrated with queries. For instance, it can now be used in a `CROSS APPLY` to retrieve attributes from `TopicID`s in another query.

---
+++ OnTopic.Data.Sql.Database/Functions/GetAttributes.sql
@@ -0,0 +1,65 @@
+﻿--------------------------------------------------------------------------------------------------------------------------------
+-- GET ATTRIBUTES
+--------------------------------------------------------------------------------------------------------------------------------
+-- Returns the most recent value of each attribute associated with a particular topic.
+--------------------------------------------------------------------------------------------------------------------------------
+
+CREATE
+FUNCTION	[dbo].[GetAttributes] (
+	@TopicID		INT	= -1
+)
+RETURNS	@Attributes		TABLE
+(
+	AttributeKey		NVARCHAR(255)	NOT NULL,
+	AttributeValue		NVARCHAR(MAX)	NOT NULL,
+	IsExtendedAttribute	BIT,
+	Version		DATETIME
+)
+AS
+
+BEGIN
+
+  --------------------------------------------------------------------------------------------------------------------------------
+  -- SETUP INSERT
+  --------------------------------------------------------------------------------------------------------------------------------
+  INSERT
+  INTO	@Attributes
+
+  --------------------------------------------------------------------------------------------------------------------------------
+  -- SELECT MOST RECENT ATTRIBUTES
+  --------------------------------------------------------------------------------------------------------------------------------
+  SELECT	AttributeKey,
+	AttributeValue,
+	0		AS IsExtendedAttribute,
+	Version
+  FROM	AttributeIndex
+  WHERE	TopicID		= @TopicID
+
+  UNION
+
+  --------------------------------------------------------------------------------------------------------------------------------
+  -- PARSE MOST RECENT EXTENDED ATTRIBUTES
+  --------------------------------------------------------------------------------------------------------------------------------
+  SELECT	Attributes.Loc.value(
+	  '@key',
+	  'VARCHAR(128)'
+	) AS		AttributeKey,
+	Attributes.Loc.value(
+	  '.[1]',
+	  'VARCHAR(MAX)'
+	) AS		AttributeValue,
+	1		AS IsExtendedAttribute,
+	Version
+  FROM	ExtendedAttributeIndex
+  CROSS APPLY	AttributesXml.nodes(
+	  '/attributes/attribute'
+	) AS		Attributes(Loc)
+  WHERE	TopicID		= @TopicID
+  ORDER BY	AttributeKey
+
+  ------------------------------------------------------------------------------------------------------------------------------
+  -- RETURN
+  ------------------------------------------------------------------------------------------------------------------------------
+  RETURN
+
+END
