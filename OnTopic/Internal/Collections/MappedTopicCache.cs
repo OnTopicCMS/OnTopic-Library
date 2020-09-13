d027d35cc2ef86bc11a190de08046293249d8860
JeremyCaney <jeremy.caney@ignia.com>
2020-09-13 16:25:13 -0700

    Introduced `MappedTopicCache` and `MappedTopicCacheEntry`
    Currently, a `ConcurrentDictionary<int, object>` is declared in the `TopicMappingService`. As we want to expand this to also track what `Relationships` flags were used to map the currently cached object, we're simplifying this by establishing a new `MappedTopicCache` object, which tracks a collection of `MappedTopicCacheEntry` objects. A `MappedTopicCacheEntry` provides access to both the `MappedTopic`—similar to the previous cache collection—as well as a `Relationships` property. This way, when an object from the cache is retrieved, it can be determined what relationships have already been mapped.

---
+++ OnTopic/Internal/Collections/MappedTopicCache.cs
@@ -0,0 +1,22 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Topics Library
+\=============================================================================================================================*/
+using System.Collections.Concurrent;
+using OnTopic.Mapping;
+using OnTopic.Internal.Mapping;
+
+namespace OnTopic.Internal.Collections {
+
+  /*============================================================================================================================
+  | CLASS: MAPPED TOPIC CACHE
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Provides a collection intended to track local caching of objects mapped using the <see cref="TopicMappingService"/>.
+  /// </summary>
+  public class MappedTopicCache: ConcurrentDictionary<int, MappedTopicCacheEntry> {
+
+
+  } //Class
+} //Namespace
---
+++ OnTopic/Internal/Mapping/MappedTopicCacheEntry.cs
@@ -0,0 +1,40 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Topics Library
+\=============================================================================================================================*/
+using OnTopic.Mapping;
+using OnTopic.Mapping.Annotations;
+
+namespace OnTopic.Internal.Mapping {
+
+  /*============================================================================================================================
+  | CLASS: MAPPED TOPIC CACHE ENTRY
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Provides an entry to tracking an object mapped using the <see cref="TopicMappingService"/>.
+  /// </summary>
+  /// <remarks>
+  ///   In addition to the actual <see cref="MappedTopic"/>, this also includes a <see cref="Relationships"/> property for
+  ///   tracking what relationships were mapped to the <see cref="MappedTopic"/>.
+  /// </remarks>
+  public class MappedTopicCacheEntry {
+
+    /*==========================================================================================================================
+    | PROPERTY: MAPPED TOPIC
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Provides a reference to the mapped object.
+    /// </summary>
+    public object MappedTopic { get; set; } = null!;
+
+    /*==========================================================================================================================
+    | PROPERTY: RELATIONSHIPS
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Provides a reference to the relationships that the <see cref="MappedTopic"/> was mapped with.
+    /// </summary>
+    public Relationships Relationships { get; set; } = Relationships.None;
+
+  } //Class
+} //Namespace
