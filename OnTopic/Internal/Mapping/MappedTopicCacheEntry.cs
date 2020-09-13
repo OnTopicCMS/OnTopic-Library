80452d2bc2f54a7923af25cc209367923a2c2b25
JeremyCaney <jeremy.caney@ignia.com>
2020-09-13 16:34:22 -0700

    Introduced `GetMissingRelationships()` and `AddMissingRelationships()`
    Given a `Relationships` enum, the `GetMissingRelationships()` will determine what relationships, if any, are not covered by the existing `MappedTopicCacheEntry.Relationships` enum, such that the `TopicMappingService` can map those. The `AddMissingRelationships()` updates the `Relationships` to include the new relationships, thus ensuring that the new relationships are accounted for in the cache.

--- OnTopic/Internal/Mapping/MappedTopicCacheEntry.cs
+++ OnTopic/Internal/Mapping/MappedTopicCacheEntry.cs
@@ -16,7 +16,11 @@ namespace OnTopic.Internal.Mapping {
   /// </summary>
   /// <remarks>
   ///   In addition to the actual <see cref="MappedTopic"/>, this also includes a <see cref="Relationships"/> property for
-  ///   tracking what relationships were mapped to the <see cref="MappedTopic"/>.
+  ///   tracking what relationships were mapped to the <see cref="MappedTopic"/>. This allows the <see cref=
+  ///   "TopicMappingService"/> to be update the cached object with any missing relationships, which can be identified using the
+  ///   <see cref="GetMissingRelationships(Relationships)"/> method. In turn, the cache can then be updated to reflect those
+  ///   new relationships by using <see cref="AddMissingRelationships(Relationships)"/>. This ensures that even if a topic has
+  ///   already been mapped, its scope can be expanded without duplicating effort.
   /// </remarks>
   public class MappedTopicCacheEntry {
 
@@ -35,6 +39,24 @@ namespace OnTopic.Internal.Mapping {
     ///   Provides a reference to the relationships that the <see cref="MappedTopic"/> was mapped with.
     /// </summary>
     public Relationships Relationships { get; set; } = Relationships.None;
+
+    /*==========================================================================================================================
+    | METHOD: GET MISSING RELATIONSHIPS
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Given a target <paramref name="relationships"/>, identifies any relationships not covered by <see cref="Relationships"
+    ///   /> and returns them as a new <see cref="OnTopic.Mapping.Annotations.Relationships"/> instance.
+    /// </summary>
+    public Relationships GetMissingRelationships(Relationships relationships) => relationships ^ (relationships | Relationships);
+
+    /*==========================================================================================================================
+    | METHOD: ADD MISSING RELATIONSHIPS
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Given a target <paramref name="relationships"/>, adds any missing <see cref="OnTopic.Mapping.Annotations.
+    ///   Relationships"/> to the <see cref="Relationships"/> property.
+    /// </summary>
+    public void AddMissingRelationships(Relationships relationships) => Relationships = relationships | Relationships;
 
   } //Class
 } //Namespace
