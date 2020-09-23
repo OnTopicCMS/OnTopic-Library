19ed8304fac528edbf85cd7c31dc26b6e37ce836
JeremyCaney <jeremy.caney@ignia.com>
2020-09-23 16:06:10 -0700

    Marked `FromList()` as deprecated
    The `FromList()` method doesn't make much sense. It acts like a constructor method—but it's not static, so it effectively creates a duplicate object. Further, the same functionality is already accounted for by the constructor that accepts an `IList<T>` as a parameter, but which populates the current collection.
    
    It's not entirely clear what the original thinking on this was, but it isn't actually used anywhere within the product or current implementations, and will be removed in the next major release of OnTopic.

--- OnTopic/Collections/ReadOnlyTopicCollection{T}.cs
+++ OnTopic/Collections/ReadOnlyTopicCollection{T}.cs
@@ -3,6 +3,7 @@
 | Client        Ignia, LLC
 | Project       Topics Library
 \=============================================================================================================================*/
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using OnTopic.Internal.Diagnostics;
@@ -58,6 +59,7 @@ namespace OnTopic.Collections {
     ///   The <paramref name="innerCollection"/> will be converted to a <see cref="TopicCollection{T}"/>.
     /// </remarks>
     /// <param name="innerCollection">The underlying <see cref="TopicCollection{T}"/>.</param>
+    [Obsolete("This is effectively satisfied by the related overload, and will be removed in OnTopic 5.0.0.", false)]
     public ReadOnlyTopicCollection<T> FromList(IList<T> innerCollection) {
       Contract.Requires(innerCollection, "innerCollection should not be null");
       return new ReadOnlyTopicCollection<T>(innerCollection);
