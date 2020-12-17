5c8bd067406df3c227c567530f054deb90c81123
JeremyCaney <jeremy.caney@ignia.com>
2020-12-17 14:12:55 -0800

    Operate off of `ITopicBindingModel` type, not `*TopicBindingModel` name
    Initially, the `DynamicTopicBindingModelLookupService` looked for all types whose names ended in `TopicBindingModel`. This is similar to how the `DynamicTopicViewModelLookupService` works.
    
    Binding models are different, however. The `ReverseTopicService` expects binding models to implement the `ITopicBindingModel` interface. As such, we needn't rely on their name to identify their purpose. Further, implementors may wish to use the same classes for view models and binding models; by forcing a naming convention that is at odds with that used with view models, we prevent that from happening.
    
    Given that, the `DyanmicTopicBindingModelLookupService` now looks for classes which implement `ITopicBindingModel`, regardless of whether or not they end with `TopicBindingModel`. In practice, today, we expect these to be identical. But this offers more flexibility to implementors.

--- OnTopic/Reflection/DynamicTopicBindingModelLookupService.cs
+++ OnTopic/Reflection/DynamicTopicBindingModelLookupService.cs
@@ -4,6 +4,7 @@
 | Project       Topics Library
 \=============================================================================================================================*/
 using System;
+using OnTopic.Models;
 
 namespace OnTopic.Reflection {
 
@@ -11,8 +12,8 @@ namespace OnTopic.Reflection {
   | CLASS: TOPIC BINDING MODEL LOOKUP SERVICE
   \---------------------------------------------------------------------------------------------------------------------------*/
   /// <summary>
-  ///   The <see cref="DynamicTopicBindingModelLookupService"/> will search all assemblies for <see cref="Type"/>s that end with
-  ///   "TopicBindingModel"
+  ///   The <see cref="DynamicTopicBindingModelLookupService"/> will search all assemblies for <see cref="Type"/>s that
+  ///   implement <see cref="ITopicBindingModel"/>.
   /// </summary>
   public class DynamicTopicBindingModelLookupService : DynamicTypeLookupService {
 
@@ -23,7 +24,7 @@ namespace OnTopic.Reflection {
     ///   Establishes a new instance of a <see cref="DynamicTopicBindingModelLookupService"/>.
     /// </summary>
     public DynamicTopicBindingModelLookupService() : base(
-      t => t.Name.EndsWith("TopicBindingModel", StringComparison.InvariantCultureIgnoreCase),
+      t => typeof(ITopicBindingModel).IsAssignableFrom(t),
       typeof(object)
     ) { }
 
