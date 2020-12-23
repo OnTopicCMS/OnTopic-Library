112444335b46ea70d5429ed54807440950d620fd
JeremyCaney <jeremy.caney@ignia.com>
2020-12-23 14:01:47 -0800

    Establish basic pre-deployment migration script
    Many of the changes we're introducing can rely on a basic schema comparison. Adding and populating non-nullable fields, however, requires a custom script. This script will add these three core fields—but will temporarily allow `NULL` values—and will populate them with data from the `Attributes` field.
    
    This should be run before the schema comparison. The schema comparison will then mark the `TopicKey` and `ContentType` fields as non-nullable, allowing the schema comparison to run without problems.

---
+++ OnTopic.Data.Sql.Database/Scripts/Upgrade from OnTopic 4 to OnTopic 5.sql
@@ -0,0 +1,50 @@
+﻿--------------------------------------------------------------------------------------------------------------------------------
+-- UPGRADE FROM ONTOPIC 4.x TO ONTOPIC 5.x
+--------------------------------------------------------------------------------------------------------------------------------
+-- There are a few data schema differences that cannot be handled as part of the schema comparison. These should be executed
+-- prior to running migrations.
+--------------------------------------------------------------------------------------------------------------------------------
+
+--------------------------------------------------------------------------------------------------------------------------------
+-- MIGRATE CORE ATTRIBUTES
+--------------------------------------------------------------------------------------------------------------------------------
+-- In OnTopic 5, core attributes that don't utilize versioning have been moved from the Attributes table to the Topics table.
+-- This includes Key, ContentType, and ParentID. Previously, these required a lot of workaround since they frequently utilized
+-- in a way that's inconsistent with other attributes. By moving them to Topic, we better acknowledge their unique status.
+--------------------------------------------------------------------------------------------------------------------------------
+
+ALTER TABLE [dbo].[Topics]
+    ADD [TopicKey]    VARCHAR (128) NULL,
+        [ContentType] VARCHAR (128) NULL,
+        [ParentID]    INT           NULL;
+
+WITH KeyAttributes AS (
+  SELECT	TopicID,
+                AttributeKey,
+                AttributeValue,
+                RowNumber = ROW_NUMBER() OVER (
+                  PARTITION BY		TopicID,
+			AttributeKey
+                  ORDER BY		Version DESC
+                )
+  FROM	[dbo].[Attributes]
+  WHERE	AttributeKey
+  IN (	'Key',
+	'ContentType',
+	'ParentID'
+  )
+)
+UPDATE	Topics
+SET	Topics.TopicKey		= Pvt.[Key],
+	Topics.ContentType	= Pvt.ContentType,
+	Topics.ParentID		= Pvt.ParentID
+FROM	KeyAttributes
+PIVOT (	MIN(AttributeValue)
+  FOR	AttributeKey IN (
+	  [Key],
+	  [ContentType],
+	  [ParentID]
+	)
+)	AS Pvt
+WHERE	RowNumber		= 1
+AND	Topics.TopicID		= Pvt.TopicID
