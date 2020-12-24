bf1685dcac823e5f5a33fc6faeb2e6a55e0b9a73
JeremyCaney <jeremy.caney@ignia.com>
2020-12-23 17:59:53 -0800

    Introduced unit test for ensuring invalid content types are skipped
    Our previous unit test for `SitemapController` didn't confirm that content types in the `ExcludeContentTypes` list (such as `List`) or which are explicitly skipped (such as `Container`) are properly excluded—and vice versa. This unit test addresses that scenario.

--- OnTopic.AspNetCore.Mvc.Tests/TopicControllerTest.cs
+++ OnTopic.AspNetCore.Mvc.Tests/TopicControllerTest.cs
@@ -149,6 +149,44 @@ namespace OnTopic.Tests {
     }
 
     /*==========================================================================================================================
+    | TEST: SITEMAP CONTROLLER: INDEX: EXCLUDES CONTENT TYPES
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Triggers the index action of the <see cref="SitemapController.Index()" /> action and verifies that it properly
+    ///   excludes <c>List</c> content types, and skips over <c>Container</c> and <c>PageGroup</c>.
+    /// </summary>
+    [TestMethod]
+    public void SitemapController_Index_ExcludesContentTypes() {
+
+      var hiddenTopic1          = _topicRepository.Load("Root:Web:Web_1:Web_1_0")!;
+      var hiddenTopic2          = _topicRepository.Load("Root:Web:Web_1:Web_1_1")!;
+
+      hiddenTopic1.ContentType  = "List";
+      hiddenTopic2.ContentType  = "Container";
+
+      var actionContext         = new ActionContext {
+        HttpContext             = new DefaultHttpContext(),
+        RouteData               = new(),
+        ActionDescriptor        = new ControllerActionDescriptor()
+      };
+      var controller            = new SitemapController(_topicRepository) {
+        ControllerContext       = new(actionContext)
+      };
+      var result                = controller.Index(false, true) as ContentResult;
+      var model                 = result.Content as string;
+
+      controller.Dispose();
+
+      Assert.IsNotNull(model);
+      Assert.IsTrue(model.Contains("<DataObject type=\"Page\">"));
+      Assert.IsFalse(model.Contains("<DataObject type=\"List\">"));
+      Assert.IsFalse(model.Contains("<DataObject type=\"Container\">"));
+      Assert.IsTrue(model.Contains("/Web/Web_1/Web_1_1/Web_1_1_0/</loc>"));
+      Assert.IsFalse(model.Contains("/Web/Web_1/Web_1_0/Web_1_0_0/</loc>"));
+
+    }
+
+    /*==========================================================================================================================
     | TEST: SITEMAP CONTROLLER: INDEX: EXCLUDES ATTRIBUTES
     \-------------------------------------------------------------------------------------------------------------------------*/
     /// <summary>
