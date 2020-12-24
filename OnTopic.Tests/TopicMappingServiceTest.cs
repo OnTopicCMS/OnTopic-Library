105cfdde7d67bbba642b4280fcf95bb47f78bb41
JeremyCaney <jeremy.caney@ignia.com>
2020-12-23 16:18:35 -0800

    Introduced new `[FilterByContentType()]` attribute
    Since `ContentType` has been moved from `Topic.Attributes` to `Topic.ContentType`, it can no longer be filtered using the `[FilterByAttribute()]` attribute during mapping (4a9f5b7). That's a big deal since that's the primary use case for the `[FilterByAttribute()]` attribute!
    
    To mitigate that, a new `[FilterByContentType()]` attribute is introduced, read by the `PropertyConfiguration`, and enforced by the `TopicMappingService`. In addition, a unit test was introduced to validate the functionality.
    
    (As part of this, an old unit test had to be renamed as this inadvertantly introduced an ambiguity with the test evaluating a collection filtered based on the collection type.)

--- OnTopic.Tests/TopicMappingServiceTest.cs
+++ OnTopic.Tests/TopicMappingServiceTest.cs
@@ -632,14 +632,14 @@ namespace OnTopic.Tests {
     }
 
     /*==========================================================================================================================
-    | TEST: MAP: FILTER BY CONTENT TYPE: RETURNS FILTERED COLLECTION
+    | TEST: MAP: FILTER BY COLLECTION TYPE: RETURNS FILTERED COLLECTION
     \-------------------------------------------------------------------------------------------------------------------------*/
     /// <summary>
     ///   Establishes a <see cref="TopicMappingService"/> and tests whether the resulting object's <see
     ///   cref="DescendentTopicViewModel.Children"/> property can be filtered by <see cref="TopicViewModel.ContentType"/>.
     /// </summary>
     [TestMethod]
-    public async Task Map_FilterByContentType_ReturnsFilteredCollection() {
+    public async Task Map_FilterByCollectionType_ReturnsFilteredCollection() {
 
       var topic                 = TopicFactory.Create("Test", "Descendent");
       var childTopic1           = TopicFactory.Create("ChildTopic1", "Descendent", topic);
@@ -818,6 +818,29 @@ namespace OnTopic.Tests {
       var target = await _mappingService.MapAsync<FilteredTopicViewModel>(topic).ConfigureAwait(false);
 
       Assert.AreEqual<int>(2, target.Children.Count);
+
+    }
+
+    /*==========================================================================================================================
+    | TEST: MAP: FILTER BY CONTENT TYPE: RETURNS FILTERED COLLECTION
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Establishes a <see cref="TopicMappingService"/> and tests whether the resulting object's <see
+    ///   cref="FilteredTopicViewModel.Children"/> property can be filtered using a <see cref="FilterByContentTypeAttribute"/>
+    ///   instances.
+    /// </summary>
+    [TestMethod]
+    public async Task Map_FilterByContentType_ReturnsFilteredCollection() {
+
+      var topic                 = TopicFactory.Create("Test", "Filtered");
+      var childTopic1           = TopicFactory.Create("ChildTopic1", "Page", topic);
+      var childTopic2           = TopicFactory.Create("ChildTopic2", "Index", topic);
+      var childTopic3           = TopicFactory.Create("ChildTopic3", "Page", topic);
+      var childTopic4           = TopicFactory.Create("ChildTopic4", "Page", childTopic3);
+
+      var target = await _mappingService.MapAsync<FilteredContentTypeTopicViewModel>(topic).ConfigureAwait(false);
+
+      Assert.AreEqual<int>(3, target.Children.Count);
 
     }
 
