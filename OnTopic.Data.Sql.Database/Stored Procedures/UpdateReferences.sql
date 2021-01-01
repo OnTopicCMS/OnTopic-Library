1a62c23f2bacc8e8c756cb1f5abe67bfba50b870
JeremyCaney <jeremy.caney@ignia.com>
2020-12-31 18:26:59 -0800

    Introduced new `UpdateReferences` stored procedure
    Technically, since this is a 1:n relationship, and not an n:n relationship, this logic could be built into e.g. the `UpdateTopic` stored procedure. To maintain consistency with `UpdateRelationships`, however, and to allow it to be tested independently, I'm setting it up as its own stored procedure.
    
    That said, given that there will only be one of these per update, we'll be adding a `@References` parameter to the `CreateTopic` and `UpdateTopic` stored procedures later, so they don't need to call this separately.

---
+++ OnTopic.Data.Sql.Database/Stored Procedures/UpdateReferences.sql
@@ -0,0 +1,62 @@
+﻿--------------------------------------------------------------------------------------------------------------------------------
+-- UPDATE REFERENCES
+--------------------------------------------------------------------------------------------------------------------------------
+-- Saves the 1:1 mappings for referenced topics.
+--------------------------------------------------------------------------------------------------------------------------------
+
+CREATE PROCEDURE [dbo].[UpdateReferences]
+	@TopicID		INT,
+	@ReferencedTopics	TopicReferences	READONLY,
+	@DeleteUnmatched	BIT	= 0
+AS
+
+--------------------------------------------------------------------------------------------------------------------------------
+-- INSERT NOVEL VALUES
+--------------------------------------------------------------------------------------------------------------------------------
+INSERT
+INTO	TopicReferences (
+	  Source_TopicID,
+	  ReferenceKey,
+	  Target_TopicID
+	)
+SELECT	@TopicID,
+	Target.ReferenceKey,
+	Target.TopicID
+FROM	@ReferencedTopics	Target
+LEFT JOIN	TopicReferences		Existing
+  ON	Source_TopicID		= @TopicID
+  AND	Existing.ReferenceKey	= Target.ReferenceKey
+WHERE	ISNULL(Source_TopicID, '')	= ''
+  AND	Target.TopicID		> 0
+
+--------------------------------------------------------------------------------------------------------------------------------
+-- UPDATE EXISTING VALUES
+--------------------------------------------------------------------------------------------------------------------------------
+UPDATE	Existing
+SET	Target_TopicID		= TopicID
+FROM	@ReferencedTopics	Target
+LEFT JOIN	TopicReferences		Existing
+  ON	Source_TopicID		= @TopicID
+  AND	Existing.ReferenceKey	= Target.ReferenceKey
+WHERE	Source_TopicID		IS NOT NULL
+  AND	Target.TopicID		!= Target_TopicID
+  AND	Target.TopicID		> 0
+
+--------------------------------------------------------------------------------------------------------------------------------
+-- DELETE UNMATCHED VALUES
+--------------------------------------------------------------------------------------------------------------------------------
+IF @DeleteUnmatched = 1
+  BEGIN
+    DELETE	Existing
+    FROM	@ReferencedTopics	New
+    RIGHT JOIN	TopicReferences		Existing
+      ON	Source_TopicID		= @TopicID
+      AND	Existing.ReferenceKey	= New.ReferenceKey
+    WHERE	Source_TopicID		= @TopicID
+      AND	ISNULL(TopicID, '')	= ''
+  END
+
+--------------------------------------------------------------------------------------------------------------------------------
+-- RETURN TOPIC ID
+--------------------------------------------------------------------------------------------------------------------------------
+RETURN	@TopicID;
