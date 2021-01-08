fdd67ce84751be836eab91f84c0ce71179181efd
JeremyCaney <jeremy.caney@ignia.com>
2021-01-08 12:59:56 -0800

    Established structure for testing `HierarchicalTopicMappingService`
    This includes establishing a shared `StubTopicRepository` wrapped in a `CachedTopicRepository`, as well as a `TopicMappingService` that can be used across the individual unit tests.

---
+++ OnTopic.Tests/HierarchicalTopicMappingServiceTest.cs
@@ -0,0 +1,76 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Topics Library
+\=============================================================================================================================*/
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OnTopic.Data.Caching;
+using OnTopic.Mapping;
+using OnTopic.Mapping.Hierarchical;
+using OnTopic.Repositories;
+using OnTopic.TestDoubles;
+using OnTopic.ViewModels;
+
+namespace OnTopic.Tests {
+
+  /*============================================================================================================================
+  | CLASS: HIERARCHICAL TOPIC MAPPING SERVICE TEST
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Provides unit tests for the <see cref="HierarchicalTopicMappingService{T}"/>.
+  /// </summary>
+  [TestClass]
+  public class HierarchicalTopicMappingServiceTest {
+
+    /*==========================================================================================================================
+    | PRIVATE VARIABLES
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    readonly                    ITopicRepository                _topicRepository;
+    readonly                    ITopicMappingService            _topicMappingService;
+    readonly                    Topic                           _topic;
+
+    /*==========================================================================================================================
+    | HIERARCHICAL TOPIC MAPPING SERVICE
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    private readonly IHierarchicalTopicMappingService<NavigationTopicViewModel> _hierarchicalMappingService;
+
+    /*==========================================================================================================================
+    | CONSTRUCTOR
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Initializes a new instance of the <see cref="HierarchicalTopicMappingServiceTest"/> with shared resources.
+    /// </summary>
+    /// <remarks>
+    ///   This uses the <see cref="StubTopicRepository"/> to provide data, and then <see cref="CachedTopicRepository"/> to
+    ///   manage the in-memory representation of the data. While this introduces some overhead to the tests, the latter is a
+    ///   relatively lightweight façade to any <see cref="ITopicRepository"/>, and prevents the need to duplicate logic for
+    ///   crawling the object graph. In addition, it initializes a shared <see cref="Topic"/> reference to use for the various
+    ///   tests.
+    /// </remarks>
+    public HierarchicalTopicMappingServiceTest() {
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Establish dependencies
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      _topicRepository          = new CachedTopicRepository(new StubTopicRepository());
+      _topic                    = _topicRepository.Load("Root:Web:Web_3:Web_3_0")!;
+      _topicMappingService      = new TopicMappingService(_topicRepository, new TopicViewModelLookupService());
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Establish hierarchical topic mapping service
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      _hierarchicalMappingService = new CachedHierarchicalTopicMappingService<NavigationTopicViewModel>(
+        new HierarchicalTopicMappingService<NavigationTopicViewModel>(
+          _topicRepository,
+          _topicMappingService
+        )
+      );
+
+    }
+
+
+  } //Class
+} //Namespace
