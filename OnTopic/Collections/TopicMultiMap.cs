d049ea90f3c7775587712050429c78ce0b7106c9
JeremyCaney <jeremy.caney@ignia.com>
2021-01-11 16:24:49 -0800

    Introduced a new `TopicMultiMap` class
    The `TopicMultiMap` is a dictionary—or, technically, a `KeyedCollection`—which provides a 1:n relationship between a given collection `key` and a collection of `Topic` objects.
    
    In addition to the basic features of a `KeyedCollection`, it includes convenience overloads which include not only the `key`—as the base methods do—but also the `Topic` as well. As such, it includes e.g. `Contains(key, topic)`, `Add(key, topic)`, and `Remove(key, topic)`. It also has a `Clear()` overload which will clear one of the target collections based on the collection `key`.
    
    Unlike a `Dictionary<TKey, TValue>`, which uses a `KeyValuePair<TKey, TValue>` object, the `TopicMultiMap` introduces its own `KeyValuesPair<TKey, TValue>` class. This therefore operates very similar to a dictionary, but with more intuitive semantics given that the `Values` property is a collection.
    
    These are intended to support an updated `Topic.Relationships` type, though they're not implemented with any relationship-specific semantics, and are thus stored in the `OnTopic.Collections` namespace to denote that they're intended to be multi-purpose.

---
+++ OnTopic/Collections/TopicMultiMap.cs
@@ -0,0 +1,156 @@
+/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Topics Library
+\=============================================================================================================================*/
+using System;
+using System.Collections.ObjectModel;
+using OnTopic.Internal.Diagnostics;
+
+namespace OnTopic.Collections {
+
+  /*============================================================================================================================
+  | CLASS: TOPIC MULTIMAP
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   The <see cref="TopicMultiMap"/> offers support for a keyed collection where each key is mapped to a collection of <see
+  ///   cref="Topic"/> instances, thus supporting a 1:n relationship with zero or more topics, organized by key.
+  /// </summary>
+  public class TopicMultiMap: KeyedCollection<string, KeyValuesPair<string, Collection<Topic>>> {
+
+    /*==========================================================================================================================
+    | CONSTRUCTOR
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Constructs a new instance of a <see cref="TopicMultiMap"/> class.
+    /// </summary>
+    public TopicMultiMap() {
+
+    }
+
+    /*==========================================================================================================================
+    | METHOD: CONTAINS
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Determines if the <paramref name="topic"/> exists in a collection with the supplied <paramref name="key"/>.
+    /// </summary>
+    /// <returns>
+    ///   Returns <c>true</c> if the <see cref="Topic"/> exists in the specified collection. Returns <c>false</c> if the
+    ///   collection doesn't exist.
+    /// </returns>
+    public bool Contains(string key, Topic topic) => Contains(key) && this[key].Values.Contains(topic);
+
+    /*==========================================================================================================================
+    | METHOD: GET TOPICS
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Retrieves a list of <see cref="Topic"/> objects grouped by a specific <paramref name="key"/>.
+    /// </summary>
+    /// <remarks>
+    ///   Returns a reference to the underlying <see cref="Collection{Topic}"/> collection.
+    /// </remarks>
+    /// <param name="key">The key of the collection to be returned.</param>
+    public Collection<Topic> GetTopics(string key) {
+      Contract.Requires<ArgumentNullException>(!String.IsNullOrWhiteSpace(key), nameof(key));
+      if (Contains(key)) {
+        return this[key].Values;
+      }
+      return new();
+    }
+
+    /*==========================================================================================================================
+    | METHOD: ADD
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Adds a <paramref name="topic"/> to a collection with the supplied <paramref name="key"/>. If the collection with
+    ///   <paramref name="key"/> doesn't exist, it will be established.
+    /// </summary>
+    public void Add(string key, Topic topic) {
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Validate parameters
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      Contract.Requires(key, nameof(key));
+      Contract.Requires(topic, nameof(topic));
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Ensure collection is established
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      if (!Contains(key)) {
+        Add(new(key, new()));
+      }
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Add topic, if it hasn't already been added
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      if (!Contains(key, topic)) {
+        this[key].Values.Add(topic);
+      }
+
+    }
+
+    /*==========================================================================================================================
+    | METHOD: REMOVE
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Removes a <paramref name="topic"/> from a collection with the supplied <paramref name="key"/>.
+    /// </summary>
+    /// <param name="key">The key of the collection.</param>
+    /// <param name="topic">The <see cref="Topic"/> to be removed.</param>
+    /// <returns>
+    ///   Returns <c>true</c> if the <see cref="Topic"/> is removed; returns <c>false</c> if either the <paramref name="key"/>
+    ///   or the <paramref name="topic"/> cannot be found.
+    /// </returns>
+    public bool Remove(string key, Topic topic) {
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Validate contracts
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      Contract.Requires<ArgumentNullException>(!String.IsNullOrWhiteSpace(key), nameof(key));
+      Contract.Requires(topic, nameof(topic));
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Validate key
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      var topics                = GetTopics(key);
+
+      if (topics is null || !topics.Contains(topic)) {
+        return false;
+      }
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Remove relationship
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      topics.Remove(topic);
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Remove true
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      return true;
+
+    }
+
+    /*==========================================================================================================================
+    | METHOD: CLEAR
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Removes all <see cref="Topic"/> objects grouped by a specific <paramref name="key"/>.
+    /// </summary>
+    /// <param name="key">The key of the collection to be cleared.</param>
+    public void Clear(string key) => GetTopics(key).Clear();
+
+    /*==========================================================================================================================
+    | OVERRIDE: GET KEY FOR ITEM
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Method must be overridden for the <see cref="TopicMultiMap"/> to extract the keys from the items.
+    /// </summary>
+    /// <param name="item">The <see cref="KeyValuesPair{TKey, TValue}"/> object from which to extract the key.</param>
+    /// <returns>The key for the specified collection item.</returns>
+    protected override string GetKeyForItem(KeyValuesPair<string, Collection<Topic>> item) {
+      Contract.Requires(item, "The item must be available in order to derive its key.");
+      return item.Key;
+    }
+
+  } //Class
+} //Namespace
