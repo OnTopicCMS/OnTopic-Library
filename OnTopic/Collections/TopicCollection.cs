9b68bb2bb99f3e4b48b739cb49551b75787359bf
JeremyCaney <jeremy.caney@ignia.com>
2021-01-12 15:57:34 -0800

    Introduced new `TopicCollection`
    With the legacy `TopicCollection` renamed to the more descriptive `KeyedTopicCollection`, we can now introduce a new `TopicCollection` class which is _not_ keyed, and is thus suitable for cases where we expect topics from across the topic graph to be represented. It's unclear what, if any, additional members we will want to add to this—though having it helps formalize the return types for the few cases where this is necessary, and works off of a more consistent naming convention.

---
+++ OnTopic/Collections/TopicCollection.cs
@@ -0,0 +1,39 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Topics Library
+\=============================================================================================================================*/
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace OnTopic.Collections {
+
+  /*============================================================================================================================
+  | CLASS: TOPIC COLLECTION
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Represents a collection of <see cref="Topic"/> objects.
+  /// </summary>
+  public class TopicCollection : Collection<Topic> {
+
+    /*==========================================================================================================================
+    | CONSTRUCTOR
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Initializes a new instance of the <see cref="TopicCollection"/>.
+    /// </summary>
+    /// <param name="topics">Seeds the collection with an optional list of topic references.</param>
+    public TopicCollection(IEnumerable<Topic>? topics = null) : base(topics.ToList()) {
+    }
+
+    /*==========================================================================================================================
+    | METHOD: AS READ ONLY
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Retrieves a read-only version of this <see cref="TopicCollection"/>.
+    /// </summary>
+    public ReadOnlyTopicCollection AsReadOnly() => new(this);
+
+  } //Class
+} //Namespace
