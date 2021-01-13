77ca3c935bccc544624997474418dd1881f7b339
JeremyCaney <jeremy.caney@ignia.com>
2021-01-12 16:01:12 -0800

    Introduced new `ReadOnlyTopicCollection`
    With the legacy `ReadOnlyTopicCollection` renamed to the more descriptive `ReadOnlyKeyedTopicCollection`, and the introduction of a new `TopicCollection` class (9b68bb2), we can now introduce a new `ReadOnlyTopicCollection` class which is _not_ keyed, and is thus suitable for cases where we expect topics from across the topic graph to be represented.
    
    It's unclear what, if any, additional members we will want to add to this—though having it helps formalize the return types for the few cases where this is necessary, and works off of a more consistent naming convention.
    
    Note: This class is not generic as we don't anticipate much use of topic derivatives moving forward—and in the few cases that we do, we expect they'll want to be keyed. For instance, the `ContentTypeDescriptorCollection` and `AttributeDescriptorCollection` both operate off of `Topic` derivatives, and are intended to have unique keys in context, so using the generic `KeyedTopicCollection<T>` is preferred.

---
+++ OnTopic/Collections/ReadOnlyTopicCollection.cs
@@ -0,0 +1,31 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Topics Library
+\=============================================================================================================================*/
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using OnTopic.Internal.Diagnostics;
+
+namespace OnTopic.Collections {
+
+  /*============================================================================================================================
+  | CLASS: READ-ONLY TOPIC COLLECTION
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Represents a collection of <see cref="Topic"/> objects.
+  /// </summary>
+  public class ReadOnlyTopicCollection : ReadOnlyCollection<Topic> {
+
+    /*==========================================================================================================================
+    | CONSTRUCTOR
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Establishes a new <see cref="ReadOnlyTopicCollection"/> based on an existing <see cref="TopicCollection"/>.
+    /// </summary>
+    /// <param name="innerCollection">The underlying <see cref="KeyedTopicCollection"/>.</param>
+    public ReadOnlyTopicCollection(IList<Topic>? innerCollection = null) : base(innerCollection) {
+    }
+
+  } //Class
+} //Namespace
