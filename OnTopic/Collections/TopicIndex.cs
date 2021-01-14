3af4c789e53e1dda58ca89a06f16720530a0d377
JeremyCaney <jeremy.caney@ignia.com>
2021-01-13 16:01:17 -0800

    Establish new `TopicIndex` collection
    A `TopicIndex` is simply a lookup table of `Topic` objects keyed by `Topic.Id`. By default, using e.g., `Topic.Load(topicId)` or even `Topic.FindFirst(t => t.Id == topicId)` is potentially expensive as it requires a recursive search of the topic graph. That's fine for one-time queries. If multiple queries need to be performed, a `TopicIndex` offers better performance by caching the results in an index.
    
    This is primarily intended to support backend infrastructure, though it may be of use for some specialized client implementations.

---
+++ OnTopic/Collections/TopicIndex.cs
@@ -0,0 +1,34 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Topics Library
+\=============================================================================================================================*/
+using System.Collections.Generic;
+
+namespace OnTopic.Collections {
+
+  /*============================================================================================================================
+  | CLASS: TOPIC INDEX
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Represents a collection of <see cref="Topic"/> objects indexed by <see cref="Topic.Id"/>.
+  /// </summary>
+  public class TopicIndex : Dictionary<int, Topic> {
+
+    /*==========================================================================================================================
+    | CONSTRUCTOR
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Initializes a new instance of the <see cref="TopicCollection"/>.
+    /// </summary>
+    /// <param name="topics">Seeds the collection with an optional list of topic references.</param>
+    public TopicIndex(IEnumerable<Topic>? topics = null) : base() {
+      if (topics is not null) {
+        foreach(var topic in topics) {
+          Add(topic.Id, topic);
+        }
+      }
+    }
+
+  } //Class
+} //Namespace
