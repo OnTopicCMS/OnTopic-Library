cff8eae622ff77af17b20298b68d677e968b9013
JeremyCaney <jeremy.caney@ignia.com>
2021-01-14 15:32:08 -0800

    Introduced new `ReferenceIndex` view
    As with other views, the `ReferenceIndex` view provides the latest version of `TopicReferences`, thus simplifying queries used by e.g. the `GetTopics` stored procedure.
    
    (Note: This should have been committed previously; I apologize for the out-of-order commit history.)

---
+++ OnTopic.Data.Sql.Database/Views/ReferenceIndex.sql
@@ -0,0 +1,29 @@
+﻿--------------------------------------------------------------------------------------------------------------------------------
+-- REFERENCES (INDEX)
+--------------------------------------------------------------------------------------------------------------------------------
+-- Filters the TopicReferences table by the latest version for each topic and reference key. For most use cases, this should be
+-- the primary sources for retrieving topic references, since it excludes historical versions.
+--------------------------------------------------------------------------------------------------------------------------------
+CREATE
+VIEW	[dbo].[ReferenceIndex]
+WITH	SCHEMABINDING
+AS
+
+WITH TopicReferences AS (
+  SELECT	Source_TopicID,
+	ReferenceKey,
+	Target_TopicID,
+	Version,
+	RowNumber = ROW_NUMBER() OVER (
+	  PARTITION BY		Source_TopicID,
+			ReferenceKey
+	  ORDER BY		Version	DESC
+	)
+  FROM	[dbo].[TopicReferences]
+)
+SELECT	TopicReferences.Source_TopicID,
+	TopicReferences.ReferenceKey,
+	TopicReferences.Target_TopicID,
+	TopicReferences.Version
+FROM	TopicReferences
+WHERE	RowNumber		= 1
