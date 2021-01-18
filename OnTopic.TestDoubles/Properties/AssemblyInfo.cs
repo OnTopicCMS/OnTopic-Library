5ba0a09ebccc3928d48ed00171037f1012cce01d
JeremyCaney <jeremy.caney@ignia.com>
2021-01-18 15:58:17 -0800

    Moved suppression of CA1711
    Normally, in .NET, classes should only end in `Attribute` if they inherit from the `Attribute` class. In this case, the naming conventions of OnTopic Editor conflict with the naming conventions of .NET. That's unfortunate, but well-established. As such, we're suppressing this warning since this is a deliberate design decision.
    
    This suppression was originally defined on OnTopic, then moved to OnTopic Tests. With the attribute type descriptors finally moved to Test Doubles (a380d90), the suppression should follow.

--- OnTopic.TestDoubles/Properties/AssemblyInfo.cs
+++ OnTopic.TestDoubles/Properties/AssemblyInfo.cs
@@ -4,6 +4,7 @@
 | Project       Topics Library
 \=============================================================================================================================*/
 using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Runtime.InteropServices;
 
 /*==============================================================================================================================
@@ -14,3 +15,10 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
 [assembly: Guid("FE175884-59C1-4C4D-A663-4CC570432ECC")]
+
+/*==============================================================================================================================
+| HANDLE SUPPRESSIONS
+>===============================================================================================================================
+| Suppress warnings from code analysis that are either false positives or not relevant for this assembly.
+\-----------------------------------------------------------------------------------------------------------------------------*/
+[assembly: SuppressMessage("Naming", "CA1711:Identifiers should not have incorrect suffix", Justification = "Expected by convention for OnTopic Editor", Scope = "namespaceanddescendants", Target = "~N:OnTopic.Tests")]
--- OnTopic.Tests/Properties/AssemblyInfo.cs
+++ OnTopic.Tests/Properties/AssemblyInfo.cs
@@ -4,7 +4,6 @@
 | Project       Topics Library
 \=============================================================================================================================*/
 using System;
-using System.Diagnostics.CodeAnalysis;
 using System.Runtime.InteropServices;
 
 /*==============================================================================================================================
@@ -15,10 +14,3 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
 [assembly: Guid("27632801-bfe3-41d9-8678-3c4bbe45e6c9")]
-
-/*==============================================================================================================================
-| HANDLE SUPPRESSIONS
->===============================================================================================================================
-| Suppress warnings from code analysis that are either false positives or not relevant for this assembly.
-\-----------------------------------------------------------------------------------------------------------------------------*/
-[assembly: SuppressMessage("Naming", "CA1711:Identifiers should not have incorrect suffix", Justification = "Expected by convention for OnTopic Editor", Scope = "namespaceanddescendants", Target = "~N:OnTopic.Tests")]
