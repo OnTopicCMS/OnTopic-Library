9c2baf9b0a25aa5985a7bd2ba9d956b244ee244a
JeremyCaney <jeremy.caney@ignia.com>
2021-01-19 14:47:26 -0800

    Introduced new `MappingModelValidationException`
    The `MappingModelValidationException` is thrown a mapping service attempts to map to a model which cannot be mapped to. This is especially important with the `ReverseTopicMappingService`, since it takes data integrity very seriously when writing back to the database and, thus, fails if any members don't align with the `ContentTypeDescriptor`.
    
    By contrast, this won't generally be used with the read-only mapping services, such as `TopicMappingService`, since it is generally designed to be forgiving.
    
    This inherits from the existing `TopicMappingException`, thus allowing callers to catch the general exception, or hone in on this specific exception if appropriate.

---
+++ OnTopic/Mapping/MappingModelValidationException.cs
@@ -0,0 +1,64 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Topics Library
+\=============================================================================================================================*/
+using System;
+using System.Runtime.Serialization;
+using OnTopic.Internal.Diagnostics;
+using OnTopic.Mapping.Reverse;
+using OnTopic.Metadata;
+
+namespace OnTopic.Mapping {
+
+  /*============================================================================================================================
+  | CLASS: MAPPING MODEL VALIDATION EXCEPTION
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   The <see cref="MappingModelValidationException"/> is thrown when an <see cref="IReverseTopicMappingService"/>
+  ///   implementation is provided a model that cannot be reliably mapped back to the target <see cref="Topic"/>, thus
+  ///   introducing potential data integrity issues.
+  /// </summary>
+  /// <remarks>
+  ///   The read-only mapping services, such as <see cref="ITopicMappingService"/>, are generally designed to be forgiving of
+  ///   mismatches. By contrast, because the <see cref="IReverseTopicMappingService"/> is intended to update the persistence
+  ///   store, it is generally designed to fail if there are any discrepancies between the source model and the target <see cref
+  ///   ="ContentTypeDescriptor"/>. Given this, the <see cref="MappingModelValidationException"/> is expected to be thrown for
+  ///   validation errors caused by e.g. the <see cref="ReverseTopicMappingService"/>.
+  /// </remarks>
+  [Serializable]
+  public class MappingModelValidationException: TopicMappingException {
+
+    /*==========================================================================================================================
+    | CONSTRUCTOR: MAPPING MODEL VALIDATION EXCEPTION
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Initializes a new <see cref="MappingModelValidationException" /> instance.
+    /// </summary>
+    public MappingModelValidationException() : base() { }
+
+    /// <summary>
+    ///   Initializes a new <see cref="MappingModelValidationException" /> instance with a specific error message.
+    /// </summary>
+    /// <param name="message">The message to display for this exception.</param>
+    public MappingModelValidationException(string message) : base(message) { }
+
+    /// <summary>
+    ///   Initializes a new <see cref="MappingModelValidationException" /> instance with a specific error message and nested exception.
+    /// </summary>
+    /// <param name="message">The message to display for this exception.</param>
+    /// <param name="innerException">The reference to the original, underlying exception.</param>
+    public MappingModelValidationException(string message, Exception innerException) : base(message, innerException) { }
+
+    /// <summary>
+    ///   Instantiates a new <see cref="MappingModelValidationException"/> instance for serialization.
+    /// </summary>
+    /// <param name="info">A <see cref="SerializationInfo"/> instance with details about the serialization requirements.</param>
+    /// <param name="context">A <see cref="StreamingContext"/> instance with details about the request context.</param>
+    /// <returns>A new <see cref="InvalidKeyException"/> instance.</returns>
+    protected MappingModelValidationException(SerializationInfo info, StreamingContext context) : base(info, context) {
+      Contract.Requires(info);
+    }
+
+  } //Class
+} //Namespace
