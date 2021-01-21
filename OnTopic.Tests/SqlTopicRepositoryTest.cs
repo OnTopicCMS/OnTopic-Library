c209f39f8b0c8e42e559fd5d6133681a331440c8
JeremyCaney <jeremy.caney@ignia.com>
2021-01-21 00:52:47 -0800

    Setup `SqlTopicRepository` for unit testing
    This includes marking the `SqlTopicRepository` internals as visible to `OnTopic.Tests`, referencing the `SqlTopicRepository` project from `OnTopic.Tests`, and establishing a basic placeholder for a `SqlTopicRepositoryTest` class.

---
+++ OnTopic.Tests/SqlTopicRepositoryTest.cs
@@ -0,0 +1,27 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Topics Library
+\=============================================================================================================================*/
+using System;
+using System.Data;
+using System.Linq;
+using System.Xml;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OnTopic.Data.Sql;
+using OnTopic.References;
+using OnTopic.Tests.Schemas;
+
+namespace OnTopic.Tests {
+
+  /*============================================================================================================================
+  | CLASS: SQL TOPIC REPOSITORY TEST
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Provides unit tests for the <see cref="SqlTopicRepository"/> class.
+  /// </summary>
+  [TestClass]
+  public class SqlTopicRepositoryTest {
+
+  } //Class
+} //Namespace
