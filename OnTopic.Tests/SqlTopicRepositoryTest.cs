e512f34caa4c025f23987de213a7149d8f4ba1ec
JeremyCaney <jeremy.caney@ignia.com>
2021-01-21 01:13:26 -0800

    Introduced unit test for validating the `SetRelationships()` extension
    This unit test creates a `TopicsDataTable` and a `RelationshipsDataTable` and passes them to the `LoadTopicGraph()` extension method, ensuring that the (private) `SetRelationships()` extension method correctly utilizes the data tables to set the specified relationship on the new `Topic` entity.

--- OnTopic.Tests/SqlTopicRepositoryTest.cs
+++ OnTopic.Tests/SqlTopicRepositoryTest.cs
@@ -72,5 +72,33 @@ namespace OnTopic.Tests {
 
     }
 
+    /*==========================================================================================================================
+    | TEST: LOAD TOPIC GRAPH: WITH RELATIONSHIP: RETURNS RELATIONSHIP
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Calls <see cref="SqlDataReaderExtensions.LoadTopicGraph(IDataReader, Topic?, Boolean?, Boolean)"/> with a <see cref="
+    ///   RelationshipsDataTable"/> record and confirms that a topic with those values is returned.
+    /// </summary>
+    [TestMethod]
+    public void LoadTopicGraph_WithRelationship_ReturnsRelationship() {
+
+      using var topics          = new TopicsDataTable();
+      using var empty           = new AttributesDataTable();
+      using var relationships   = new RelationshipsDataTable();
+
+      topics.AddRow(1, "Root", "Container", null);
+      topics.AddRow(2, "Web", "Container", 1);
+      relationships.AddRow(1, "Test", 2, false);
+
+      using var tableReader     = new DataTableReader(new DataTable[] { topics, empty, empty, relationships });
+
+      var topic                 = tableReader.LoadTopicGraph();
+
+      Assert.IsNotNull(topic);
+      Assert.AreEqual<int>(1, topic.Id);
+      Assert.AreEqual<int?>(2, topic.Relationships.GetTopics("Test").FirstOrDefault()?.Id);
+
+    }
+
   } //Class
 } //Namespace
