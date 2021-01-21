4bc95e109a57fdcc662df0545d328f7c3e34a8c4
JeremyCaney <jeremy.caney@ignia.com>
2021-01-21 13:39:00 -0800

    Reintroduced the `IsExternal` polyfills
    This effectively rolls back the deletion of the `IsExternalInit` polyfills (b9857c8), which are not necessary when targeting .NET 5.0, but are necessary now that we've rolled back to targeting .NET Standard 3.1 and .NET Core 3.1.

---
+++ OnTopic/Internal/Runtime/IsExternalInit.cs
@@ -0,0 +1,19 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Topics Library
+\=============================================================================================================================*/
+
+namespace System.Runtime.CompilerServices {
+
+  /*============================================================================================================================
+  | CLASS: IS EXTERNAL INIT
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   The <see cref="IsExternalInit"/> class is made available as part of the .NET 5.0 CLR in order to enable init accessors.
+  ///   As this is not available in .NET Standard, however, we must maintain this separate copy until we migrate to .NET 5.0.
+  /// </summary>
+  internal static class IsExternalInit {
+
+  } //Class
+} //Namespace
