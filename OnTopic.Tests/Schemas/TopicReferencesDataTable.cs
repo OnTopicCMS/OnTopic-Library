e5f02c5f4646a267c4ad71921156f4cbe3b43e64
JeremyCaney <jeremy.caney@ignia.com>
2021-01-21 01:08:55 -0800

    Established `TopicReferencesDataTable` for unit testing
    While we can't easily test `SqlDataReader`, we can easily test a `DataTableReader`, which operates off of the same `IDataReader` interface as `SqlDataReader`. Given this, the `TopicReferencesDataTable` provides a simple way of mimicking SQL response data for the `IDataReader` extension methods used by `SqlTopicRepository` to operate off of, thus allowing us to unit test these methods without using a SQL database.
    
    The `TopicReferencesDataTable` class includes not only the basic schema definition for the `TopicReferences` table, but also an `AddRow()` method with strongly typed parameters to simplify creating a new record.

---
+++ OnTopic.Tests/Schemas/TopicReferencesDataTable.cs
@@ -0,0 +1,102 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Topics Library
+\=============================================================================================================================*/
+using System;
+using System.Data;
+using OnTopic.Data.Sql;
+using OnTopic.Internal.Diagnostics;
+
+namespace OnTopic.Tests.Schemas {
+
+  /*============================================================================================================================
+  | CLASS: TOPIC REFERENCES DATA TABLE
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Provides a <see cref="DataTable"/> which maps to the expected schema of the <c>TopicReferences</c> table.
+  /// </summary>
+  /// <remarks>
+  ///   This allows testing of the <see cref="SqlTopicRepository"/> via its <see cref="SqlDataReaderExtensions"/> methods.
+  /// </remarks>
+  public class TopicReferencesDataTable: DataTable {
+
+    /*==========================================================================================================================
+    | CONSTRUCTOR
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Instantiates a new instance of the <see cref="TopicReferencesDataTable"/>.
+    /// </summary>
+    /// <returns>A new instance of the <see cref="TopicReferencesDataTable"/>.</returns>
+    public TopicReferencesDataTable() : base("TopicReferences") {
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Add Source_TopicId column
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      Columns.Add(new DataColumn() {
+        DataType                = typeof(int),
+        ColumnName              = "Source_TopicId",
+        Unique                  = true
+      });
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Add RelationshipKey column
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      Columns.Add(new DataColumn() {
+        DataType                = typeof(string),
+        ColumnName              = "ReferenceKey"
+      });
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Add Target_TopicId column
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      Columns.Add(new DataColumn() {
+        DataType                = typeof(int),
+        ColumnName              = "Target_TopicId",
+        AllowDBNull             = true
+      });
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Add ParentId column
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      Columns.Add(new DataColumn() {
+        DataType                = typeof(DateTime),
+        ColumnName              = "Version"
+      });
+
+    }
+
+    /*==========================================================================================================================
+    | ADD ROW
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Adds a new <see cref="DataRow"/> to the <see cref="TopicReferencesDataTable"/>.
+    /// </summary>
+    public void AddRow(int sourceTopicId, string referenceKey, int? targetTopicId, DateTime? version = null) {
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Verify parameters
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      Contract.Requires(sourceTopicId, nameof(sourceTopicId));
+      Contract.Requires(referenceKey, nameof(referenceKey));
+      Contract.Requires(targetTopicId, nameof(targetTopicId));
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Create new row
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      var row = NewRow();
+
+      row["Source_TopicId"]     = sourceTopicId;
+      row["ReferenceKey"]       = referenceKey;
+      row["Target_TopicId"]     = targetTopicId.HasValue ? (object)targetTopicId : DBNull.Value;
+      row["Version"]            = version?? DateTime.UtcNow;
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Add row to table
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      Rows.Add(row);
+
+    }
+
+  } //Class
+} //Namespace
