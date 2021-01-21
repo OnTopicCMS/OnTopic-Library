824bf335dbf675766ca3cfa546040a8b7c2ec9ba
JeremyCaney <jeremy.caney@ignia.com>
2021-01-21 01:03:53 -0800

    Established `AttributesDataTable` for unit testing
    While we can't easily test `SqlDataReader`, we can easily test a `DataTableReader`, which operates off of the same `IDataReader` interface as `SqlDataReader`. Given this, the `AttributesDataTable` provides a simple way of mimicking SQL response data for the `IDataReader` extension methods used by `SqlTopicRepository` to operate off of, thus allowing us to unit test these methods without using a SQL database.
    
    The `AttributesDataTable` class includes not only the basic schema definition for the `Attributes` table, but also an `AddRow()` method with strongly typed parameters to simplify creating a new record.

---
+++ OnTopic.Tests/Schemas/AttributesDataTable.cs
@@ -0,0 +1,101 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Topics Library
+\=============================================================================================================================*/
+using System;
+using System.Data;
+using OnTopic.Data.Sql;
+using OnTopic.Internal.Diagnostics;
+
+namespace OnTopic.Tests.Schemas {
+
+  /*============================================================================================================================
+  | CLASS: ATTRIBUTES DATA TABLE
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Provides a <see cref="DataTable"/> which maps to the expected schema of the <c>Attributes</c> table.
+  /// </summary>
+  /// <remarks>
+  ///   This allows testing of the <see cref="SqlTopicRepository"/> via its <see cref="SqlDataReaderExtensions"/> methods.
+  /// </remarks>
+  public class AttributesDataTable: DataTable {
+
+    /*==========================================================================================================================
+    | CONSTRUCTOR
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Instantiates a new instance of the <see cref="AttributesDataTable"/>.
+    /// </summary>
+    /// <returns>A new instance of the <see cref="AttributesDataTable"/>.</returns>
+    public AttributesDataTable() : base("Attributes") {
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Add TopicId column
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      Columns.Add(new DataColumn() {
+        DataType                = typeof(int),
+        ColumnName              = "TopicId",
+        Unique                  = true
+      });
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Add AttributeKey column
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      Columns.Add(new DataColumn() {
+        DataType                = typeof(string),
+        ColumnName              = "AttributeKey"
+      });
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Add AttributeValue column
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      Columns.Add(new DataColumn() {
+        DataType                = typeof(string),
+        ColumnName              = "AttributeValue",
+        AllowDBNull             = true
+      });
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Add Version column
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      Columns.Add(new DataColumn() {
+        DataType                = typeof(DateTime),
+        ColumnName              = "Version"
+      });
+
+    }
+
+    /*==========================================================================================================================
+    | ADD ROW
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Adds a new <see cref="DataRow"/> to the <see cref="AttributesDataTable"/>.
+    /// </summary>
+    public void AddRow(int topicId, string attributeKey, string? attributeValue, DateTime? version = null) {
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Verify parameters
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      Contract.Requires(topicId, nameof(topicId));
+      Contract.Requires(attributeKey, nameof(attributeKey));
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Create new row
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      var row = NewRow();
+
+      row["TopicId"]            = topicId;
+      row["AttributeKey"]       = attributeKey;
+      row["AttributeValue"]     = attributeValue is null? DBNull.Value : attributeValue;
+      row["Version"]            = version?? DateTime.UtcNow;
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Add row to table
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      Rows.Add(row);
+
+    }
+
+  } //Class
+} //Namespace
