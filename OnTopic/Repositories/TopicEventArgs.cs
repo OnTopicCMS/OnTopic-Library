f016318fedca7d9f91d265977c75465de3991571
JeremyCaney <jeremy.caney@ignia.com>
2021-01-25 14:03:00 -0800

    Updated comments in `TopicEventArgs` for consistency

--- OnTopic/Repositories/TopicEventArgs.cs
+++ OnTopic/Repositories/TopicEventArgs.cs
@@ -8,7 +8,7 @@ using System;
 namespace OnTopic.Repositories {
 
   /*============================================================================================================================
-  | CLASS: TOPIC EVENT ARGS
+  | CLASS: TOPIC EVENT ARGUMENTS
   \---------------------------------------------------------------------------------------------------------------------------*/
   /// <summary>
   ///   The <see cref="TopicEventArgs"/> class defines an event argument type shared among <see cref="ITopicRepository"/>
@@ -18,12 +18,12 @@ namespace OnTopic.Repositories {
   ///   All <see cref="ITopicRepository"/> events share at least one shared element: the <see cref="Topic"/> being operated
   ///   against. Some, such as the <see cref="ITopicRepository.DeleteEvent"/>, <i>only</i> relate to that information. Others,
   ///   such as <see cref="ITopicRepository.MoveEvent"/>, need additional information, and thus offer derived classes, such as
-  ///   <see cref="MoveEventArgs"/>, to capture additional information.
+  ///   <see cref="TopicMoveEventArgs"/>, to capture additional information.
   /// </remarks>
   public class TopicEventArgs : EventArgs {
 
     /*==========================================================================================================================
-    | CONSTRUCTOR: TAXONOMY DELETE EVENT ARGS
+    | CONSTRUCTOR
     \-------------------------------------------------------------------------------------------------------------------------*/
     /// <summary>
     ///   Initializes a new instance of the <see cref="TopicEventArgs"/> class.
