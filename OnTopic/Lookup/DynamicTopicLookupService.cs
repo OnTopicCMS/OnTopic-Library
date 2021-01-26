cd9e5400061cd2c123fa1753d51fad8fc418e9c7
JeremyCaney <jeremy.caney@ignia.com>
2021-01-26 13:44:26 -0800

    Added `AttributeDescriptor` fallback logic for `DynamicTopicLookupService`
    As discussed in a previous commit (4116633), the strongly typed versions of `AttributeDescriptor` are now contained within plugins for the OnTopic Editor (1d122e0), and will only be within scope if the OnTopic Editor is configured and that plugin is included.
    
    In absence of that, however, most other applications rely on members provided by the more general `AttributeDescriptor`. Given that, instead of falling back to the default of `Topic`, we want to fall back to `AttributeDescriptor` if the requested `typeName` ends in `AttributeDescriptor`, but the specialized type cannot be found.
    
    This ensures that e.g. the `ContentTypeCollection.AttributeTypes` collection is properly initialized, and that code that needs general access to attribute type configuration, such as `SqlTopicRepository`, can still get access to that, even if they don't have the more precise overrides from e.g. `EditorType` or `ModelType` typically exposed by the subclasses.
    
    This allows the attribute types to be decoupled from the OnTopic library—as well as the OnTopic Editor—and thus enabled plugins to be dynamically included without introducing problems for applications where those plugins aren't in scope.

--- OnTopic/Lookup/DynamicTopicLookupService.cs
+++ OnTopic/Lookup/DynamicTopicLookupService.cs
@@ -4,6 +4,7 @@
 | Project       Topics Library
 \=============================================================================================================================*/
 using System;
+using OnTopic.Metadata;
 
 namespace OnTopic.Lookup {
 
@@ -26,6 +27,32 @@ namespace OnTopic.Lookup {
       t => typeof(Topic).IsAssignableFrom(t),
       typeof(Topic)
     ) { }
+
+    /*==========================================================================================================================
+    | METHOD: LOOKUP
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <inheritdoc/>
+    /// <remarks>
+    ///   The <see cref="DynamicTopicLookupService"/> version of <see cref="Lookup(String)"/> will automatically fall back to
+    ///   <see cref="AttributeDescriptor"/> if the <paramref name="typeName"/> ends with <c>AttributeDescriptor</c>, but a
+    ///   <see cref="AttributeDescriptor"/> with the specified name cannot be found. This accounts for the fact that strongly
+    ///   typed <see cref="AttributeDescriptor"/> classes are expected to be in external plugins which may not be available
+    ///   unless the current application is configured to use the OnTopic Editor. In that case, the base <see cref="
+    ///   AttributeDescriptor"/> class will provide access to the attributes needed by most applications, including the core
+    ///   OnTopic library.
+    /// </remarks>
+    public override Type? Lookup(string typeName) {
+      if (typeName is null) {
+        return DefaultType;
+      }
+      else if (Contains(typeName)) {
+        return base.Lookup(typeName);
+      }
+      else if (typeName.EndsWith("AttributeDescriptor", StringComparison.OrdinalIgnoreCase)) {
+        return typeof(AttributeDescriptor);
+      }
+      return DefaultType;
+    }
 
   } //Class
 } //Namespace
