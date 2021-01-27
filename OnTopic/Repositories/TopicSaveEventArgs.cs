97be8ae3905c1175342cb653e2a8407221948561
JeremyCaney <jeremy.caney@ignia.com>
2021-01-27 11:50:08 -0800

    Introduced `TopicSaveEventArgs` for `ITopicRepository.Save()` operations
    The `TopicSaveEventArgs` introduces an `IsNew` property for tracking whether or not this is a newly created topic, or an update to an existing one.

---
+++ OnTopic/Repositories/TopicSaveEventArgs.cs
@@ -0,0 +1,53 @@
+/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Topics Library
+\=============================================================================================================================*/
+using System;
+using OnTopic.Internal.Diagnostics;
+
+namespace OnTopic.Repositories {
+
+  /*============================================================================================================================
+  | CLASS: TOPIC SAVE EVENT ARGUMENTS
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   The <see cref="TopicSaveEventArgs"/> object defines an event argument type specific to save events.
+  /// </summary>
+  public class TopicSaveEventArgs : TopicEventArgs {
+
+    /*==========================================================================================================================
+    | CONSTRUCTOR
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   The <see cref="TopicSaveEventArgs"/> object defines the event arguments relevant to a <see cref="ITopicRepository.
+    ///   Save(Topic, Boolean)"/> operation.
+    /// </summary>
+    /// <param name="topic">The <see cref="Topic"/> object associated with the rename event.</param>
+    /// <param name="isRecursive">Whether or not descendants of the <see cref="Topic"/> were also saved.</param>
+    /// <param name="isNew">Whether or not this was a newly created <see cref="Topic"/>.</param>
+    public TopicSaveEventArgs(Topic topic, bool isRecursive, bool isNew): base(topic, isRecursive) {
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Vaidate parameters
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      Contract.Requires(isNew, nameof(isNew));
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Initialize properties
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      IsNew                     = isNew;
+
+    }
+
+    /*==========================================================================================================================
+    | PROPERTY: IS NEW
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Gets or sets whether the <see cref="TopicEventArgs.Topic"/> was newly created, or if it was an existing <see cref="
+    ///   Topic"/> that has been updated.
+    /// </summary>
+    public bool IsNew { get; set; }
+
+  } //Class
+} //Namespace
