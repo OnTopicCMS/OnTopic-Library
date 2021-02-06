cf7badfe9ca2065291b8dbab49a43d4d280669e4
JeremyCaney <jeremy.caney@ignia.com>
2021-02-06 15:56:54 -0800

    Reintroduced `IRelatedTopicBindingModel` as deprecated
    In the previous commit, we renamed `IRelatedTopicBindingModel` to `IAssociatedTopicBindingModel` to ensure that it accounted for both relationships and topic references, thus using the preferred "associations" nomenclature.
    
    As `IRelatedTopicBindingModel` hadn't been marked as deprecated, this could caused confusion. To help mitigate that, the `IRelatedTopicBindingModel` is being reintroduced as a deprecated so that consumers will be giving instructions on how to migrate their implementations.
    
    These will be removed with OnTopic 5.1.0.

---
+++ OnTopic.ViewModels/BindingModels/RelatedTopicBindingModel.cs
@@ -0,0 +1,31 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Topics Library
+\=============================================================================================================================*/
+using System;
+using OnTopic.Mapping.Reverse;
+using OnTopic.Models;
+
+namespace OnTopic.ViewModels.BindingModels {
+
+  /*============================================================================================================================
+  | CLASS: RELATED TOPIC BINDING MODEL
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Provides a generic data transfer topic for binding a relationship of a binding model to an existing <see cref="Topic"/>.
+  /// </summary>
+  /// <remarks>
+  ///   While implementors may choose to create a custom <see cref="IAssociatedTopicBindingModel"/> implementation, the out-of-
+  ///   the-box <see cref="AssociatedTopicBindingModel"/> implementation satisfies all of the requirements of the <see cref="
+  ///   ReverseTopicMappingService"/>. The only reason to implement a custom definition is if the caller needs additional
+  ///   metadata for separate validation or processing.
+  /// </remarks>
+  [Obsolete(
+    "The RelatedTopicBindingModel has been replaced by the AssociatedTopicBindingModel. Please update references.",
+    true
+  )]
+  public record RelatedTopicBindingModel: AssociatedTopicBindingModel {
+
+  } //Class
+} //Namespaces
---
+++ OnTopic/Models/IRelatedTopicBindingModel.cs
@@ -0,0 +1,29 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Topics Library
+\=============================================================================================================================*/
+using System;
+using OnTopic.Mapping.Reverse;
+
+namespace OnTopic.Models {
+
+  /*============================================================================================================================
+  | INTERFACE: RELATED TOPIC BINDING MODEL
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Provides a generic data transfer topic for binding an association of a binding model to an existing <see cref="Topic"/>.
+  /// </summary>
+  /// <remarks>
+  ///   It is strictly required that any binding models used as associations implement the <see cref="IRelatedTopicBindingModel"
+  ///   /> interface for the default <see cref="ReverseTopicMappingService"/> to correctly identify and map an association back
+  ///   to a <see cref="Topic"/>.
+  /// </remarks>
+  [Obsolete(
+    "The IRelatedTopicBindingModel has been replaced by the IAssociatedTopicBindingModel. Please update references.",
+    true
+  )]
+  public interface IRelatedTopicBindingModel: IAssociatedTopicBindingModel {
+
+  } //Class
+} //Namespace
