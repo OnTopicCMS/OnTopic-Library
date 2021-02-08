d551442bcc9fa1e1cd482e7dc1ae5862d3e25d83
JeremyCaney <jeremy.caney@ignia.com>
2021-02-08 14:17:44 -0800

    Introduce `IEnumerable<Topic>` extension methods
    The `TopicCollectionExtension` methods provide some basic extension methods for evaluating all topics in an `IEnumerable<Topic>` collection. These are implemented as extensions so that they don't need to be separately defined on e.g. `TopicCollection`, `KeyedTopicCollection<T>`, &c. This allows these methods to be applied to all of those at once—assuming, of course, that `OnTopic.Querying` is in scope.
    
    To start with, two extension methods are introduced. One will determine if any of the topics `IsDirty`. The other will determine if any of the topics `IsNew`.
    
    These are pretty trivial shortcuts. We could implement this via a delegate as well—but, at that point, callers might as well simply call LINQ's `Any()` directly.

---
+++ OnTopic/Querying/TopicCollectionExtensions.cs
@@ -0,0 +1,49 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Topics Library
+\=============================================================================================================================*/
+using System.Collections.Generic;
+using System.Linq;
+using OnTopic.Collections;
+
+namespace OnTopic.Querying {
+
+  /*============================================================================================================================
+  | CLASS: TOPIC COLLECTION (EXTENSIONS)
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Provides extensions for querying <see cref="TopicCollection"/>, <see cref="KeyedTopicCollection"/>, and related
+  ///   collections via the generic <see cref="IEnumerable{T}"/> interface.
+  /// </summary>
+  public static class TopicCollectionExtensions {
+
+    /*==========================================================================================================================
+    | METHOD: ANY DIRTY?
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Determines whether any of the <see cref="Topic"/> instances in the collection are marked as <see cref="Topic.
+    ///   IsDirty()"/>.
+    /// </summary>
+    /// <remarks>
+    ///   This does not determine if the collection itself is dirty—it only determines if any <see cref="Topic"/> instances in
+    ///   the collection are <see cref="Topic.IsDirty()"/>. This distinction is important. For example, if a clean <see cref="
+    ///   Topic"/> is added to the collection, then the collection will be dirty—but <see cref="AnyDirty(IEnumerable{Topic})"/>
+    ///   will be false.
+    /// </remarks>
+    /// <param name="topics">The collection of <see cref="Topic"/> instances to operate against.</param>
+    /// <returns>Returns <c>true</c> if any of the <see cref="Topic"/> instances are <see cref="Topic.IsDirty()"/>.</returns>
+    public static bool AnyDirty(this IEnumerable<Topic> topics) => topics.Any(t => t.IsDirty(true));
+
+    /*==========================================================================================================================
+    | METHOD: ANY NEW?
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Determines whether any of the <see cref="Topic"/> instances in the collection are marked as <see cref="Topic.IsNew"/>.
+    /// </summary>
+    /// <param name="topics">The collection of <see cref="Topic"/> instances to operate against.</param>
+    /// <returns>Returns <c>true</c> if any of the <see cref="Topic"/> instances are <see cref="Topic.IsNew"/>.</returns>
+    public static bool AnyNew(this IEnumerable<Topic> topics) => topics.Any(t => t.IsNew);
+
+  }
+}
