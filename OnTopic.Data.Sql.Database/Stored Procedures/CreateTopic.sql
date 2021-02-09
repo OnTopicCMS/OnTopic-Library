64ae337cb81a8a738bc1c69fe1be2ac4a4427d6b
JeremyCaney <jeremy.caney@ignia.com>
2021-02-08 16:59:55 -0800

    Wrapped the `CreateTopic` stored procedure in a transaction
    While it's generally a best practice to wrap stored procedures in transactions, and especially when they involve multiple CUD operations, it's especially important when modifying the nested set since errors caused during these operations can result in corrupting the hierarchy. Given this, the `MoveTopic` and `DeleteTopic` stored procedures were already wrapped in transactions, but the `CreateTopic` stored procedure wasn't. This corrects that issue by wrapping it in similar transaction logic, along with `TRY`/`CATCH` blocks

--- OnTopic.Data.Sql.Database/Stored Procedures/CreateTopic.sql
+++ OnTopic.Data.Sql.Database/Stored Procedures/CreateTopic.sql
@@ -15,6 +15,37 @@ CREATE PROCEDURE [dbo].[CreateTopic]
 AS
 
 --------------------------------------------------------------------------------------------------------------------------------
+-- DECLARE VARIABLES
+--------------------------------------------------------------------------------------------------------------------------------
+DECLARE	@IsNestedTransaction	BIT;
+DECLARE	@TopicID		INT;
+
+BEGIN TRY
+
+--------------------------------------------------------------------------------------------------------------------------------
+-- BEGIN TRANSACTION
+--------------------------------------------------------------------------------------------------------------------------------
+-- ### NOTE JJC20210218: By necessity, this procedure potentially makes a massive number of changes to the Topics table's nested
+-- set. During the execution, the nested set hierarchy WILL be in an inconsistent state. Read operations during that time are
+-- very likely to be corrupted. As such, it's critical that the updates made as part of this procedure be isolated from other
+-- reads being performed on the system. Further, we don't want any writes being made to the Topics table during this time—see
+-- notes below regarding TABLOCK. By combining SERIALIZABLE with TABLOCK, we ensure that a) readers get a stable state, while b)
+-- writers are prevented from concurrently modifying the table. Fortunately, these types of operations should be pretty
+-- uncommon! The nested set model is very much optimized for read performance and presumes a relatively stable data set.
+--------------------------------------------------------------------------------------------------------------------------------
+IF (@@TRANCOUNT = 0)
+  BEGIN
+    SET @IsNestedTransaction = 0;
+    BEGIN TRANSACTION;
+  END
+ELSE
+  BEGIN
+    SET @IsNestedTransaction = 1;
+  END
+
+SET TRANSACTION ISOLATION LEVEL SERIALIZABLE
+
+--------------------------------------------------------------------------------------------------------------------------------
 -- SET DEFAULT VERSION DATETIME
 --------------------------------------------------------------------------------------------------------------------------------
 IF	@Version		IS NULL
@@ -76,8 +107,6 @@ Values (
 	@Version
 )
 
-DECLARE	@TopicID		INT
-
 SELECT	@TopicID		= SCOPE_IDENTITY()
 
 --------------------------------------------------------------------------------------------------------------------------------
@@ -127,6 +156,27 @@ IF @ReferenceCount > 0
 			@Version,
 			1
   END
+
+--------------------------------------------------------------------------------------------------------------------------------
+-- COMMIT TRANSACTION
+--------------------------------------------------------------------------------------------------------------------------------
+IF (@@TRANCOUNT > 0 AND @IsNestedTransaction = 0)
+  BEGIN
+    COMMIT
+  END
+END TRY
+
+--------------------------------------------------------------------------------------------------------------------------------
+-- HANDLE ERRORS
+--------------------------------------------------------------------------------------------------------------------------------
+BEGIN CATCH
+  IF (@@TRANCOUNT > 0 AND @IsNestedTransaction = 0)
+    BEGIN
+      ROLLBACK;
+    END;
+  THROW
+  RETURN;
+END CATCH
 
 --------------------------------------------------------------------------------------------------------------------------------
 -- RETURN TOPIC ID
