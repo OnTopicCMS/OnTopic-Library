696080089b90107e39134f1e409b17bdff818263
JeremyCaney <jeremy.caney@ignia.com>
2021-02-09 16:43:34 -0800

    Renamed `TopicReference` to `TopicReferenceRecord`
    This aligns with the rename of `TrackedItem` to `TrackedRecord` (a6db252).

--- OnTopic/Associations/TopicReferenceRecord.cs
+++ OnTopic/Associations/TopicReferenceRecord.cs
@@ -11,7 +11,7 @@ using OnTopic.Repositories;
 namespace OnTopic.Associations {
 
   /*============================================================================================================================
-  | CLASS: TOPIC REFERENCE
+  | CLASS: TOPIC REFERENCE RECORD
   \---------------------------------------------------------------------------------------------------------------------------*/
   /// <summary>
   ///   Represents the immutable value of a particular topic reference on a <see cref="Topic"/>.
@@ -23,41 +23,41 @@ namespace OnTopic.Associations {
   ///     TrackedRecord{T}.LastModified"/> date.
   ///   </para>
   ///   <para>
-  ///     Typically, the <see cref="TopicReference"/> will be exposed as part of a <see cref="TopicReferenceCollection"/> via
-  ///     the <see cref="Topic.References"/> collection.
+  ///     Typically, the <see cref="TopicReferenceRecord"/> will be exposed as part of a <see cref="TopicReferenceCollection"/>
+  ///     via the <see cref="Topic.References"/> collection.
   ///   </para>
   ///   <para>
-  ///     Be aware that while <see cref="TopicReference"/> represents the value of a specific topic reference, the metadata for
-  ///     describing the purpose, constraints, and usage of that particular attribute is described by the <see
-  ///     cref="AttributeDescriptor"/> class.
+  ///     Be aware that while <see cref="TopicReferenceRecord"/> represents the value of a specific topic reference, the
+  ///     metadata for describing the purpose, constraints, and usage of that particular attribute is described by the <see cref
+  ///     ="AttributeDescriptor"/> class.
   ///   </para>
   ///   <para>
   ///     This class is immutable: once it is constructed, the values cannot be changed. To change a value, callers must either
-  ///     create a new instance of the <see cref="TopicReference"/> class or, preferably, call the <see cref="Topic.References"
-  ///     />'s <see cref="TrackedRecordCollection{TItem, TValue, TAttribute}.SetValue(String, TValue, Boolean?, DateTime?)"/>
-  ///     method.
+  ///     create a new instance of the <see cref="TopicReferenceRecord"/> class or, preferably, call the <see cref="Topic.
+  ///     References"/>'s <see cref="TrackedRecordCollection{TItem, TValue, TAttribute}.SetValue(String, TValue, Boolean?,
+  ///     DateTime?)"/> method.
   ///   </para>
   /// </remarks>
-  public record TopicReference: TrackedRecord<Topic> {
+  public record TopicReferenceRecord: TrackedRecord<Topic> {
 
     /*==========================================================================================================================
     | CONSTRUCTOR
     \-------------------------------------------------------------------------------------------------------------------------*/
     /// <inheritdoc/>
-    public TopicReference(): base() { }
+    public TopicReferenceRecord(): base() { }
 
     /// <summary>
-    ///   Initializes a new instance of the <see cref="TopicReference"/> class, using the specified key/value pair.
+    ///   Initializes a new instance of the <see cref="TopicReferenceRecord"/> class, using the specified key/value pair.
     /// </summary>
     /// <param name="key">
-    ///   The string identifier for the <see cref="TopicReference"/> collection item key/value pair.
+    ///   The string identifier for the <see cref="TopicReferenceRecord"/> collection item key/value pair.
     /// </param>
     /// <param name="value">
-    ///   The string value text for the <see cref="TopicReference"/> collection item key/value pair.
+    ///   The string value text for the <see cref="TopicReferenceRecord"/> collection item key/value pair.
     /// </param>
     /// <param name="isDirty">
-    ///   An optional boolean indicator noting whether the <see cref="TopicReference"/> collection item is a new value, and
-    ///   should thus be saved to the database when <see cref="ITopicRepository.Save(Topic, Boolean)"/> is next called.
+    ///   An optional boolean indicator noting whether the <see cref="TopicReferenceRecord"/> collection item is a new value,
+    ///   and should thus be saved to the database when <see cref="ITopicRepository.Save(Topic, Boolean)"/> is next called.
     /// </param>
     /// <param name="lastModified">
     ///   The <see cref="DateTime"/> value that the attribute was last modified. This is intended primarily for use when
@@ -68,7 +68,7 @@ namespace OnTopic.Associations {
     ///   description="The key must be specified for the key/value pair." exception="T:System.ArgumentNullException">
     ///   !String.IsNullOrWhiteSpace(key)
     /// </requires>
-    public TopicReference(
+    public TopicReferenceRecord(
       string key,
       Topic value,
       bool isDirty              = true,
