01ad3c916eb143ff59a2fc4d1067d03b8631945b
JeremyCaney <jeremy.caney@ignia.com>
2021-02-17 14:37:41 -0800

    Introduced polyfill for `[MemberNotNull]` attribute
    The `[MemberNotNull]` attribute is introduced in .NET 5, and not available in .NET Standard 2.1, which OnTopic targets. We could multi-target .NET 5 to gain access to it. But as it's one of the few .NET 5 capabilities we have an immediate need for, and it can easily be introduced via a polyfill, we're doing that. This allows us to annotate e.g. properties as a means of validating that local members—including private fields—will not be null after the annotated member is called.

---
+++ OnTopic/Internal/Diagnostics/MemberNotNullAttribute.cs
@@ -0,0 +1,45 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Topics Library
+\=============================================================================================================================*/
+namespace System.Diagnostics.CodeAnalysis {
+
+  /*============================================================================================================================
+  | CLASS: MEMBER NOT NULL (ATTRIBUTE)
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Specifies that the method or property will ensure that the listed field and property members have not-null values.
+  /// </summary>
+  [AttributeUsage(AttributeTargets.Method | AttributeTargets.Property, Inherited = false, AllowMultiple = true)]
+  internal sealed class MemberNotNullAttribute : Attribute {
+
+    /*==========================================================================================================================
+    | CONSTRUCTOR
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>Initializes the attribute with a field or property member.</summary>
+    /// <param name="member">
+    /// The field or property member that is promised to be not-null.
+    /// </param>
+    #pragma warning disable CA1019 // Define accessors for attribute arguments
+    public MemberNotNullAttribute(string member) {
+      Members = new[] { member };
+      }
+    #pragma warning restore CA1019 // Define accessors for attribute arguments
+
+    /// <summary>Initializes the attribute with the list of field and property members.</summary>
+    /// <param name="members">
+    /// The list of field and property members that are promised to be not-null.
+    /// </param>
+    public MemberNotNullAttribute(params string[] members) {
+      Members = members;
+    }
+
+    /*==========================================================================================================================
+    | PROPERTY: MEMBERS
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>Gets field or property member names.</summary>
+    public string[] Members { get; }
+
+  }
+}
