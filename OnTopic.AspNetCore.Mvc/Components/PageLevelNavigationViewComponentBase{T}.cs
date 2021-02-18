c87182566d86ec6a41c086a255212372450b829b
JeremyCaney <jeremy.caney@ignia.com>
2021-02-17 16:26:01 -0800

    Sort and remove usings
    A small bit of housekeeping

--- OnTopic.AspNetCore.Mvc/Components/PageLevelNavigationViewComponentBase{T}.cs
+++ OnTopic.AspNetCore.Mvc/Components/PageLevelNavigationViewComponentBase{T}.cs
@@ -3,7 +3,6 @@
 | Client        Ignia, LLC
 | Project       Topics Library
 \=============================================================================================================================*/
-using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using OnTopic.AspNetCore.Mvc.Models;
--- OnTopic.Tests/KeyedTopicCollectionTest.cs
+++ OnTopic.Tests/KeyedTopicCollectionTest.cs
@@ -6,7 +6,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using OnTopic.Attributes;
 using OnTopic.Collections;
 
 namespace OnTopic.Tests {
