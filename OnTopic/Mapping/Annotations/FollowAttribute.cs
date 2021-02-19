addce60ce2fd83b991add5e8b206e8aae4ca547c
JeremyCaney <jeremy.caney@ignia.com>
2021-02-19 13:52:43 -0800

    Reintroduced `[Follow]` attribute as `[Obsolete]`
    This was renamed to `[Include]`. This provides instructions to callers of the original attribute name.

---
+++ OnTopic/Mapping/Annotations/FollowAttribute.cs
@@ -0,0 +1,38 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Topics Library
+\=============================================================================================================================*/
+using System;
+
+namespace OnTopic.Mapping.Annotations {
+
+  /*============================================================================================================================
+  | ATTRIBUTE: FOLLOW
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <inheritdoc cref="IncludeAttribute"/>
+  [AttributeUsage(System.AttributeTargets.Property)]
+  [Obsolete("The [Follow] attribute has been renamed to [Include].", true)]
+  public sealed class FollowAttribute : Attribute {
+
+    /*==========================================================================================================================
+    | CONSTRUCTOR
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Annotates a property with the <see cref="FollowAttribute"/> by providing an <paramref name="relationships"/>.
+    /// </summary>
+    /// <param name="relationships">The specific relationships that should be crawled.</param>
+    public FollowAttribute(Relationships relationships) {
+      Relationships = relationships;
+    }
+
+    /*==========================================================================================================================
+    | PROPERTY: RELATIONSHIPS
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Gets the type(s) of relationships that should be recursed over.
+    /// </summary>
+    public Relationships Relationships { get; }
+
+  } //Class
+} //Namespace
