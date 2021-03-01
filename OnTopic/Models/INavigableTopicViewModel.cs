adf7ff8b2cb939d4e6d9e57acc1eae7ece5669c4
JeremyCaney <jeremy.caney@ignia.com>
2021-03-01 12:19:51 -0800

    Centralized key navigation properties to `INavigableTopicViewModel`
    These were already defined on `INavigationTopicViewModel<T>`, but that _also_ implemented `IHierarchicalTopicViewModel<T>`.
    
    Sometimes, there are topics that should be evaluated for navigation, but which _aren't_ hierarchical, and which _don't_ necessitate the use of e.g. the `IHierarchicalTopicMappingService` or `NavigationViewComponentBase<T>`. For example, offering a list of child topics on a page. In this case, these _may_ implement `IHierarchicalTopicViewModel<T>`, but they only _need_ `Title`, `ShortTitle`, and `WebPath`.
    
    To support this, the `INavigableTopicViewModel` extracts these from the `INavigationTopicViewModel<T>` so they can, optionally, be applied independently. Then, the `INavigationTopicViewModel<T>` effectively becomes a composite of `INavigableTopicViewModel` and `IHierarchicalTopicViewModel<T>`, adding only `IsSelected()`.
    
    This is useful since the view models for the `NavigationTopicViewComponent<T>` are likely going to be independent from other view models, since most view models don't want or need a `Children` collection. But many basic view models will satisfy the `INavigableTopicViewModel` interface, and should be usable for that purpose. This allows different view models types from different chains (e.g., `OnTopic.ViewModels` and a separate customer implementation) to be handled by e.g. the same partial control.

---
+++ OnTopic/Models/INavigableTopicViewModel.cs
@@ -0,0 +1,47 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia
+| Project       Website
+\=============================================================================================================================*/
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace OnTopic.Models {
+
+  /*============================================================================================================================
+  | INTERFACE: NAVIGABLE TOPIC VIEW MODEL
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Ensures basic properties needed to treat a view model as a navigable entry.
+  /// </summary>
+  /// <remarks>
+  ///   No topics are expected to have a <c>Navigable</c> content type. Instead, implementers of this view model are expected
+  ///   to manually construct instances.
+  /// </remarks>
+  public interface INavigableTopicViewModel {
+
+    /*==========================================================================================================================
+    | PROPERTY: TITLE
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <inheritdoc cref="ITopicViewModel.Title"/>
+    [Required, NotNull, DisallowNull]
+    string? Title { get; init; }
+
+    /*==========================================================================================================================
+    | PROPERTY: SHORT TITLE
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   In addition to the Title, a site may opt to define a Short Title used exclusively in the navigation. If present, this
+    ///   value should be used instead of Title.
+    /// </summary>
+    string? ShortTitle { get; init; }
+
+    /*==========================================================================================================================
+    | PROPERTY: WEB PATH
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <inheritdoc cref="ITopicViewModel.WebPath"/>
+    [Required, NotNull, DisallowNull]
+    string? WebPath { get; init; }
+
+  } //Class
+} //Namespace
