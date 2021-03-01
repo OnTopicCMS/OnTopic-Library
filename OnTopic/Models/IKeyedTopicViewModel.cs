565273fa93217ca507c611a8b4f578384a1dc667
JeremyCaney <jeremy.caney@ignia.com>
2021-03-01 11:46:16 -0800

    Introduced new `IKeyedTopicViewModel` interface
    One of the places where we rely on `ITopicViewModel` is for the `KeyedTopicViewModelCollection`. But that only necessitates a `Key`, so that each instance can be indexed by the collection. Obviously, the generic types will almost certainly have more properties, but as those are strongly typed once the generic has been constructed, that's not an issue. Given that, requiring the (overly) comprehensive `ITopicViewModel` is excessive. This provides a (very) lightweight, base interface for keyed (view) models.

---
+++ OnTopic/Models/IKeyedTopicViewModel.cs
@@ -0,0 +1,40 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Topics Library
+\=============================================================================================================================*/
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using OnTopic.Mapping;
+
+namespace OnTopic.Models {
+
+  /*============================================================================================================================
+  | INTERFACE: KEYED TOPIC VIEW MODEL
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Ensures that a model maintains, at minimum, a <see cref="Key"/> property, necessary to support e.g. a <see cref="
+  ///   KeyedCollection{TKey, TValue}"/>.
+  /// </summary>
+  /// <remarks>
+  ///   It is not required that topic view models implement the <see cref="IKeyedTopicViewModel"/> interface for the <see cref="
+  ///   TopicMappingService"/> to correctly identify and map <see cref="Topic"/>s to topic view models. That said, the interface
+  ///   does ensure that those view models can be keyed, which is useful for, especially, child collections.
+  /// </remarks>
+  public interface IKeyedTopicViewModel {
+
+    /*==========================================================================================================================
+    | PROPERTY: KEY
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Gets the topic's <see cref="Key"/> attribute, the primary text identifier for the <see cref="Topic"/>.
+    /// </summary>
+    /// <requires description="The value from the getter must not be null." exception="T:System.ArgumentNullException">
+    ///   value is not null
+    /// </requires>
+    [Required, NotNull, DisallowNull]
+    string? Key { get; init; }
+
+  } //Class
+} //Namespace
