07544859d836e722ca788b6c74e5c1f7a2aa206f
JeremyCaney <Jeremy.Caney@Ignia.com>
2021-03-09 11:48:00 -0800

    Introduced test for `ResetPermittedContentTypes()`
    The `PermittedContentTypes` collection can usually remain cached once it's been generated. If the `ContentTypes` collection changes, however, then it should be reset. The `ResetPermittedContentTypes()` allows for this to happen. This test validates that behavior.

--- OnTopic.Tests/ContentTypeDescriptorTest.cs
+++ OnTopic.Tests/ContentTypeDescriptorTest.cs
@@ -46,5 +46,40 @@ namespace OnTopic.Tests {
       Assert.IsTrue(page.PermittedContentTypes.Contains(video));
 
     }
+
+    /*==========================================================================================================================
+    | TEST: CONTENT TYPE DESCRIPTOR: RESET PERMITTED CONTENT TYPES: RETURNS UPDATED
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Creates a hierarchy of <see cref="ContentTypeDescriptor"/>s, and ensures that <see cref="ContentTypeDescriptor.
+    ///   PermittedContentTypes"/> returns all updated <see cref="ContentTypeDescriptor"/> instances from the associated
+    ///   <c>ContentTypes</c> collection in <see cref="Topic.Relationships"/> after calling <see cref="ContentTypeDescriptor.
+    ///   ResetPermittedContentTypes"/>.
+    /// </summary>
+    [TestMethod]
+    public void ContentTypeDescriptor_ResetPermittedContentTypes_ReturnsUpdated() {
+
+      var page                  = new ContentTypeDescriptor("Page", "ContentTypeDescriptor");
+      var video                 = new ContentTypeDescriptor("Video", "ContentTypeDescriptor", page);
+      var slideshow             = new ContentTypeDescriptor("Slideshow", "ContentTypeDescriptor", page);
+
+      page.Relationships.SetValue("ContentTypes", page);
+      page.Relationships.SetValue("ContentTypes", video);
+
+      var initialCollection     = page.PermittedContentTypes.ToList();
+
+      page.ResetPermittedContentTypes();
+      page.Relationships.Remove("ContentTypes", page);
+      page.Relationships.SetValue("ContentTypes", slideshow);
+
+      Assert.AreEqual<int>(2, page.PermittedContentTypes.Count);
+      Assert.IsTrue(page.PermittedContentTypes.Contains(video));
+      Assert.IsTrue(page.PermittedContentTypes.Contains(slideshow));
+      Assert.IsFalse(page.PermittedContentTypes.Contains(page));
+      Assert.IsTrue(initialCollection.Contains(page));
+      Assert.IsFalse(initialCollection.Contains(slideshow));
+
+    }
+
   } //Class
 } //Namespace
