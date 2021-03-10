dd3c895a24e285a09f77ec6ad09680c7375eec4c
JeremyCaney <Jeremy.Caney@Ignia.com>
2021-03-10 12:23:56 -0800

    Introduced test for `DefaultTopicLookupService`

--- OnTopic.Tests/TypeLookupServiceTest.cs
+++ OnTopic.Tests/TypeLookupServiceTest.cs
@@ -4,11 +4,14 @@
 | Project       Topics Library
 \=============================================================================================================================*/
 using System;
+using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OnTopic.Internal.Diagnostics;
 using OnTopic.Lookup;
+using OnTopic.Metadata;
 using OnTopic.Tests.BindingModels;
+using OnTopic.Tests.Entities;
 
 namespace OnTopic.Tests {
 
@@ -23,6 +26,27 @@ namespace OnTopic.Tests {
   [TestClass]
   [ExcludeFromCodeCoverage]
   public class TypeLookupServiceTest {
+
+    /*==========================================================================================================================
+    | TEST: DEFAULT TOPIC LOOKUP SERVICE: LOOKUP: RETURNS EXPECTED
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Tests the <see cref="DefaultTopicLookupService"/> to ensure it correctly identifies not only the built-in models, but
+    ///   also an additional model injected via the constructor.
+    /// </summary>
+    [TestMethod]
+    public void DefaultTopicLookupService_Lookup_ReturnsExpected() {
+
+      var topics = new List<Type> {
+        typeof(CustomTopic)
+      };
+      var lookupService         = new DefaultTopicLookupService(topics);
+
+      Assert.AreEqual<Type?>(typeof(AttributeDescriptor), lookupService.Lookup(nameof(AttributeDescriptor)));
+      Assert.AreEqual<Type?>(typeof(CustomTopic), lookupService.Lookup(nameof(CustomTopic)));
+      Assert.IsNull(lookupService.Lookup("TextAttributeDescriptor"));
+
+    }
 
     /*==========================================================================================================================
     | TEST: DYNAMIC TOPIC BINDING MODEL LOOKUP SERVICE: LOOKUP: RETURNS EXPECTED
