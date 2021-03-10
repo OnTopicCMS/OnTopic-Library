a2ee7b1a4f4805335c852dac1c785a642f14f8f8
JeremyCaney <Jeremy.Caney@Ignia.com>
2021-03-10 12:17:04 -0800

    Introduce new `TypeLookupServiceTest` class
    This will be used to test `ITypeLookupService` implementations, such as `StaticTypeLookupService`, `DynamicTypeLookupService`, `DynamicTopicBindingModelLookupService`, &c.

---
+++ OnTopic.Tests/TypeLookupServiceTest.cs
@@ -0,0 +1,28 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Topics Library
+\=============================================================================================================================*/
+using System;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OnTopic.Internal.Diagnostics;
+using OnTopic.Lookup;
+using OnTopic.Tests.BindingModels;
+
+namespace OnTopic.Tests {
+
+  /*============================================================================================================================
+  | CLASS: TYPE LOOKUP SERVICE TEST
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Provides unit tests for the <see cref="ITypeLookupService"/> interface and its implementations, such as the <see cref="
+  ///   StaticTypeLookupService"/>, <see cref="DynamicTypeLookupService"/>, <see cref="DynamicTopicBindingModelLookupService"/>,
+  ///   and the underlying <see cref="TypeCollection"/>.
+  /// </summary>
+  [TestClass]
+  [ExcludeFromCodeCoverage]
+  public class TypeLookupServiceTest {
+
+  } //Class
+} //Namespace
