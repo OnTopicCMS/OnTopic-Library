69022a0360ce26c837f358866343f477221c167a
JeremyCaney <Jeremy.Caney@Ignia.com>
2021-03-12 13:02:38 -0800

    Updated `Load()` to use `GetByUniqueKey()` extension method
    This method is faster for looking up unique keys. It also better matches how real world topic repositories implement `Load()`.

--- OnTopic.TestDoubles/StubTopicRepository.cs
+++ OnTopic.TestDoubles/StubTopicRepository.cs
@@ -85,7 +85,7 @@ namespace OnTopic.TestDoubles {
       var topic = _cache;
       if (uniqueKey is not null && uniqueKey.Length > 0) {
         uniqueKey = uniqueKey.Contains(":", StringComparison.Ordinal) ? uniqueKey : "Root:" + uniqueKey;
-        topic = _cache.FindFirst(t => t.GetUniqueKey().Equals(uniqueKey, StringComparison.OrdinalIgnoreCase));
+        topic = _cache.GetByUniqueKey(uniqueKey);
       }
 
       /*------------------------------------------------------------------------------------------------------------------------
