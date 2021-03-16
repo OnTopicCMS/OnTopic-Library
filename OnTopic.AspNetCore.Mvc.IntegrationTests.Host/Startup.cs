b24886db64c2e00572507e679c4c22579f6103c0
JeremyCaney <Jeremy.Caney@Ignia.com>
2021-03-16 14:53:03 -0700

    Established barebones `Host` project for integration testing
    Integration testing for ASP.NET Core operates off of a host ASP.NET Core application. This establishes a barebones `Host` project. Currently, it doesn't have any views or topic routes, but those will be established in response to specific needs identified for integration tests.

---
+++ OnTopic.AspNetCore.Mvc.IntegrationTests.Host/Startup.cs
@@ -0,0 +1,92 @@
+/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Integration Tests Host
+\=============================================================================================================================*/
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace OnTopic.AspNetCore.Mvc.IntegrationTests.Host {
+
+  /*============================================================================================================================
+  | CLASS: STARTUP
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Configures the application and sets up dependencies.
+  /// </summary>
+  [ExcludeFromCodeCoverage]
+  public class Startup {
+
+    /*==========================================================================================================================
+    | CONSTRUCTOR
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Constructs a new instances of the <see cref="Startup"/> class. Accepts an <see cref="IConfiguration"/>.
+    /// </summary>
+    /// <param name="configuration">
+    ///   The shared <see cref="IConfiguration"/> dependency.
+    /// </param>
+    public Startup(IConfiguration configuration) {
+      Configuration = configuration;
+    }
+
+    /*==========================================================================================================================
+    | PROPERTY: CONFIGURATION
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Provides a (public) reference to the application's <see cref="IConfiguration"/> service.
+    /// </summary>
+    public IConfiguration Configuration { get; }
+
+    /*==========================================================================================================================
+    | METHOD: CONFIGURE SERVICES
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Provides configuration of services. This method is called by the runtime to bootstrap the server configuration.
+    /// </summary>
+    public void ConfigureServices(IServiceCollection services) {
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Configure: MVC
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      services.AddControllersWithViews()
+
+        //Add OnTopic support
+        .AddTopicSupport();
+
+    /*==========================================================================================================================
+    | METHOD: CONFIGURE (APPLICATION)
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Provides configuration the application. This method is called by the runtime to bootstrap the application
+    ///   configuration, including the HTTP pipeline.
+    /// </summary>
+    public static void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Configure: Error Pages
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      app.UseDeveloperExceptionPage();
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Configure: Server defaults
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      app.UseStaticFiles();
+      app.UseRouting();
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Configure: MVC
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      app.UseEndpoints(endpoints => {
+        endpoints.MapControllers();
+      });
+
+    }
+
+  } //Class
+} //Namespace
