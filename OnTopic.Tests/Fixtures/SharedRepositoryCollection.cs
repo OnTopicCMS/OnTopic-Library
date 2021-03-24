e6f0ffb9a08755678222118901dd66d0d95eb3ec
JeremyCaney <Jeremy.Caney@Ignia.com>
2021-03-24 13:48:22 -0700

    Established shared collection
    The `SharedRepositoryCollection` ("Shared Repository") allows resources to be shared between multiple unit test classes. It also allows the definition of the `TopicInfrastructureFixture` to be centrally defined.

---
+++ OnTopic.Tests/Fixtures/SharedRepositoryCollection.cs
@@ -0,0 +1,22 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Topics Library
+\=============================================================================================================================*/
+using OnTopic.TestDoubles;
+using Xunit;
+
+namespace OnTopic.Tests.Fixtures {
+
+  /*============================================================================================================================
+  | CLASS: SHARED REPOSITORY COLLECTION
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Defines a common context used to share an implementation of the <see cref="TopicInfrastructureFixture{T}"/> between
+  ///   tests using a common <see cref="StubTopicRepository"/>.
+  /// </summary>
+  [CollectionDefinition("Shared Repository")]
+  public class SharedRepositoryCollection: ICollectionFixture<TopicInfrastructureFixture<StubTopicRepository>> {
+
+  }
+}
