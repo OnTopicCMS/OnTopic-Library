0f55914260d09ead73d82fc3e65156d7270067db
JeremyCaney <Jeremy.Caney@Ignia.com>
2021-03-24 17:17:17 -0700

    Introduced test for successful `Convert(string, Type)`
    Introduced a test for `AttributeValueConverter.Convert(string, Type)` which confirms that sample data for each type returns the expected results. This doesn't include `Uri` or `DateTime` since I can't construct a constant value for their success state via `[InlineData()]`.

--- OnTopic.Tests/AttributeValueConverterTest.cs
+++ OnTopic.Tests/AttributeValueConverterTest.cs
@@ -40,5 +40,28 @@ namespace OnTopic.Tests {
     public void IsConvertible_Type_ReturnsExpected(Type type, bool result) =>
       Assert.True(AttributeValueConverter.IsConvertible(type) == result);
 
+    /*==========================================================================================================================
+    | TEST: CONVERT: VALUE: SUCCEEDS
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Passes a valid <paramref name="input"/> into <see cref="AttributeValueConverter.Convert(String?, Type)"/> based on a
+    ///   <paramref name="type"/> and confirms that the result matches the <paramref name="expected"/>.
+    /// </summary>
+    [Theory]
+    [InlineData(                typeof(string),                 "String",                       "String")]
+    [InlineData(                typeof(int),                    "1",                            1)]
+    [InlineData(                typeof(bool),                   "1",                            true)]
+    [InlineData(                typeof(bool),                   "0",                            false)]
+    [InlineData(                typeof(bool),                   "true",                         true)]
+    [InlineData(                typeof(bool),                   "false",                        false)]
+    [InlineData(                typeof(string),                 "",                             "")]
+    [InlineData(                typeof(string),                 null,                           null)]
+    [InlineData(                typeof(int?),                   "",                             null)]
+    [InlineData(                typeof(bool?),                  "",                             null)]
+    [InlineData(                typeof(DateTime?),              "",                             null)]
+    [InlineData(                typeof(object),                 "Value",                        null)]
+    public void Convert_Value_Succeeds(Type type, string? input, object? expected) =>
+      Assert.Equal(expected, AttributeValueConverter.Convert(input, type));
+
   } //Class
 } //Namespace
