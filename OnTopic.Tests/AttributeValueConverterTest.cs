de7d6fa59511c8a04838a9188f1f0163a9b90756
JeremyCaney <Jeremy.Caney@Ignia.com>
2021-03-24 17:15:44 -0700

    Introduced test for successful `IsConvertible()`
    Introduced a test for `AttributeValueConverter.IsConvertible(type)` which confirms that each type returns the expected results. As this uses an array for an attribute, the class must not be marked as CLI Compliant, so I disabled that as part of this.

--- OnTopic.Tests/AttributeValueConverterTest.cs
+++ OnTopic.Tests/AttributeValueConverterTest.cs
@@ -19,5 +19,26 @@ namespace OnTopic.Tests {
   [ExcludeFromCodeCoverage]
   public class AttributeValueConverterTest {
 
+    /*==========================================================================================================================
+    | TEST: IS CONVERTIBLE? TYPE: RETURNS EXPECTED
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Passes a <paramref name="type"/> into the <see cref="AttributeValueConverter.IsConvertible(Type)"/> method and
+    ///   confirms that result matches the <paramref name="result"/>.
+    ///   <c>true</c>
+    /// </summary>
+    [Theory]
+    [InlineData(                typeof(string),                 true)]
+    [InlineData(                typeof(int),                    true)]
+    [InlineData(                typeof(bool),                   true)]
+    [InlineData(                typeof(DateTime),               true)]
+    [InlineData(                typeof(Uri),                    true)]
+    [InlineData(                typeof(int?),                   true)]
+    [InlineData(                typeof(bool?),                  true)]
+    [InlineData(                typeof(DateTime?),              true)]
+    [InlineData(                typeof(object),                 false)]
+    public void IsConvertible_Type_ReturnsExpected(Type type, bool result) =>
+      Assert.True(AttributeValueConverter.IsConvertible(type) == result);
+
   } //Class
 } //Namespace
