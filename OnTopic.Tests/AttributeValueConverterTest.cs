535632902e98655f5b53bcdd3f18e914e31701d5
JeremyCaney <Jeremy.Caney@Ignia.com>
2021-03-24 17:13:52 -0700

    Established unit test class for the `AttributeValueConverter`
    In a recent update, I introduced a new `AttributeValueConverter` class. While this is indirectly tested via e.g. `AttributeValueCollection` extension tests, I wanted to directly test it. I will be introducing tests in the subsequent commits.

---
+++ OnTopic.Tests/AttributeValueConverterTest.cs
@@ -0,0 +1,23 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Topics Library
+\=============================================================================================================================*/
+using System;
+using System.Diagnostics.CodeAnalysis;
+using OnTopic.Attributes;
+using Xunit;
+
+namespace OnTopic.Tests {
+
+  /*============================================================================================================================
+  | CLASS: ATTRIBUTE VALUE CONVERTER TEST
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Provides unit tests for the <see cref="AttributeValueConverter"/> class.
+  /// </summary>
+  [ExcludeFromCodeCoverage]
+  public class AttributeValueConverterTest {
+
+  } //Class
+} //Namespace
