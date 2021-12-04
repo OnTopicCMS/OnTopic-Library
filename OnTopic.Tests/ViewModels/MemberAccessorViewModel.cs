cb28d35f86f89844baf0e2d7bfbc1b301b66ce82
JeremyCaney <Jeremy.Caney@Ignia.com>
2021-12-04 12:33:51 -0800

    Established basic unit tests for the new `MemberAccessor`
    These tests ensures that basic access to the `MemberAccessor` functions, including the validation and parsing of the required `MemberInfo`, as well as basic `GetValue()` and `SetValue()` methods.
    
    This includes a new `MemberAccessorViewModel` for evaluating `MemberInfo` types specific to this class.
    
    In practice, most access will be performed via higher level classes and, as such, we'll expect more comprehensive tests related to type compatibility and conversion to be performed as part of those classes in the future.

---
+++ OnTopic.Tests/ViewModels/MemberAccessorViewModel.cs
@@ -0,0 +1,37 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Topics Library
+\=============================================================================================================================*/
+using System;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using OnTopic.ViewModels;
+
+namespace OnTopic.Tests.ViewModels {
+
+  /*============================================================================================================================
+  | VIEW MODEL: MEMBER ACCESSOR
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Provides a strongly-typed data transfer object for testing settable methods and gettable methods.
+  /// </summary>
+  /// <remarks>
+  ///   This is a sample class intended for test purposes only; it is not designed for use in a production environment.
+  /// </remarks>
+  [ExcludeFromCodeCoverage]
+  public class MemberAccessorViewModel {
+
+    public MemberAccessorViewModel() { }
+    public int? NullableProperty { get; set; }
+    public int NonNullableProperty { get; set; }
+    public Type NonNullableReferenceGetter { get; set; } = typeof(MemberAccessorViewModel);
+    public int? ReadOnlyProperty { get; }
+    public int WriteOnlyProperty { set { } }
+    public int GetMethod() => 0;
+    public int InvalidGetMethod(int value) => value;
+    public void SetMethod(int value) => _ = value;
+    public void InvalidSetMethod(int value, int newValue) => _ = value == newValue;
+
+  } //Class
+} //Namespace
