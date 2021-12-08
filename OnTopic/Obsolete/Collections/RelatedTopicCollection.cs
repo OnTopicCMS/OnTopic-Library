6d43ea7640d4927132a9fc1def56d50e7de900c9
JeremyCaney <Jeremy.Caney@Ignia.com>
2021-12-08 13:09:13 -0800

    CA1801: Removed unnecessary suppression

--- OnTopic/Obsolete/Collections/RelatedTopicCollection.cs
+++ OnTopic/Obsolete/Collections/RelatedTopicCollection.cs
@@ -9,7 +9,6 @@ using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using OnTopic.Internal.Diagnostics;
 
-#pragma warning disable CA1801 // Review unused parameters
 #pragma warning disable IDE0060 // Remove unused parameter
 
 namespace OnTopic.Collections {
@@ -199,5 +198,4 @@ namespace OnTopic.Collections {
   } //Class
 } //Namespace
 
-#pragma warning restore CA1801 // Review unused parameters
 #pragma warning restore IDE0060 // Remove unused parameter
