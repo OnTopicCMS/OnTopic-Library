c284285fb15aec64e88723db33de0a9569730bbb
JeremyCaney <Jeremy.Caney@Ignia.com>
2021-12-11 12:08:49 -0800

    Fixed indentation with `Program`
    With the ASP.NET Core 6 Hosting Model now in place, we can now unify the indentation, taking full advantage of C# 9 top-level statements.

--- OnTopic.AspNetCore.Mvc.Host/Program.cs
+++ OnTopic.AspNetCore.Mvc.Host/Program.cs
@@ -8,66 +8,66 @@ using Microsoft.AspNetCore.Mvc.ViewComponents;
 using OnTopic.AspNetCore.Mvc;
 using OnTopic.AspNetCore.Mvc.Host;
 
-    /*==========================================================================================================================
-    | CONFIGURE SERVICES
-    \-------------------------------------------------------------------------------------------------------------------------*/
-    var builder = WebApplication.CreateBuilder(args);
+/*==============================================================================================================================
+| CONFIGURE SERVICES
+\-----------------------------------------------------------------------------------------------------------------------------*/
+var builder = WebApplication.CreateBuilder(args);
 
-      /*------------------------------------------------------------------------------------------------------------------------
-      | Configure: Cookie Policy
-      \-----------------------------------------------------------------------------------------------------------------------*/
-      builder.Services.Configure<CookiePolicyOptions>(options => {
-        // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-        options.CheckConsentNeeded = context => true;
-        options.MinimumSameSitePolicy = SameSiteMode.None;
-      });
+/*------------------------------------------------------------------------------------------------------------------------------
+| Configure: Cookie Policy
+\-----------------------------------------------------------------------------------------------------------------------------*/
+builder.Services.Configure<CookiePolicyOptions>(options => {
+  // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+  options.CheckConsentNeeded = context => true;
+  options.MinimumSameSitePolicy = SameSiteMode.None;
+});
 
-      /*------------------------------------------------------------------------------------------------------------------------
-      | Configure: MVC
-      \-----------------------------------------------------------------------------------------------------------------------*/
-      builder.Services.AddControllersWithViews()
+/*------------------------------------------------------------------------------------------------------------------------------
+| Configure: MVC
+\-----------------------------------------------------------------------------------------------------------------------------*/
+builder.Services.AddControllersWithViews()
 
-        //Add OnTopic support
-        .AddTopicSupport();
+  //Add OnTopic support
+  .AddTopicSupport();
 
-      /*------------------------------------------------------------------------------------------------------------------------
-      | Register: Activators
-      \-----------------------------------------------------------------------------------------------------------------------*/
-      var activator = new SampleActivator(builder.Configuration.GetConnectionString("OnTopic"));
+/*------------------------------------------------------------------------------------------------------------------------------
+| Register: Activators
+\-----------------------------------------------------------------------------------------------------------------------------*/
+var activator = new SampleActivator(builder.Configuration.GetConnectionString("OnTopic"));
 
-      builder.Services.AddSingleton<IControllerActivator>(activator);
-      builder.Services.AddSingleton<IViewComponentActivator>(activator);
+builder.Services.AddSingleton<IControllerActivator>(activator);
+builder.Services.AddSingleton<IViewComponentActivator>(activator);
 
-    /*==========================================================================================================================
-    | CONFIGURE APPLICATION
-    \-------------------------------------------------------------------------------------------------------------------------*/
-    var app = builder.Build();
+/*==============================================================================================================================
+| CONFIGURE APPLICATION
+\-----------------------------------------------------------------------------------------------------------------------------*/
+var app = builder.Build();
 
-      /*------------------------------------------------------------------------------------------------------------------------
-      | Configure: Error Pages
-      \-----------------------------------------------------------------------------------------------------------------------*/
-      if (app.Environment.IsDevelopment()) {
-        app.UseDeveloperExceptionPage();
-      }
-      else {
-        app.UseExceptionHandler("/Home/Error");
-        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-        app.UseHsts();
-      }
+/*------------------------------------------------------------------------------------------------------------------------------
+| Configure: Error Pages
+\-----------------------------------------------------------------------------------------------------------------------------*/
+if (app.Environment.IsDevelopment()) {
+  app.UseDeveloperExceptionPage();
+}
+else {
+  app.UseExceptionHandler("/Home/Error");
+  // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+  app.UseHsts();
+}
 
-      /*------------------------------------------------------------------------------------------------------------------------
-      | Configure: Server defaults
-      \-----------------------------------------------------------------------------------------------------------------------*/
-      app.UseHttpsRedirection();
-      app.UseStaticFiles();
-      app.UseCookiePolicy();
-      app.UseRouting();
-      app.UseCors("default");
+/*------------------------------------------------------------------------------------------------------------------------------
+| Configure: Server defaults
+\-----------------------------------------------------------------------------------------------------------------------------*/
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseCookiePolicy();
+app.UseRouting();
+app.UseCors("default");
 
-      /*------------------------------------------------------------------------------------------------------------------------
-      | Configure: MVC
-      \-----------------------------------------------------------------------------------------------------------------------*/
-        app.MapTopicRoute("Web");
-        app.MapTopicSitemap();
-        app.MapTopicRedirect();
-        app.MapControllers();
+/*------------------------------------------------------------------------------------------------------------------------------
+| Configure: MVC
+\-----------------------------------------------------------------------------------------------------------------------------*/
+app.MapTopicRoute("Web");
+app.MapTopicSitemap();
+app.MapTopicRedirect();
+app.MapControllers();
