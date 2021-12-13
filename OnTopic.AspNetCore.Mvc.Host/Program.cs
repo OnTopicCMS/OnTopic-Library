5140a7e89f259be676f9545b818d5a8fc1c815f0
JeremyCaney <Jeremy.Caney@Ignia.com>
2021-12-13 14:50:58 -0800

    Updated `UseExceptionHandler()` to use the new `ErrorController`
    With the introduction of the `ErrorController` (e7fe64fa, bf46877a), we now have a valid target for the `UseExceptionHandler()` call. Previously, this pointed to a non-existent page. This was fine for sample purposes, but now that we have an actual page to us, this is preferred.
    
    This could alternatively point to any arbitrary `Topic` under `Error` (e.g., `Error:InternalServer`).

--- OnTopic.AspNetCore.Mvc.Host/Program.cs
+++ OnTopic.AspNetCore.Mvc.Host/Program.cs
@@ -52,8 +52,8 @@ if (app.Environment.IsDevelopment()) {
   app.UseDeveloperExceptionPage();
 }
 else {
-  app.UseExceptionHandler("/Home/Error");
   app.UseStatusCodePagesWithReExecute("/Error/{0}");
+  app.UseExceptionHandler("/Error/500/");
   // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
   app.UseHsts();
 }
