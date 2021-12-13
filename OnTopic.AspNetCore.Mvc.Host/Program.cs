8ab1d7736154207a3a1027c4b7f7f61faaccd672
JeremyCaney <Jeremy.Caney@Ignia.com>
2021-12-13 14:47:11 -0800

    Introduced ASP.NET Core status code error handling
    With the introduction of the new `ErrorController` (e7fe64fa), I've introduced the `UseStatusCodePagesWithReExecute()` configuration to the `Host` project.

--- OnTopic.AspNetCore.Mvc.Host/Program.cs
+++ OnTopic.AspNetCore.Mvc.Host/Program.cs
@@ -53,6 +53,7 @@ if (app.Environment.IsDevelopment()) {
 }
 else {
   app.UseExceptionHandler("/Home/Error");
+  app.UseStatusCodePagesWithReExecute("/Error/{0}");
   // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
   app.UseHsts();
 }
