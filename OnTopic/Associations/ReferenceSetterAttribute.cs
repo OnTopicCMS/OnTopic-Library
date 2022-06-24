8fab56be0f08c25506ed68a7ab23d38faf354469
JeremyCaney <Jeremy.Caney@Ignia.com>
2022-06-24 16:03:49 -0700

    Fixed errant usage of `<code />` in favor of `<c />`
    The `<code />` element should be reserved for code _blocks_; for inline code references, the `<c />` element should be used instead. This will help support #29.

--- OnTopic/Associations/ReferenceSetterAttribute.cs
+++ OnTopic/Associations/ReferenceSetterAttribute.cs
@@ -18,15 +18,15 @@ namespace OnTopic.Associations {
   ///   <para>
   ///     When a call is made to <see cref="TrackedRecordCollection{TItem, TValue, TAttribute}.SetValue(String, TValue,
   ///     Boolean?, DateTime?)"/> the code will check to see if a property with the same name as the reference key exists, and
-  ///     whether that property is decorated with the <see cref="ReferenceSetterAttribute"/> (i.e., <code>[ReferenceSetter]
-  ///     </code>). If is, then the update will be routed through that property. This ensures that business logic is enforced by
-  ///     local properties, instead of allowing business logic to be potentially bypassed by writing directly to the <see cref="
-  ///     Topic.References"/> collection.
+  ///     whether that property is decorated with the <see cref="ReferenceSetterAttribute"/> (i.e., <c>[ReferenceSetter]</c>). 
+  ///     If is, then the update will be routed through that property. This ensures that business logic is enforced by local 
+  ///     properties, instead of allowing business logic to be potentially bypassed by writing directly to the <see cref="Topic.
+  ///     References"/> collection.
   ///   </para>
   ///   <para>
   ///     As an example, the <see cref="Topic.BaseTopic"/> property is adorned with the <see cref="ReferenceSetterAttribute"/>.
-  ///     As a result, if a client calls <code>topic.References.SetValue("BaseTopic", topic)</code>, then that update will be
-  ///     routed through <see cref="Topic.BaseTopic"/>, thus enforcing any validation.
+  ///     As a result, if a client calls <c>topic.References.SetValue("BaseTopic", topic)</c>, then that update will be routed 
+  ///     through <see cref="Topic.BaseTopic"/>, thus enforcing any validation.
   ///   </para>
   ///   <para>
   ///     To ensure this logic, it is critical that implementers of <see cref="ReferenceSetterAttribute"/> ensure that the
