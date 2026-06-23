4699df33d51c6e246833b322075f29090316f630
Jeremy Caney <Jeremy.Caney@Ignia.com>
2026-06-22 20:21:49 -0700

    Resolve CS8602: Dereference of a null reference
    This would only occur if an assembly were dynamically compiled at runtime. That's not something we currently expect, but there's no reason not to support those scenarios. In that case, such assemblies would still be included.

--- OnTopic/Lookup/DynamicTypeLookupService.cs
+++ OnTopic/Lookup/DynamicTypeLookupService.cs
@@ -31,7 +31,7 @@ namespace OnTopic.Lookup {
       var matchedTypes = AppDomain
         .CurrentDomain
         .GetAssemblies()
-        .Where(a => !a.FullName.StartsWith("Microsoft", StringComparison.Ordinal) && !a.FullName.StartsWith("System", StringComparison.Ordinal))
+        .Where(a => !(a.FullName?.StartsWith("Microsoft", StringComparison.Ordinal) ?? false) && !(a.FullName?.StartsWith("System", StringComparison.Ordinal) ?? false))
         .SelectMany(t => t.GetTypes())
         .Where(t => t.IsClass && predicate(t))
         .OrderBy(t => t.Namespace?.StartsWith("OnTopic", StringComparison.Ordinal))
